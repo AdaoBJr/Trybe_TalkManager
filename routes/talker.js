be4598adf2ad2f162b41be78a942a12938dfec83
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-07-04 18:02:50 -0300

    feat(GET|/talker/:id): returns talker by id endpoint

--- routes/talker.js
+++ routes/talker.js
@@ -14,4 +14,15 @@ router.use(async (req, res, next) => {
 
 router.get('/', (req, res) => res.status(200).json(req.context.data));
 
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const result = req.context.data.find((value) => value.id === Number(id))
+    || new Error('Pessoa palestrante não encontrada');
+  if (result instanceof Error) {
+    result.errCode = 404;
+    return next(result);
+  }
+  return res.status(200).json(result);
+});
+
 module.exports = router;
