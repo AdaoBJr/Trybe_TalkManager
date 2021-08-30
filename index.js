20d30dbc5fb7de732cd6be67636162a78e28432e
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-08-30 20:42:37 -0300

    add exercicio 2

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '8081';
+const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -19,6 +19,18 @@ app.get('/talker', async (req, res) => {
     return res.status(404).json({ message: 'talker not found' });
   }
   return res.status(200).json(file);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const file = await readFile();
+
+  const talker = file.find((person) => person.id === Number(id));
+
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(talker);
 });
 
 app.listen(PORT, () => {
