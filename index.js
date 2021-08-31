0449564e77dfcfd62c5c6357f6b73fb4e14bc66f
esionascimento <esio.392@protonmail.com>
2021-08-31 15:22:38 -0400

    arrumado agora sim desafio 1 passa direto

--- index.js
+++ index.js
@@ -17,13 +17,27 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
-async function req(_req, res) {
+async function datas() {
   const response = await fs.readFile('./talker.json', 'utf8');
   const data = JSON.parse(response);
+  return data;
+}
+
+async function palestrantes(_req, res) {
+  const data = await datas();
+  console.log(data);
   if (data.length === 0) {
     return res.status(HTTP_OK_STATUS).json([]);
   }
-  return res.status(HTTP_OK_STATUS).json({ data });
+  return res.status(HTTP_OK_STATUS).json(data);
 }
 
-app.get('/talker', req);
+app.get('/talker', palestrantes);
+/* 
+async function idPalestrante(req) {
+  const { params: { id } } = req;
+
+  return
+}
+
+app.get('/talker/:id', idPalestrante); */
