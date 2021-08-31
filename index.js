2794f12b4c89ba101b3b31de3a50c2b2cf59e616
isTorres <ismaeltorres612@gmail.com>
2021-08-31 18:00:41 -0300

    trying to fix req 1

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const fs = require('fs');
+
+// fonte: https://nodejs.org/dist/latest-v8.x/docs/api/util.html#util_util_promisify_original
+const { promisify } = require('util');
+
 const app = express();
 app.use(bodyParser.json());
 
-const fs = require('fs');
-const { promisify } = require('util');
-// const { promisify } = require('util');
-
-// const talkers = '/talker.json';
-
 const HTTP_OK_STATUS = 200;
-// const HTTP_NOT_FOUND_STATUS = 404;
+const HTTP_NOT_FOUND = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -21,25 +20,15 @@ app.get('/', (_request, response) => {
 
 // -------------- // --------------- //
 
-app.get('/talker', (req, res) => {  
-  fs.readFile('./talker.json', 'utf-8', promisify((err, content) => {
+app.get('/talker', (_req, res) => {  
+  fs.readFile('./talker.json', 'utf-8', promisify((err, data) => {
     if (err) {
-      res.status(400).send({ message: 'Not Found' });
+      res.status(HTTP_NOT_FOUND).send({ message: 'Not Found' });
       return;
     }
-    console.log('err', err);
-    console.log('cont', content);
-    res.status(200).send(content);
+    res.status(200).send(data);
   }));
 });
-
-// app.get('/talker', (req, res) => {  
-//   const read = async () => {
-//     const talkerList = await fs.readFile(talkers, 'utf-8');
-//     return talkerList;
-//   };
-//   return res.status(HTTP_OK_STATUS).json(read());
-// });
 
 app.listen(PORT, () => {
   console.log('Online');
