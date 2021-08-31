87ba77e3f99f63468608b7e44c3aa200568d0754
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-08-31 20:57:20 +0200

    Code correction

--- utils/login.js
+++ utils/login.js
@@ -9,7 +9,7 @@ async function login(req, res) {
   const regex = /^[^\s@]+@[^\s@]+$/;
   if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   if (!email.match(regex)) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   if (password.length < 6) {
