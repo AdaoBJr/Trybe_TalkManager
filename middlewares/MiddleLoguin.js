fd291896da3e59b7af9c63f6f64aaaf7057cf422
joao966 <joaooliveira966@gmail.com>
2021-08-31 20:31:16 -0300

    FIX: req-3

--- middlewares/MiddleLoguin.js
+++ middlewares/MiddleLoguin.js
@@ -3,16 +3,15 @@ const { validateData } = require('./validadeData');
 
 const loguin = (req, res) => {
   const { email, password } = req.body;
+  console.log(password);
   
   validateData(email, password, res);
 
   const emailIsValid = /((\w+)@(\w+)\.(\w+))/;
   if (!email.match(emailIsValid)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  
-  const passwordLength = 6;
-  if (password < passwordLength) {
+  } if (password.length < 6) {
+    console.log('if pass');
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
