225747d0a691a44501187187a1ffa37e7910db4f
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-31 19:13:03 -0300

    adiciona o endpoint que deleta um palestrante

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 
-const { readContentFile } = require('../helpers/readWriteFile');
+const { readContentFile, writeContentFile } = require('../helpers/readWriteFile');
+const { 
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate } = require('../middlewares/validations');
 
 const PATH_FILE = './talker.json';
 
@@ -19,6 +26,30 @@ router.get('/:id', async (req, res) => {
   if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   return res.status(200).json(talkerId);
+});
+
+router.post('/',
+validateToken, 
+validateName,
+validateAge,
+validateTalk,
+validateRate,
+validateWatchedAt,
+
+async (req, res) => {
+  const newTalker = { ...req.body };
+  const talker = await writeContentFile(PATH_FILE, newTalker);
+
+  return res.status(201).json([talker]);
+});
+
+router.delete('/:id', validateToken, async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readContentFile(PATH_FILE) || [];
+  const talkerId = talkers.findIndex((talker) => talker.id === Number(id));
+  
+  talkers.splice(talkerId, 1);
+  return res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
 });
 
 module.exports = router;
