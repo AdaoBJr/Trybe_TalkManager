e2d245a4528526ad7725ca55abbd9253e5e39da7
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-31 15:25:32 -0300

    adicionando a função que valida o nome

--- utils.js
+++ utils.js
@@ -60,9 +60,26 @@ function validateToken(req, res, next) {
   next();
 }
 
+function validateName(req, res, next) {
+  const { name } = req.body;
+  const blankSpacesRegex = /^\s+$/;
+
+  if (!name || blankSpacesRegex.test(name)) {
+    return res.status(HTTP_BAD_REQUEST_STATUS).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res.status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+}
+
 module.exports = {
   readTalkerFile,
   validateEmail,
   validatePassword,
   validateToken,
+  validateName,
 };
