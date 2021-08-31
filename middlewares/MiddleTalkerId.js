5ce0c4831e8e62ba304b7e8ef0c73ee9c3d44c94
joao966 <joaooliveira966@gmail.com>
2021-08-31 17:17:10 -0300

    FIX: request-2

--- middlewares/MiddleTalkerId.js
+++ middlewares/MiddleTalkerId.js
@@ -5,8 +5,11 @@ const talkerId = async (req, res) => {
   const response = await fs.readFile('./talker.json', 'utf-8');
   const convert = JSON.parse(response);
   const data = convert.find((cur) => cur.id === Number(id));
+  if (!data) {
+    return res.status(200).json({ message: 'Pessoa palestrante não encontrada' });
+  }
   
-  return res.json(data);
+  return res.status(200).json(data);
 };
 
 module.exports = {
