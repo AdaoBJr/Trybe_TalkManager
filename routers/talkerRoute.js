757c694f6713711ac04e00c92ec037791f17a043
JonnesBezerra <jonnes.bezerra@gmail.com>
2021-08-31 20:08:44 -0300

    Feat: requirement 2 DONE!

--- routers/talkerRoute.js
+++ routers/talkerRoute.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const fs = require('fs').promises;
-// const talkers = require('../talker.json');
-
-console.log(typeof talkers);
 
 const router = express.Router();
 const HTTP_OK_STATUS = 200;
+const HTTP_NOTFOUND_STATUS = 404;
 
 const getTalkerList = async () => {
   const list = await fs.readFile('./talker.json', 'utf-8');
-  console.log(list);
   return JSON.parse(list);
 };
 
 router.get('/', async (_req, res) => {
   const talkersList = await getTalkerList();
   res.status(HTTP_OK_STATUS).json(talkersList);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkersList = await getTalkerList();
+  const talker = talkersList.find((t) => t.id === parseInt(id, 10));
+  if (!talker) {
+    return res.status(HTTP_NOTFOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(HTTP_OK_STATUS).json(talker);
 });
 
 module.exports = router;
