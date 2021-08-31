a8d447179abd13fc4ca3ec23c85f8bdc26494b1b
esmaben <felippecorrea2010@gmail.com>
2021-08-30 21:44:07 -0300

    DONE: requisitos 1 e 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,26 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  fs.readFile('./talker.json', 'utf-8', (err, content) => {
+    if (err) {
+      return res.status(200).json([]);
+    }
+    const data = JSON.parse(content);
+    return res.status(200).json(data);
+  });
+});
+
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+  fs.readFile('./talker.json', 'utf-8', (err, content) => {
+    const data = JSON.parse(content);
+    const talker = data.find((r) => r.id === Number(id));
+    if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(200).json(talker);
+  });
 });
 
 app.listen(PORT, () => {
