47f6dc5fc9dcad3109df326fdea60e7a0d7b79ae
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-31 11:23:09 -0300

    implementando o endpoint GET /talker/:id

--- talkerRoutes.js
+++ talkerRoutes.js
@@ -2,7 +2,21 @@ const express = require('express');
 const { readTalkerFile } = require('./utils');
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 const router = express.Router();
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const talkers = readTalkerFile();
+
+  const talkerFound = talkers.find((talker) => `${talker.id}` === id);
+
+  if (!talkerFound) {
+    return res.status(HTTP_NOT_FOUND_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(HTTP_OK_STATUS).json(talkerFound);
+});
 
 router.get('/', (req, res) => {
   const talkers = readTalkerFile();
