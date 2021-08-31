c6fd6f0d2d382adeec537269c06b822ddd641e38
CIGR2021 <appencigr@gmail.com>
2021-08-31 17:35:59 -0300

    Requisito 2 - Feito

--- routes/talkerRoute.js
+++ routes/talkerRoute.js
@@ -19,10 +19,23 @@ const getRequisition = (req, res) => {
   return res.status(200).send(talker);
 };
 
+const getRequisitionID = (req, res) => {
+  const talker = getTalker();
+  const filterID = talker.filter((talk) => talk.id === Number(req.params.id));
+  
+  const result = !filterID.length
+    ? res.status(404).send({ message: 'Pessoa palestrante não encontrada' })
+    : res.status(200).send(filterID);
+  
+  return result;
+};
 const talkerRoute = (app) => {
   app.route('/talker')
     // Pegando usuários
     .get(getRequisition);
+  app.route('/talker/:id?')
+    // Pegando usuários
+    .get(getRequisitionID);
 };
 
 module.exports = talkerRoute;
