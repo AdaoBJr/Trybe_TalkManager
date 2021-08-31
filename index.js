4f81508a1b41afd36dab13861d66d28c37e3b783
arthur menezes <arthurmcorrea282@gmail.com>
2021-08-31 17:25:59 -0300

    re1 2

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 
 const { getTalker } = require('./getArquivo/getTalker');
 
 const app = express();
+
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
@@ -22,3 +24,16 @@ app.get('/talker', async (_req, res) => {
   const content = (await getTalker());
   res.status(200).send(content);
 });
+
+app.get(
+  '/talker/:id',
+  rescue(async (req, res) => {
+    const talker = (await getTalker());
+    const falador = talker.find(({ id }) => id === Number(req.params.id));
+    if (!falador) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+    return res.status(200).json(falador);
+  }),
+);
