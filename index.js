cac510a9d2db7d97ae0208ec00746796e8c47c5f
Ederson Rodrigues Araujo <edersonrodara@gmail.com>
2021-08-31 13:34:47 -0300

    desafio3 - completo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,19 +18,68 @@ const readFile = async (arquivo) => {
   return parseResposta;
 };
 
+const gerarToken = () => {
+  const resultado = crypto.randomBytes(8).toString('hex');
+  return resultado;
+};
+
+const validarEmail = (req, res, next) => {
+  const {
+    email,
+  } = req.body;
+  if (!email) {
+    return res.status(400).json({
+      message: 'O campo "email" é obrigatório',
+    });
+  }
+  if (!email.includes('@') || !email.includes('.com')) {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  next();
+};
+
+const validarPassword = (req, res, next) => {
+  const {
+    password,
+  } = req.body;
+  if (!password) {
+    return res.status(400).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+  if (password.toString().length < 6) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+  next();
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.post('/login', validarEmail, validarPassword, (req, res) => {
+  res.status(200).json({
+    token: gerarToken(),
+  });
+});
+
 app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
+  const {
+    id,
+  } = req.params;
   const resposta = await readFile(talker);
-  
+
   const findId = resposta.find((a) => a.id.toString() === id);
 
   if (!findId) {
-    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
   }
   return res.status(200).json(findId);
 });
