9f503c23fc29ac6f603f2a1ca1803cdd235ef9fa
Victor Lopes <victor.cabrera0209@gmail.com>
2021-08-31 15:45:55 -0300

    Second  Requirement

--- middlewares.js
+++ middlewares.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
 
-module.exports = async (_req, res) => {
+const readFile = async (_req, res) => {
   const content = await fs.readFile('./talker.json', 'utf-8');
   if (content.length === 0) {
     return res.status(200).json([]);
@@ -8,3 +8,16 @@ module.exports = async (_req, res) => {
   const contentInJSON = JSON.parse(content);
   return res.status(200).json(contentInJSON);
 };
+
+const readFileId = async (req, res) => {
+  const { id } = req.params;
+  const content = await fs.readFile('./talker.json', 'utf-8');
+  const contentInJSON = JSON.parse(content);
+  const foundTalker = contentInJSON.find((eachTalker) => eachTalker.id === Number(id));
+  if (!foundTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(foundTalker);
+};
+
+module.exports = { readFile, readFileId };
--- router.js
+++ router.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const readFile = require('./middlewares');
+const { readFile } = require('./middlewares');
+const { readFileId } = require('./middlewares');
 
 const router = express.Router();
 
 router.get('/', readFile);
+
+router.get('/:id', readFileId);
 
 module.exports = router;
