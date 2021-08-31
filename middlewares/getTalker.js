18996126362683a7e3a6e0de38d14e02da9d6d48
Caio <thehaseol@gmail.com>
2021-08-31 17:56:37 -0300

    Fixing and req 7

--- middlewares/getTalker.js
+++
@@ -1,7 +0,0 @@
-/* const { readFile } = require('../fs-utils');
-const getTalker = async (req, res) => {
-  const talkersList = await readFile();
-  const { q } = req.query;
-};
-module.exports = getTalker;
- */
