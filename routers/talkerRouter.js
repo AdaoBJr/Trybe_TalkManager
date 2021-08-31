c07fcdeeb6e38e4acd8ab2ba35e93cb5c1b396e8
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-31 15:07:54 -0300

    cria um get que retorna o conteudo do arquivo talker.json

---
+++ routers/talkerRouter.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const { readContentFile } = require('../helpers/readWriteFile');
+
+const PATH_FILE = './talker.json';
+
+router.get('/', async (req, res) => {
+  const talkers = await readContentFile(PATH_FILE) || [];
+
+  res.status(200).json(talkers);
+});
+
+module.exports = router;
