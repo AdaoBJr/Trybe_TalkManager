745fd60b03f53a1e709662b5f9eba3199cec8e33
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-08-31 18:40:34 -0300

    desafio 4 feito

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -27,4 +27,107 @@ router.get('/:id', (req, res) => {
     return res.status(200).json(filtered);
   });
 });
+
+const verifyToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  if (authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const verifyName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name || name.length === 0) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+ }
+ if (name.length < 3) {
+   return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+ }
+ next();
+};
+
+const verifyAge = (req, res, next) => {
+  const { age } = req.body;
+  if (!age || age.length === 0) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+const verifyWathedAt = (req, res, next) => {
+  const { talk } = req.body;
+  const { watchedAt } = talk;
+  const dateRegex = new RegExp(/\d{2}\/\d{2}\/\d{4}/);
+
+  if (!watchedAt) {
+    return res.status(400).json({
+        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios', 
+    });
+}
+
+if (!dateRegex.test(watchedAt)) {
+    return res.status(400)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+}
+next();
+};
+
+const verifyRate = (req, res, next) => {
+  const { talk } = req.body;
+  const { rate } = talk;
+
+  if (!rate) {
+    return res.status(400).json({
+        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios', 
+    });
+}
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+   }
+
+  next();
+};
+
+const verifyTalk = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk) {
+      return res.status(400).json({ 
+          message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios', 
+      });
+  }
+
+  return next();
+};
+
+router.post('/',
+ verifyToken, 
+ verifyName, 
+ verifyAge, 
+ verifyTalk, 
+ verifyWathedAt, 
+ verifyRate,
+ (req, res) => {
+  fs.readFile(arquivo, 'utf8')
+  .then((info) => JSON.parse(info))
+  .then((info) => {
+    const data = info;
+    const newTalker = {
+      ...req.body,
+      id: info.length + 1,
+    };
+    data.push(newTalker);
+    fs.writeFile('./talker.json', JSON.stringify(data));
+    return res.status(201).json({ ...newTalker });
+  })
+.catch((err) => res.status(400).json(err));
+});
 module.exports = router;
