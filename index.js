36b3597bdf5e849e5f4c20ed10131c7d9b74b097
Johnny batata <johnny.arguello007@gmail.com>
2021-08-31 18:06:31 -0300

    requirement 1

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const talker = require('./talker.json'); 
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -14,4 +15,9 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', (req, res) => {
+  if (talker.length === 0) return res.status(200).json([]);
+  return res.status(200).json(talker);
 });
