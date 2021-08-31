6cbc19819d4c2b411810f20ab28c2c3885c72a0f
Henrique 'Rikos' Zózimo <hzozimo@gmail.com>
2021-08-31 16:37:16 -0300

    desafio 2 feito

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -16,4 +16,15 @@ router.get('/', (_req, res) => {
   })
 .catch((err) => { res.status(400).json(err); });
 });
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  fs.readFile(arquivo, 'utf8')
+  .then((info) => JSON.parse(info))
+  .then((info) => {
+    const filtered = info.find((item) => item.id === Number(id));
+    if (!filtered) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    return res.status(200).json(filtered);
+  });
+});
 module.exports = router;
