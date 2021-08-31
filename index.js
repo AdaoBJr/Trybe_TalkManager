a68f6fdeef0bfc12e723e499a17a74befc410277
Abimaelr <abimaelr.albu@gmail.com>
2021-08-31 16:36:32 -0300

    Talkers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const file = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +12,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_request, response) => {
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    response.status(HTTP_OK_STATUS).send(data);
+  } catch (err) {
+    response.status(200).send([]);
+  }
 });
 
 app.listen(PORT, () => {
