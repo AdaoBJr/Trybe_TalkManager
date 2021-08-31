73b2468c193ac7a80edac10dd8efe86710e3d883
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-31 18:30:55 -0300

    implementando o endpoint POST /talker

--- talkerRoutes.js
+++ talkerRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { readTalkerFile } = require('./utils');
+const { readTalkerFile, validateToken,
+  validateName, validateAge, validateTalk, writeTalkerFile } = require('./utils');
 
 const HTTP_OK_STATUS = 200;
+const HTTP_CREATED_STATUS = 201;
 const HTTP_NOT_FOUND_STATUS = 404;
 const router = express.Router();
 
@@ -22,5 +24,38 @@ router.get('/', (req, res) => {
   const talkers = readTalkerFile();
   return res.status(HTTP_OK_STATUS).json(talkers);
 });
+
+router.post(
+  '/',
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  (req, res) => {
+    const { name, age, talk } = req.body;
+    const talkers = readTalkerFile();
+
+    const lastElementIndex = talkers.length - 1;
+    const id = talkers[lastElementIndex].id + 1;
+
+    const newTalker = { id, name, age, talk };
+    const newTalkers = [...talkers, newTalker];
+
+    writeTalkerFile(JSON.stringify(newTalkers));
+
+    return res.status(HTTP_CREATED_STATUS).json(newTalker);
+  },
+);
+
+/*
+{
+  "name": "Danielle Santos",
+  "age": 56,
+  "talk": {
+    "watchedAt": "22/10/2019",
+    "rate": 5
+  }
+}
+*/
 
 module.exports = router;
