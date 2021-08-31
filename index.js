37271848ebb9c79e2183b25bdbc8328ed1e079ac
hugolfsimoes <hugolfsimoes@gmail.com>
2021-08-31 15:34:09 -0300

    feito desafio numero 2 do projeto

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ app.get('/talker', async (_req, res) => {
   return res.status(200).send(content);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const content = (await readContentFile()) || [];
+
+  const talker = await content.find((talk) => talk.id === Number(id));
+
+  if (!talker) {
+    return res.status(404).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  }
+
+  return res.status(200).json(talker);
+});
+
 app.listen(PORT, () => {
   console.log('Online');
 });
