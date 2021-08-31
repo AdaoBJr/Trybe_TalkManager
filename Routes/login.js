54f47519a2c024790bde94208de96b93f411d521
riquee <strikedv@hotmail.com>
2021-08-31 13:47:35 -0300

    req3 finalizado

--- Routes/login.js
+++ Routes/login.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const crypto = require('crypto');
+const { validateLogin } = require('../middlewares');
 
 const router = express.Router();
+
+router.post('/login', validateLogin, (req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  res.status(200).json({ token });
+});
 
 module.exports = router;
