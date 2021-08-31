49ab69f7e3d4fcac3f3bf06b32f09c27ee9c54d7
joao966 <joaooliveira966@gmail.com>
2021-08-31 18:48:12 -0300

    FIX: teste avaliador one-one

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { talkerSearch, palestrantes, talkerId, loguinToken,
-  post, putFunction, exclude } = require('./middlewares');
+const { palestrantes } = require('./middlewares/index');
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,25 +15,25 @@ app.get('/', (_request, response) => {
 });
 
 // desafio7
-app.get('/talker/search', talkerSearch);
+// app.get('/talker/search', talkerSearch);
 
 // // desafio 1
 app.get('/talker', palestrantes);
 
 // // desafio2
-app.get('/talker/:id', talkerId);
+// app.get('/talker/:id', talkerId);
 
 // // desafio3
-app.post('/loguin', loguinToken);
+// app.post('/loguin', loguinToken);
 
 // // desafio4
-app.post('/talker', post);
+// app.post('/talker', post);
 
 // // desafio5
-app.put('/talker/:id', putFunction);
+// app.put('/talker/:id', putFunction);
 
 // // desafio6
-app.delete('/talker/:id', exclude);
+// app.delete('/talker/:id', exclude);
 
 app.listen(PORT, () => {
   console.log('Online');
