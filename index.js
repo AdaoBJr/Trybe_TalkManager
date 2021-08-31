2ded4f2e53bb9b23b174ee4f55e11a86660794e4
RafaMaths <rmathscoach@gmail.com>
2021-08-30 21:14:25 -0300

    requirement 1: DONE

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+fs.readFileSync('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  fs.readFile('./talker.json', (err, content) => {
+    if (err) {
+      return res.status(200).json([]);
+    }
+    res.status(200).json(JSON.parse(content.toString('utf8')));
+  });
 });
 
 app.listen(PORT, () => {
