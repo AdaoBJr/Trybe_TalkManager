a38c8a7c358f5129440ea3b5fdd2a287fcb7e61f
EderPaiva <edergomesdepaiva12@gmail.com>
2021-08-31 17:07:52 -0300

    primeiro requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs').promises;
-const util = require('util');
+// const util = require('util');
 const express = require('express');
 const bodyParser = require('body-parser');
 // const { json } = require('body-parser');
