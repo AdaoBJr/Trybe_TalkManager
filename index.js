17a54f98b711b2773236f661e10cc389622006cd
Roger Franco <rogerfrancogalt@gmail.com>
2021-08-31 16:42:04 -0400

    req 1

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs'); // importei
+const { promisify } = require('util'); // 
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+/* ==================== */
+app.get('/talker', (req, res) => {
+  fs.readFile('/talker.json', 'utf-8', promisify((err, content) => {
+    if (err) {
+      res.status(400).send({ message: 'Not found' });
+      return;
+    }
+    res.send(200).send(content);
+  }));
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
