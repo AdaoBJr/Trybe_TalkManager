4e3fa536ca1f6bf5459782a7cca26ae5004c3847
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-08-30 21:16:38 -0300

    add exercicio 3 - problema no primeiro item - token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { readFile } = require('./readFile');
+const { validateEmail, validatePassword } = require('./validateUser');
 
 const app = express();
 app.use(bodyParser.json());
@@ -31,6 +32,15 @@ app.get('/talker/:id', async (req, res) => {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   return res.status(200).json(talker);
+});
+
+app.post('/login', validateEmail, validatePassword, (req, res) => {
+  const token = req.headers.authorization;
+
+  // if (!token || token.length === 16) {
+  //   return res.status(401).json({ message: 'Não autorizado(a)' });
+  // }
+  return res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
