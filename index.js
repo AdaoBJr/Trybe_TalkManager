d8b8c2afc3b9a30cfcc8ef8a799d2fdac628e611
Mariana Savoldi <marianasavodi123@gmail.com>
2021-08-31 17:08:10 -0300

    Requisito 1 teste

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,16 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// https://stackoverflow.com/questions/17604866/difference-between-readfile-and-readfilesync
+app.get('/talker', (_req, res) => {
+  try {
+    const talkers = fs.readFileSync('talker.json');
+    res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
+  } catch (error) {
+    res.status(HTTP_OK_STATUS).json(JSON.parse([]));
+  }
 });
 
 app.listen(PORT, () => {
