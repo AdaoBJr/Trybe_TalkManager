0f4b012cfed6cf99ff3a4f58aa4d83bd9c17f4bf
Fabio Marturano <fabiocmjor@gmail.com>
2021-08-31 13:42:42 -0300

    Req 2

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -17,4 +17,17 @@ router.get('/', async (req, res) => {
   return res.status(200).json(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  const talkers = await readFilePromise('talker.json')
+  .then((content) => JSON.parse(content))
+  .catch((err) => console.log(`erro: ${err.message}`));
+
+  const talker = talkers.find((talk) => talk.id === +id);
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talker);
+});
+
 module.exports = router;
