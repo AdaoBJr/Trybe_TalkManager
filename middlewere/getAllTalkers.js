10876be1797f616882d2572d3e7ba43f06336bac
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-08-31 16:42:03 -0300

    requisito 1

---
+++ middlewere/getAllTalkers.js
@@ -0,0 +1,10 @@
+const fs = require('fs').promises;
+
+const arquivo = './talker.json';
+
+const getAllTalkers = (_req, res, _next) => {
+fs.readFile(arquivo, 'utf8')
+  .then((data) => res.status(200).send(JSON.parse(data)));
+};
+
+module.exports = getAllTalkers;
