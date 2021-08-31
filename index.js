233799dcbebb3b69bd4269e1376e59fdef76f587
EderPaiva <edergomesdepaiva12@gmail.com>
2021-08-31 17:05:05 -0300

    requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const fs = require('fs').promises;
+const util = require('util');
 const express = require('express');
 const bodyParser = require('body-parser');
+// const { json } = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +10,19 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+async function getTalker() {
+  const resConvert = await fs.readFile('./talker.json', 'utf-8');
+  return JSON.parse(resConvert);
+}
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK_STATUS).send('Diretorio Raiz');
+});
+// está escutando as requisições feitas pelos usuários.
+app.get('/talker', async (_request, response) => {
+  const talker = await getTalker();
+  response.status(HTTP_OK_STATUS).send(talker);
 });
 
 app.listen(PORT, () => {
