5b356ba922ed27ef250d7a1436d3e28c34eee017
joao966 <joaooliveira966@gmail.com>
2021-08-31 19:32:23 -0300

    FIX: request-3

--- middlewares/MiddleLoguin.js
+++ middlewares/MiddleLoguin.js
@@ -23,7 +23,7 @@ const loguin = (req, res) => {
   if (!emailIsValid) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
-  if (!passwordLength) {
+  if (password <= passwordLength) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
