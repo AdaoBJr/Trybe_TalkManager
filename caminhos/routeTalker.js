cafc0778aa6fc808684b7a0add65439f254376b2
Daniel <darkdanielr@gmail.com>
2021-08-31 19:55:22 -0300

    desafio4

--- caminhos/routeTalker.js
+++ caminhos/routeTalker.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
-const router = express.Router();
 const fs = require('fs').promises;
+const { autoriza, data, idade, nome, talkExist } = require('../middleware/talkPost.js');
+
+const router = express.Router();
 
 const readFile = () => {
     const file = fs.readFile('./talker.json', 'utf8')
@@ -21,6 +23,29 @@ router.get('/:id', async (req, res) => {
   const filtered = readF.find((resp) => Number(resp.id) === Number(id));
   if (!filtered) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   res.status(200).json(filtered);
+});
+
+// adicionar pessoa abaixo
+
+router.post('/',
+  autoriza,
+  nome,
+  idade,
+  talkExist,
+  data,
+  async (req, res) => {
+    const file = await readFile();
+    const id = file.length + 1;
+    const { name, age, talk } = req.body;
+    const result = {
+      id,
+      name,
+      age,
+      talk,
+    };
+    const newTalkers = JSON.stringify([...file, result]);
+    await fs.writeFile('talker.json', newTalkers);
+    res.status(201).json(result);
 });
 
 module.exports = router;
