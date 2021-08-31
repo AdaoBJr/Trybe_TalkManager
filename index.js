00920321dd99bd8a1dbf6c5a52cf86a12ff11909
Leandro Reis <lleandrofr@gmail.com>
2021-08-31 14:14:40 -0300

    req 3 - creates the POST /login endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
@@ -28,6 +29,9 @@ async function readFile(path) {
 
 app.get('/talker', async (_req, res) => {
   const talkers = await readFile('./talker.json');
+  if (!talkers) {
+    return res.status(200).send([]);
+  }
   return res.status(HTTP_OK_STATUS).json(talkers);
 });
 
@@ -43,3 +47,42 @@ app.get('/talker/:id', async (req, res) => {
   }  
   return res.status(HTTP_OK_STATUS).send(talker);
 });
+
+function emailValidation(email) {
+  const validation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return validation.test(String(email).toLowerCase());
+}
+// reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+
+function validateEmail(req, res, next) {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailValidation(email)) {
+    return res.status(400)
+    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+   next();
+}
+
+function validatePassword(req, res, next) {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.toString().length < 6) {
+    return res.status(400)
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  next();
+}
+
+app.post('/login',
+  validateEmail,
+  validatePassword,
+  (_req, res) => {
+    const token = crypto.randomBytes(8).toString('hex');
+    return res.status(200).json({ token: `${token}` });
+  });
