13043ee197d16aab6dc1d7b2f08b2a4ca8b37e9d
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-08-31 15:49:06 -0300

    requisito 2

--- Route/talkerRoutes.js
+++ Route/talkerRoutes.js
@@ -9,8 +9,12 @@ router.get('/', async (req, res) => {
   return res.status(HTTP_OK_STATUS).json(fileContent);
 });
 
-// router.get('/:id', (req, res) => {
-//   return res.send('tetste');
-// });
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await utils.readFile('./talker.json');
+  const talker = talkers.find((el) => el.id === Number(id));
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talker);
+});
 
 module.exports = router;
--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs').promises;
 
 async function readFile(filename) {
-  const contentFile = JSON.parse(await fs.readFile(filename, 'utf-8'));
+  const contentFile = await fs.readFile(filename, 'utf-8')
+    .then((content) => JSON.parse(content));
   if (contentFile.length > 0) return contentFile;
   return [];
 }
