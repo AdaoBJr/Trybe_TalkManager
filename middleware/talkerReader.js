db4f30681de04f0b7d0f8154c32dda5e39c20c8d
Carlos <cemargato@gmail.com>
2021-08-31 18:28:42 -0300

    requisito-2

--- middleware/talkerReader.js
+++ middleware/talkerReader.js
@@ -13,4 +13,14 @@ router.get('/', async (req, res) => {
 return res.status(200).send(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+const { id } = req.params;
+const file = await fileReader();
+const talker = file.find((item) => item.id === +id);
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(200).json({ talker }); 
+});
+
 module.exports = router;
