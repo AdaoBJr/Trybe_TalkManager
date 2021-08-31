625e2675e5d257f12f812e18c20e750f28b4467d
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-08-31 15:45:45 -0300

    exclui funcao

--- setFileSeed.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs').promises;
-
-async function setFileSeed(newUser) {
-    await fs.writeFile('./seed.json', JSON.stringify(newUser));
-}
-
-module.exports = {
-  setFileSeed,
-};
