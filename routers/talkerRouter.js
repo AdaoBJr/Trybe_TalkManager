8817c925a5e5aa7c368e252bc2759ee1c9cd3515
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-31 15:14:05 -0300

    cria um endpoint que retorna o talker pelo seu id

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -4,10 +4,21 @@ const { readContentFile } = require('../helpers/readWriteFile');
 
 const PATH_FILE = './talker.json';
 
-router.get('/', async (req, res) => {
+router.get('/', async (_req, res) => {
   const talkers = await readContentFile(PATH_FILE) || [];
 
   res.status(200).json(talkers);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readContentFile(PATH_FILE) || [];
+
+  const talkerId = talkers.find((talker) => talker.id === Number(id));
+
+  if (!talkerId) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(200).json(talkerId);
 });
 
 module.exports = router;
