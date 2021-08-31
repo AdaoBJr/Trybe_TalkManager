a8571daa15f7b6eb56c4a541f59df929ccc83637
root <diegoserafim1@gmail.com>
2021-08-31 18:38:43 -0300

    D3 Cria o endpoint POST /login

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (_req, res) => {
   res.status(HTTP_OK_STATUS).json(JSON.parse(await fs.readFile('./talker.json', 'utf-8')));
 });
-
 // #2 Cria o endpoint GET /talker/:id
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
@@ -29,8 +28,28 @@ app.get('/talker/:id', async (req, res) => {
   if (!talk) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
-  return res.status(200).json(talk);
+  return res.status(HTTP_OK_STATUS).json(talk);
 });
+// #3 Cria o endpoint POST /login
+app.post('/login', (req, res) => {
+  const emailVallid = (email) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email);
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailVallid(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  const Rand = () => Math.random(0).toString(36).substr(2);
+  const token = (length) => (Rand() + Rand()).substr(0, length);
+  return res.status(HTTP_OK_STATUS).json({ token: token(16) });
+  });
 
 app.listen(PORT, () => {
   console.log('Online');
