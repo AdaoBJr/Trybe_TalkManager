1554d055425ea2bb40ac7ead538a0f7744d97222
André Arnoni <andre_arnoni@yahoo.com.br>
2021-08-31 16:29:40 -0300

    executado requisito 1

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get('/talker', (_req, res) => {
+  try {
+    const data = fs.readFileSync('./talker.json', 'utf8');
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(200).send([]);
+  }
+});
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
