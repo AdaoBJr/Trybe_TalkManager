fcb1ab6112d9df8610e070fd7ce0543427cd1ec7
Fernando <fhomaia@gmail.com>
2021-08-31 19:15:59 -0300

    fix req 3

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ const verifyEmail = (req, res, next) => {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!email.includes('@') || !email.includes('.com')) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato email@email.com' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   next();
 };
