5d24f82f02e22268ad3de9f9d4eff92aeda3280c
Mariana Mohr <marianamohr@gmail.com>
2021-08-31 18:45:42 -0300

    test requisito 4

--- routers/TalkersRouter.js
+++ routers/TalkersRouter.js
@@ -6,6 +6,62 @@ const fs = require('fs').promises;
 
 const HTTP_OK_STATUS = 200;
 
+const testToken = (req, res, next) => {
+    const { authorization } = req.headers;
+    if (!authorization) res.status(401).send({ message: 'Token não encontrado' });
+    if (authorization.length < 16) res.status(401).send({ message: 'Token inválido' });
+    next();
+};
+
+const testUserName = (req, res, next) => {
+    const { name } = req.body;
+    if (!name) res.status(400).send({ message: 'O campo "name" é obrigatório' });
+    if (name.length <= 3) {
+        res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+    }
+    next();
+};
+
+const testUserAge = (req, res, next) => {
+    const { age } = req.body;
+    if (!age || age.length === 0) {
+        res.status(400).send({ message: 'O campo "age" é obrigatório' });
+    }
+    if (Number(age) <= 18) {
+        res.status(400).send({ message: 'A pessoa palestrante deve ser maior de idade' });
+    }
+    next();
+};
+
+const testUserTalk = (req, res, next) => {
+    const { talk } = req.body;
+    if (!talk || talk.watchedAt.length === 0 || talk.rate.length === 0) {
+        res.status(400).send(
+            {
+                message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+            },
+        );
+    }
+    next();
+};
+
+const testUserTalkData = (req, res, next) => {
+    const { talk } = req.body;
+    const dataRegex = new RegExp(/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i);
+    if (!dataRegex.test(talk.watchedAt)) {
+        res.status(400).send({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+    }
+    next();
+};
+
+const testUserTalkRate = (req, res, next) => {
+    const { talk } = req.body;
+    if (talk.rate <= 0 || talk.rate >= 6) {
+        res.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    }
+    next();
+};
+
 const getTalkers = async () => {
     const talkers = await fs.readFile('./talker.json', 'utf8');
     return JSON.parse(talkers);
@@ -21,9 +77,20 @@ router.get('/:id', async (req, res) => {
     const talkers = await getTalkers();
     const talker = talkers.find((talk) => talk.id === Number(id));
     if (!talker || talker === undefined) {
-    res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+        res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
     }
     res.status(200).json(talker);
 });
+router.post('/',
+    testToken,
+    testUserName,
+    testUserAge,
+    testUserTalk,
+    testUserTalkData,
+    testUserTalkRate,
+    (req, res) => {
+        const { name, age, talk } = req.body;
+        res.status(201).send({ name, age, talk });
+    });
 
 module.exports = router;
