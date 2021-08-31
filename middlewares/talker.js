c9a55850dac3f54c3f4053b8582c41369b0ee14a
Caio <thehaseol@gmail.com>
2021-08-31 14:31:50 -0300

    Fix lint

--- middlewares/talker.js
+++ middlewares/talker.js
@@ -4,8 +4,8 @@ const talkersUtils = require('../utils.js');
 const HTTP_OK_STATUS = 200;
 
 const talker = rescue(async (req, res) => {
-    const talkersList = await talkersUtils.readFile();
-    res.status(HTTP_OK_STATUS).json(talkersList);
-  });
+  const talkersList = await talkersUtils.readFile();
+  res.status(HTTP_OK_STATUS).json(talkersList);
+});
 
 module.exports = talker;
