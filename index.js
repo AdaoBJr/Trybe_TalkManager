20df49cb75d5c70853985413ba81743f4c9d8ad2
Thiago Kachinsky <tjk091@gmail.com>
2021-08-31 17:14:41 -0300

    [Thiago Kachinsky] - Projeto Talker Manager

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { readFileTalker } = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// 1
+app.get('/talker', (req, res) => {
+  const getAllTalkers = await readFileTalker();
+  return res.status(200).send(getAllTalkers);
 });
 
 app.listen(PORT, () => {
---
+++ middlewares.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const talker = require('./talker.json');
+
+const readFileTalker = async () => {
+  const talkesList = await fs.readFile(talker, 'utf-8');
+  return JSON.parse(talkesList);
+}
+
+module.exports = {
+  readFileTalker,
+}
