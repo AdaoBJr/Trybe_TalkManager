ec735c8411485c147fb6d052c8984ed664fca656
Thiago Kachinsky <tjk091@gmail.com>
2021-08-31 18:20:04 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { readFileTalker } = require('./middlewares');
+const { readFileTalker, getTalkerById } = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// 2
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkerId = await getTalkerById(id);
+
+  if (talkerId) {
+    return res.status(200).json(talkerId);
+  }
+  
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
 
 // 1
 app.get('/talker', async (req, res) => {
--- middlewares.js
+++ middlewares.js
@@ -3,10 +3,16 @@ const fs = require('fs').promises;
 const talker = './talker.json';
 
 const readFileTalker = async () => {
-  const talkesList = await fs.readFile(talker, 'utf-8');
-  return JSON.parse(talkesList);
+  const talkerList = await fs.readFile(talker, 'utf-8');
+  return JSON.parse(talkerList);
+};
+
+const getTalkerById = async (id) => {
+  const talkerList = await readFileTalker();
+  return talkerList.find((obj) => obj.id === Number(id));
 };
 
 module.exports = {
   readFileTalker,
+  getTalkerById,
 };
