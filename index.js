5334f0691327bf66e57552c1e93ee871c9ef8b1e
Pedro Amaral <amaralph1995@gmail.com>
2021-08-31 16:14:29 -0300

    Cria endpoint /talker que le json talker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,15 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  try {
+    const talkerData = fs.readFileSync('talker.json');
+    res.status(200).json(JSON.parse(talkerData));
+  } catch (err) {
+    res.status(200).json(JSON.parse([]));
+  }
 });
 
 app.listen(PORT, () => {
