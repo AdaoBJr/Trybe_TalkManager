073e8263ca7ee6e91f6cad8b50a70592854c7907
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-08-31 19:19:47 -0300

    Resolve validation

--- validations.js
+++ validations.js
@@ -88,17 +88,11 @@ const validateTalk = (req, res, next) => {
 
 const validateTalk2 = (req, res, next) => {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt) {
+  if (!talk || !talk.watchedAt || !talk.rate) {
     return res.status(400).json({
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
       });
     }
-    if (!talk.rate) {
-      return res.status(400).json({
-          message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-        });
-      }
-
     next();
 };
 
