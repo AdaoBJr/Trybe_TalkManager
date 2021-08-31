65f52686140315d9b4fd5e70aa35bf6f4375b931
renanpbraga <renanpbraga@gmail.com>
2021-08-31 20:23:00 -0300

    adiciona req 3, corrige erros de lint

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { validateEmail, validatePassword, createToken } = require('./middleware/validations');
-
 const fs = require('fs');
+const { validateEmail, validatePassword, createToken } = require('./middleware/validations');
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,7 +16,7 @@ app.get('/', (_request, response) => {
 
 app.get('/talker', (_req, res) => {
   fs.readFile('./talker.json', 'utf-8', (err, content) => {
-    if(err){
+    if (err) {
       return res.status(200).json([]);
     }
     const data = JSON.parse(content);
@@ -28,25 +27,23 @@ app.get('/talker', (_req, res) => {
 app.get('/talker/:id', (req, res) => {
   fs.readFile('./talker.json', 'utf-8', (_err, content) => {
     const data = JSON.parse(content);
-    const findId = data.find(({id}) => id === Number(req.params.id));
-    if(!findId){
+    const findId = data.find(({ id }) => id === Number(req.params.id));
+    if (!findId) {
       return res.status(404).json(
         {
-          "message": "Pessoa palestrante não encontrada"
-        }
+          message: 'Pessoa palestrante não encontrada',
+        },
       );
     }
     return res.status(200).json(findId);
   });
 });
 
-app.post('/login', validateEmail, validatePassword, (_req, res) => {
-  return res.status(200).json(
-    {
+app.post('/login', validateEmail, validatePassword, (_req, res) => res.status(200).json(
+  {
     token: createToken(),
-    }
-  );
-});
+  },
+  ));
 
 app.listen(PORT, () => {
   console.log('Online');
--- middleware/validations.js
+++ middleware/validations.js
@@ -1,60 +1,52 @@
+const crypto = require('crypto');
+
 const validateEmail = (req, res, next) => {
-  const { email } = req.body;
-  const validEmail = email.match(/[a-z]+@[a-z]+.com/g);
-  if(!email || email === '') {
+  const { email = '' } = req.body;
+  if (!email || email === '') {
     return res.status(400).json(
       {
         message: 'O campo "email" é obrigatório',
       },
-    );
-  };
-  if(!validEmail){
+      );
+    }
+  const validEmail = email.match(/[a-z]+@[a-z]+.com/g);
+  if (!validEmail) {
     return res.status(400).json(
       {
-        message: "O \"email\" deve ter o formato \"email@email.com\""
+        message: 'O "email" deve ter o formato "email@email.com"',
       },
     );
-  };
+  }
   next();
 };
 
 const validatePassword = (req, res, next) => {
-  const { password } = req.body;
-  if(password.length < 6 && password.length !== 0){
+  const { password = '' } = req.body;
+  if (password.length < 6 && password.length !== 0) {
     return res.status(400).json(
       {
-        message: "O \"password\" deve ter pelo menos 6 caracteres"
+        message: 'O "password" deve ter pelo menos 6 caracteres',
       },
     );
-  };
-  if(!password || password === ''){
+  }
+  if (!password || password === '') {
     return res.status(400).json(
       {
-        message: "O campo \"password\" é obrigatório"
+        message: 'O campo "password" é obrigatório',
       },
     );
-  };
+  }
   next();
 };
 
-const tokenize  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIFJKLMNOPQRSTUVWXYX1234567890";
-function shuffleWord (Word){
-  let shuffledWord = '';
-  Word = tokenize.split('');
-  while (Word.length > 0) {
-    shuffledWord +=  Word.splice(Word.length * Math.random() << 0, 1);
-  }
-  return shuffledWord;
-}
-
 const createToken = () => {
-  const tokenEmbaralhado = shuffleWord();
-  const token = tokenEmbaralhado.slice(0,16);
-  return token;
-}
+  const token = crypto.randomBytes(8).toString('hex');
+  console.log(token);
+  return token; 
+};
 
 module.exports = {
   validateEmail,
   validatePassword,
   createToken,
-}
+};
