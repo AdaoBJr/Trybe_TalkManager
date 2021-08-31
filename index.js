9ac1db9bb8c03864411fd6a708eefc589be4cbe5
esionascimento <esio.392@protonmail.com>
2021-08-31 14:38:37 -0400

    concluded desafio 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,3 +16,14 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+async function req(_req, res) {
+  const response = await fs.readFile('./talker.json', 'UTF-8');
+  if (!response) {
+    return res.status(HTTP_OK_STATUS).json([]);
+  }
+  const data = JSON.parse(response);
+  return res.status(HTTP_OK_STATUS).json({ data });
+}
+
+app.get('/talker', req);
