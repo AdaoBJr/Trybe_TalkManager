9262033b34a026e6dca4bfb95bb4956fe7406b95
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-08-31 16:57:19 -0300

    enpoint de login com validador e token

--- fs-utils.js
+++ fs-utils.js
@@ -1,8 +1,18 @@
 const fs = require('fs').promises;
+const crypto = require('crypto-js');
 
 function getTalkers() {
   return fs.readFile('./talker.json', 'utf-8')
     .then((fileContent) => JSON.parse(fileContent));
 }
 
-module.exports = getTalkers;
+// https://stackoverflow.com/questions/48524452/base64-encoder-via-crypto-js
+function generateToken(myString) {
+  const encodedWord = crypto.enc.Utf8.parse(myString); // encodedWord Array object
+  const encoded = crypto.enc.Base64.stringify(encodedWord); // string: 'NzUzMjI1NDE='
+  return encoded;
+}
+
+console.log(generateToken(Math.random().toFixed(8)));
+
+module.exports = { getTalkers, generateToken };
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises;
+const Base64 = require('crypto-js/enc-base64');
 
-const getTalkers = require('./fs-utils');
+const { getTalkers, generateToken } = require('./fs-utils');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const NOT_FOUND_STATUS = 404;
+const HTTP_BAD_REQUEST = 400;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -35,4 +36,30 @@ app.get('/talker/:id', async (req, res) => {
   }
 
   return res.status(HTTP_OK_STATUS).json(talkerById);
+});
+
+function validateEmail(email) {
+  const valid = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  return valid.test(String(email).toLowerCase());
+}
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email) {
+    return res.status(HTTP_BAD_REQUEST).json({ message: 'O campo "email" é obrigatório' });
+  } if (!validateEmail(email)) {
+    return res.status(HTTP_BAD_REQUEST).json({
+      message: 'O "email" deve ter o formato "email@email.com"', 
+    });
+  }
+  
+  if (!password) {
+    return res.status(HTTP_BAD_REQUEST).json({ message: 'O campo "password" é obrigatório' });
+  } if (password.length < 6) {
+    return res.status(HTTP_BAD_REQUEST).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres', 
+    });
+  }
+  const token = generateToken(Math.random().toFixed(8));
+  return res.status(HTTP_OK_STATUS).json({ token });
 });
