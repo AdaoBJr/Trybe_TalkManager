56a2a9b7c30e6393e68c32677c5119f8f51553cc
Caio <thehaseol@gmail.com>
2021-08-31 18:04:33 -0300

    Prettier me sabotou n.n

--- middlewares/deleteTalker.js
+++ middlewares/deleteTalker.js
@@ -5,9 +5,8 @@ const HTTP_OK_STATUS = 200;
 const deleteTalker = async (req, res) => {
   const { id: idDelete } = req.params;
   const talkersList = await readFile();
-  const newTalkerList = talkersList.filter(
-    ({ id }) => Number(id) !== Number(idDelete)
-  );
+  const newTalkerList = talkersList
+  .filter(({ id }) => Number(id) !== Number(idDelete));
   await writeFileTalker(newTalkerList);
   return res
     .status(HTTP_OK_STATUS)
--- middlewares/login.js
+++ middlewares/login.js
@@ -7,24 +7,18 @@ const login = (req, res) => {
   const { email, password } = req.body;
   const emailValidated = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
   if (!email) {
-    return res
-      .status(FAIL_STATUS)
-      .json({ message: 'O campo "email" é obrigatório' });
+    return res.status(FAIL_STATUS).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!emailValidated) {
-    return res
-      .status(FAIL_STATUS)
-      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+   return res
+     .status(FAIL_STATUS).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   if (!password) {
-    return res
-      .status(FAIL_STATUS)
-      .json({ message: 'O campo "password" é obrigatório' });
+    return res.status(FAIL_STATUS).json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
     return res
-      .status(FAIL_STATUS)
-      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+      .status(FAIL_STATUS).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   return res.status(HTTP_OK_STATUS).json({ token: generateToken() });
 };
