ea5b5bf997aa2b145ee2b0526ebaf57419e04dfe
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-08-31 18:26:23 -0300

    ate o req 2

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {
+  readContentFile,
+writeContentFile,
+} = require('./helpers');
+
+const PATH_FILE = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +16,26 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar!
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// REQUISITO 1
+app.get('/talker', async (req, res) => {
+  const talkers = await readContentFile(PATH_FILE) || [];
+
+  res.status(200).json(talkers);
+});
+
+// REQUISITO 2
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readContentFile(PATH_FILE) || [];
+  const talker = talkers.find((i) => Number(i.id) === Number(id));
+
+  if (talker) {
+    res.status(200).json(talker);
+  } else {
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
 });
 
 app.listen(PORT, () => {
---
+++ talkerRoutes.js
@@ -0,0 +1,13 @@
+// const router = require('express').Router();
+// const {
+//     readContentFile,
+//   writeContentFile,
+// } = require('./helpers');
+
+// const PATH_FILE = './talker.json';
+
+// router.get('/talker', async (req, res) => {
+//   const talkers = await readContentFile(PATH_FILE);
+
+//   res.status(200).json(talkers);
+// });
