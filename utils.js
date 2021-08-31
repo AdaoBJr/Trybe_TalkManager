b61fa46e611af275e0b191455598f5a5cea9d387
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-08-31 15:24:50 -0300

    adicionando a função que valida o token

--- utils.js
+++ utils.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 
 const TALKER_JSON_PATH = 'talker.json';
 const HTTP_BAD_REQUEST_STATUS = 400;
+const HTTP_UNHAUTHORIZED_STATUS = 401;
 
 function readTalkerFile() {
   const contentFile = fs.readFileSync(TALKER_JSON_PATH, 'utf-8');
@@ -27,7 +28,7 @@ function validateEmail(req, res, next) {
 
 function validatePassword(req, res, next) {
   const { password } = req.body;
-  const blankSpacesRegex = /\s+/;
+  const blankSpacesRegex = /^\s+$/;
 
   if (blankSpacesRegex.test(password) || !password) {
     return res.status(HTTP_BAD_REQUEST_STATUS)
@@ -42,8 +43,26 @@ function validatePassword(req, res, next) {
   next();
 }
 
+function validateToken(req, res, next) {
+  const token = req.headers.authorization;
+  const invalidToken = /\w{16}/;
+
+  if (!token) {
+    return res.status(HTTP_UNHAUTHORIZED_STATUS)
+      .json({ message: 'Token não encontrado' });
+  }
+
+  if (!invalidToken.test(token)) {
+    return res.status(HTTP_UNHAUTHORIZED_STATUS)
+      .json({ message: 'Token inválido' });
+  }
+
+  next();
+}
+
 module.exports = {
   readTalkerFile,
   validateEmail,
   validatePassword,
+  validateToken,
 };
