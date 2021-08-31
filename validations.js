d99973a3efd1a052518b6a6820a5c17ba76bc4fc
Misael Bueno <misa.hbueno@gmail.com>
2021-08-31 19:05:34 -0300

    requisito 3 corrigido

--- validations.js
+++ validations.js
@@ -2,11 +2,11 @@ const validateEmail = (req, res, next) => {
   const { email } = req.body;
 
   if (!email) {
-    return res.status(400).json({ message: 'O campo email é obrigatório' });
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
 
   if (!email.includes('@') || !email.includes('.com')) {
-    return res.status(400).json({ message: 'O email deve ter o formato email@email.com' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
 
   next();
@@ -16,11 +16,11 @@ const validatePassword = (req, res, next) => {
   const { password } = req.body;
 
   if (!password) {
-    return res.status(400).json({ message: 'O campo password é obrigatório' });
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
 
   if (password.length < 6) {
-    return res.status(400).json({ message: 'O password deve ter pelo menos 6 caracteres' });
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
   next();
