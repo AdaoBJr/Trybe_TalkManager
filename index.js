1f9b1a498b4186ab3c4310c6cd826f4e401fcd56
Aremtior <marcosleandrofl@gmail.com>
2021-08-31 23:10:51 -0300

    requirement 2

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const talkerData = 'talker.json';
 
 async function readData() {
   try {
-    const data = await fs.readFile(talkerData, 'utf8');
+    const data = await fs.readFile(talkerData);
     return data;
   } catch (err) {
     console.error(`Não foi possível ler o arquivo ${talkerData}\n Erro: ${err}`);
-    process.exit(1);
+    // process.exit(1);
   }
 }
 
@@ -28,9 +28,19 @@ app.get('/', (_request, response) => {
 app.get('/talker', async (req, res) => {
   const talkers = await readData();
 
-  if (!talkers) return res.status(200).json({});
+  if (!talkers) return res.status(HTTP_OK_STATUS).json({});
 
-  res.status(200).json(talkers);
+  res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readData().json();
+  const talker = talkers.find((t) => t.id === parseInt(id, 10));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  res.status(HTTP_OK_STATUS).json(talker);
 });
 
 app.listen(PORT, () => {
