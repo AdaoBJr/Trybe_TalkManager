39e05c3fa8d58ce0ff71ea7f5c8cd98b60542da3
Diegho <dieghonm@gmail.com>
2021-09-01 19:11:42 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -6,10 +6,22 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 404;
 const PORT = '3000';
 
 app.get('/talker', async (req, res) => res.status(HTTP_OK_STATUS)
   .send(JSON.parse(await fs.readFile('talker.json', 'utf-8'))));
+
+app.get('/talker/:id', async (req, res) => { 
+  const { id } = req.params;
+  const DB = JSON.parse(await fs.readFile('talker.json', 'utf-8'));
+  const resp = DB.find((item) => item.id === +id);
+  console.log(resp);
+  if (!resp) {
+    return res.status(HTTP_ERROR_STATUS).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(HTTP_OK_STATUS).send(DB.find((item) => item.id === +id));
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
