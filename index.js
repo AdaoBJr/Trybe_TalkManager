935903e0b3ed632d4652ecf8f5d9bb9df04d051e
Cristian Bugs <cristian.bugs@gmail.com>
2021-09-01 03:37:37 -0300

    first and second task

--- index.js
+++ index.js
@@ -21,30 +21,6 @@ const talkerCaller = async () => {
   return JSON.parse(data);
 };
 
-// app.get('/talker/:id', (req, res) => {
-//   fs.readFile('./talker.json', 'utf-8', util.promisify((err, content) => {
-//     if (err) {
-//       return err;
-//     }
-//     const { id } = req.params;
-//     const data = content.find((c) => c.id === Number(id));
-//     return res.status(HTTP_OK_STATUS).json(data);
-//   }));
-// });
-
-// app.get('/talker', (req, res) => {
-//   fs.readFile('./talker.json', 'utf-8', util.promisify((err, content) => {
-//     if (err) {
-//       res.status(HTTP_OK_STATUS).send([]);
-//     }
-//     const data = JSON.parse(content);
-//     if (data.length === 0) {
-//       res.status(HTTP_OK_STATUS).json(Array.from([]));
-//     }
-//     res.status(HTTP_OK_STATUS).send(data);
-//   }));
-// });
-
 app.get('/talker/:id', async (req, res) => {
   const data = await talkerCaller();
   const { id } = req.params;
@@ -55,11 +31,16 @@ app.get('/talker/:id', async (req, res) => {
   res.status(HTTP_OK_STATUS).send(result);
 });
 
-app.get('/talker/', async (req, res) => {
+app.get('/talker', async (req, res) => {
   const data = await talkerCaller();
   if (data.length === 0) return res.status(HTTP_OK_STATUS).send([]);
   res.status(HTTP_OK_STATUS).send(data);
 });
+
+// app.post('/talker/', (req, res) => {
+//   const { name, age, talk, watchedAt, rate } = req.body;
+
+// });
 
 app.listen(PORT, () => {
   console.log('Online');
