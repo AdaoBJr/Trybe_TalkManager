c8a6262c05fd354a24f56c820e5150ea6e9c2b70
Víctor Cançado <victor.cancado@gmail.com>
2021-09-01 17:19:08 -0300

    Requisito 1 - passando nos testes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,9 +8,20 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// Olhei o código do colega Thiago Leite para fazer esta função
+const getTalkers = () => fs.readFile('./talker.json', 'utf-8').then((res) => JSON.parse(res));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const talkers = await getTalkers();
+  if (!talkers) {
+    return res.send([]);
+  }
+  return res.json(talkers);
 });
 
 app.listen(PORT, () => {
