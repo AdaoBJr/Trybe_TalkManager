95743c5bbe6b790a8085c1f4b49e351336ae2aeb
arthur menezes <arthurmcorrea282@gmail.com>
2021-09-01 14:58:29 -0300

    teste

--- index.js
+++ index.js
@@ -41,22 +41,15 @@ app.get(
 );
 
 app.get('/login', (req, res) => {
-  const token = { token: generateToken(16) };
-  
+  const token = generateToken(16);
   const { email, password } = req.body;
-  if (!email) {
-    res.status(400).send({ message: 'O campo "email" é obrigatório' });
-  }
-  const emailResult = validateEmail(email);
-  if (!emailResult) {
+  validateEmail(email, password, res);
+  const emailIsValid = /((\w+)@(\w+)\.(\w+))/;
+  if (!email.match(emailIsValid)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  if (!password) {
-    res.status(400).send({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < 6) {
-    console.log('entrou no lengu');
+  } if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
-  res.status(200).send(token);
+
+  return res.status(200).json({ token });
 });
--- token/email.js
+++ token/email.js
@@ -1,6 +1,11 @@
-function validateEmail(email) {
-  const re = /\S+@\S+\.\S+/;
-  return re.test(email);
-}
+const validateEmail = (email, password, res) => {
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+};
 
 module.exports = { validateEmail };
