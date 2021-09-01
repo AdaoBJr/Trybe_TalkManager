276149d9872045ecf2ef1a2cc0f8339f35470aca
Thiago Leite <leite.tjs@gmail.com>
2021-09-01 16:49:49 -0300

    fix 2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/talker/:id', async (req, res) => {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   
-  return res.status(HTTP_OK_STATUS).json(talkerFiltered);
+  return res.status(HTTP_OK_STATUS).json(...talkerFiltered);
 });
 
 app.get('/talker', async (req, res) => {
