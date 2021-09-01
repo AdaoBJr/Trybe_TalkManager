90598a52c4661123d334fbbf2a0a8fe730dfbf47
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-09-01 15:03:18 -0300

    Requisito 2 estruturado

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker/:id', (req, res) => {
+  const { id } = req.params;
+    const filterPeople = data.filter((people) => {
+      console.log(people.id);
+      return people.id === id;
+    });
+    if (filterPeople) return res.status(HTTP_OK_STATUS).json(filterPeople);
+});
+
 app.get('/talker', (_req, res) => {
   if (!data) return res.status(HTTP_OK_STATUS).json([]);
   return res.status(HTTP_OK_STATUS).json(data);
