603f0995829637c8db80d1deede7a653dc456ada
Cristian Bugs <cristian.bugs@gmail.com>
2021-09-01 15:21:00 -0300

    quotes on password and email

--- routes/middleware.js
+++ routes/middleware.js
@@ -18,12 +18,12 @@ const isValidEmail = (req, res, next) => {
   const { email } = req.body;
   
   if (email.length === 0) {
-   return res.status(400).json({ message: `O campo ${email} é obrigatório` });
+   return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
 
   if (!email.includes('@') || !email.includes('.com')) {
     return res.status(400)
-    .json({ message: `O campo ${email}" deve ter o formato email@email.com` });
+    .json({ message: 'O campo "email" deve ter o formato email@email.com' });
   }
     
   next();
@@ -34,11 +34,11 @@ const isValidEmail = (req, res, next) => {
     const passwordRegex = /^[0-9]*$/;
     
     if (password.length < 6) {
-    return res.status(400).json({ message: 'O password deve ter pelo menos 6 caracteres' });
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
 
     if (!password.match(passwordRegex)) {
-    return res.status(400).json({ message: 'O campo password é obrigatório' });
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
     }
     
     next();
