238dc5db76afe609238c5b91235627bbbaa51786
joao966 <joaooliveira966@gmail.com>
2021-08-31 23:00:52 -0300

    FIX: request-4

--- middlewares/MiddlePostTalker.js
+++ middlewares/MiddlePostTalker.js
@@ -3,14 +3,18 @@ const { validate } = require('./Validade');
 const { validateName } = require('./validadeName');
 const { validateRate } = require('./validateRateAndWitch');
 
+const verificadeRate = (rate, res) => {
+   if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+};
+
 const post = async (req, res) => {
   const { headers: { token }, body: { name, age, talk: { watchedAt, rate } } } = req;
   validate(token, res);
   validateName(name, age, res);
   validateRate(rate, watchedAt, res);
-  if (rate < 1 || rate > 5) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  }
+  verificadeRate(rate, res);
   const response = await fs.readFile('./talker.json', 'utf-8');
   const convert = JSON.parse(response);
   const newPalester = {
@@ -20,7 +24,7 @@ const post = async (req, res) => {
     talk: { watchedAt, rate } };
   convert.push(newPalester);
   await fs.writeFile('./talker.json', JSON.stringify(convert));
-  return res.status(200).json(newPalester);
+  // return res.status(201).json(newPalester);
 };
 
 module.exports = {
--- middlewares/Validade.js
+++ middlewares/Validade.js
@@ -2,10 +2,12 @@ const validate = (token, res) => {
   if (!token) {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
+
   const tokenIsValid = 16;
   if (token.length < tokenIsValid) {
     return res.status(401).json({ message: 'Token inválido' });
   }
+  return null;
 };
 
 module.exports = {
