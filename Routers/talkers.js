7d3e4992da9972642fcfb7f5b51df668090ae696
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-09-01 17:33:08 -0300

    refatora código

--- Routers/talkers.js
+++ Routers/talkers.js
@@ -3,6 +3,7 @@ const express = require('express');
 const talkers = express.Router();
 
 const {
+  findAll,
   findOne,
   validaToken,
   editTalker,
@@ -16,31 +17,13 @@ const {
   deletaTalker,
 } = require('../middlewares');
 
-const {
-  readFile,
-} = require('../utils/talkers');
-
 talkers.get('/search',
   validaToken,
   seachTalker);
 
-talkers.get('/', async (_req, res) => {
-  const talk = await readFile();
-  return res.status(200).json(talk);
-});
+talkers.get('/', findAll);
 
-// ALTERAR!
-talkers.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const talk = await readFile();
-  const talkerFind = findOne(id, talk);
-  if (talkerFind) {
-    return res.status(200).json(talkerFind);
-  }
-  return res.status(404).json({
-    message: 'Pessoa palestrante não encontrada',
-  });
-});
+talkers.get('/:id', findOne);
 
 talkers.use(validaToken);
 
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,8 +1,23 @@
 const { readFile, writeFile } = require('../utils/talkers');
 
-const findOne = (id, listTalkers) => {
-  const talkerFind = listTalkers.find((talker) => talker.id === +id);
-  return talkerFind;
+const findAll = async (_req, res) => {
+  const talk = await readFile();
+
+  return res.status(200).json(talk);
+};
+
+const findOne = async (req, res) => {
+  const { id } = req.params;
+
+  const talk = await readFile();
+  const talkerFind = talk.find((talker) => talker.id === +id);
+
+  if (talkerFind) {
+    return res.status(200).json(talkerFind);
+  }
+  return res.status(404).json({
+    message: 'Pessoa palestrante não encontrada',
+  });
 };
 
 const validarEmail = (email) => {
@@ -12,6 +27,7 @@ const validarEmail = (email) => {
   if (!email) return obrigatorio; 
   const validEmail = email.includes('@') && email.includes('.com');
   if (!validEmail) return formato;
+
   return 'ok';
 };
 
@@ -21,33 +37,39 @@ const validarSenha = (password) => {
 
   if (!password) return obrigatorio;  
   if (password.length < 6) return minimo;
+
   return 'ok';
 };
 
 const validaToken = (req, res, next) => {
   const token = req.headers.authorization;
+
   if (!token) {
     return res.status(401).json({ message: 'Token não encontrado' });
   } 
   if (token.length && token.length !== 16) {
     return res.status(401).json({ message: 'Token inválido' });
   } 
+
   next();
 };
 
 const validaNome = (req, res, next) => {
   const { name } = req.body;
+
   if (!name) {
     return res.status(400).json({ message: 'O campo "name" é obrigatório' });
   }
   if (name.length < 3) {
     return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
   }
+
   next();
 };
 
 const validaAge = (req, res, next) => {
   const { age } = req.body;
+
   if (!age) {
     return res.status(400).json({ message: 'O campo "age" é obrigatório' });
   }
@@ -55,11 +77,13 @@ const validaAge = (req, res, next) => {
     return res
       .status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
   }
+
   next();
 };
 
 const validaTalk = (req, res, next) => {
   const { talk } = req.body;
+
   if (!talk) {
     return res
      .status(400)
@@ -70,11 +94,13 @@ const validaTalk = (req, res, next) => {
      .status(400)
      .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
+
   next();
 };
 
 const validaRate = (req, res, next) => {
   const { rate } = req.body.talk;
+
   if (!rate && rate !== 0) {
     return res
      .status(400)
@@ -83,22 +109,27 @@ const validaRate = (req, res, next) => {
   if (rate < 1 || rate > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
+
   next();
 };
 
 const validaDate = (req, res, next) => {
   const { talk } = req.body;
+
   const regexDate = /^[0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4}$/g;
+
   if (!regexDate.test(talk.watchedAt)) {
     return res
       .status(400)
       .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
+
   next();
 };
 
 const addTalker = async (req, res) => {
   const { name, age, talk } = req.body;
+
   const talkersList = await readFile();
   const countTalker = talkersList.length;
   const newTalker = {
@@ -107,45 +138,60 @@ const addTalker = async (req, res) => {
     age,
     talk,
   };
+
   talkersList.push(newTalker);
+
   await writeFile(talkersList);
+
   return res.status(201).json(newTalker);
 };
 
 const editTalker = async (req, res) => {
   const { name, age, talk } = req.body;
+
   let talkersList = await readFile();
   const { id } = req.params;  
+
   talkersList = talkersList.filter((talker) => talker.id !== +id);
   talkersList.push({ id: +id, name, age, talk });
 
   await writeFile(talkersList);  
+
   return res.status(200).json({ id: +id, name, age, talk });
 };
 
 const seachTalker = async (req, res) => {
   const { q } = req.query;
+
   const talkers = await readFile();
+
   if (!q || q === '') {
     return res.status(200).json(talkers);    
   }   
+
   const filteredTalkers = talkers.filter((talker) => talker.name.includes(q));
+
   if (filteredTalkers.length > 0) {
     return res.status(200).json(filteredTalkers); 
   } 
+
   return res.status(200).json(Array.from([])); 
 };
 
 const deletaTalker = async (req, res) => {  
-  let talkersList = await readFile();
   const { id } = req.params;  
+
+  let talkersList = await readFile();
+  
   talkersList = talkersList.filter((talker) => talker.id !== +id);
   
   await writeFile(talkersList);  
+  
   return res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
 };
   
 module.exports = {
+  findAll,
   findOne,
   validarSenha,
   validarEmail,
