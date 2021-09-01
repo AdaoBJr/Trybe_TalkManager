e226f77b0da32715b11c04b56f6bcf4678bf197c
EderPaiva <edergomesdepaiva12@gmail.com>
2021-09-01 13:06:59 -0300

    requisito 3 re-up

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/login',
 (req, res, next) => {
   const { email } = req.body;
   if (email === '') {
-        return res.status(401).json({ message: 'O campo "email" é obrigatório' });
+        return res.status(401).send({ message: 'O campo "email" é obrigatório' });
       } 
 
   next(); // 1 Email com string vazia
@@ -66,14 +66,14 @@ app.post('/login',
   const { email } = req.body;
   const validEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
   if (validEmail.test(email) === false) {
-        return res.status(401).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+        return res.status(401).send({ message: 'O "email" deve ter o formato "email@email.com"' });
       }
   next(); // 2 Email sem validade do regex
 },
 (req, res, next) => {
   const { password } = req.body;
   if (password === '') {
-    return res.status(401).json({ message: 'O campo "password" é obrigatório' });
+    return res.status(401).send({ message: 'O campo "password" é obrigatório' });
       } 
 
   next(); // 3 Password com string vazia
@@ -83,7 +83,7 @@ app.post('/login',
   const SIX = 6;
   const { password } = req.body;
   if (password.length > SIX) {
-    return res.status(401).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    return res.status(401).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
       } 
 
   next(); // 4 Password com número de caracteres insuficientes.
