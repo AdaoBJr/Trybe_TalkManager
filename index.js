ac66621348ed508c1b1c6dd705cdcee8a2ab7a0e
Diegho <dieghonm@gmail.com>
2021-09-01 17:06:37 -0300

    Iniciando projeto, req 1

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', async (req, res) => {
+  return res.status(HTTP_OK_STATUS)
+  .send(JSON.parse(await fs.readFile('talker.json', 'utf-8')));
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -15,3 +21,7 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+// const rescue = require('express-rescue');
+// const randomBytes = require('crypto');
+// const token = randomBytes(8).toString('hex'); 
