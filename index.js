6a615cd7a7fad29dd9a2285db9bacd5d70719781
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-09-01 15:09:33 -0300

    I created the endpoint /talker

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
+
 app.get('/talker', (_req, res) => {
   if (talkers.length === 0) {
     return res.status(200).send([]);
   }
   return res.status(200).send(talkers);
-});
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
