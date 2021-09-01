3120f70877b05bcfc01a45ecbdc9f31455a03e9c
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-09-01 16:24:08 -0300

    I created the endpoint /talker/:id

--- index.js
+++ index.js
@@ -15,14 +15,27 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+// REQUISITO 1
 // Usei o async/await por se tratar de um funcção assíncrona (readFile);
 app.get('/talker', async (_req, res) => {
   const getTalkers = await fs.readFile('./talker.json');
   const talkersJson = await JSON.parse(getTalkers);
   if (talkersJson.length === 0) {
-    return res.status(200).send([]);
+    return res.status(HTTP_OK_STATUS).send([]);
   }
-  return res.status(200).json(talkersJson);
+  return res.status(HTTP_OK_STATUS).send(talkersJson);
+});
+
+// REQUISITO 2
+app.get('/talker/:id', async (req, res) => {
+  const getTalkers = await fs.readFile('./talker.json');
+  const talkersJson = await JSON.parse(getTalkers);
+  const { id } = req.params;
+  const getTheTalker = talkersJson.find((talker) => talker.id === +id);
+  if (!getTheTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(HTTP_OK_STATUS).send(getTheTalker);
 });
 
 app.listen(PORT, () => {
