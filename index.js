5ddbd72a3378fa4f28cba2f54c4c10e2ad56bff3
Cristian Bugs <cristian.bugs@gmail.com>
2021-09-01 18:33:55 -0300

    fixing third task

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 const router = require('./routes/userRouter');
-
-// const util = require('util');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(express.json());
+// app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -18,25 +17,25 @@ app.get('/', (_request, response) => {
 
 app.use('/', router);
 
-// const talkerCaller = async () => {
-//   const data = await fs.readFile('./talker.json', 'utf-8');
-//   return JSON.parse(data);
-// };
+const talkerCaller = async () => {
+  const data = await fs.readFile('./talker.json', 'utf-8');
+  return JSON.parse(data);
+};
 
-// app.get('/talker', async (req, res) => {
-//   const data = await talkerCaller();
-//   res.status(HTTP_OK_STATUS).send(data);
-// });
+app.get('/talker', async (req, res) => {
+  const data = await talkerCaller();
+  res.status(HTTP_OK_STATUS).send(data);
+});
 
-// app.get('/talker/:id', async (req, res) => {
-//   const data = await talkerCaller();
-//   const { id } = req.params;
-//   const result = data.find((c) => c.id === Number(id));
-//   if (!result) {
-//   return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
-//   }
-//   res.status(HTTP_OK_STATUS).send(result);
-// });
+app.get('/talker/:id', async (req, res) => {
+  const data = await talkerCaller();
+  const { id } = req.params;
+  const result = data.find((c) => c.id === Number(id));
+  if (!result) {
+  return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(HTTP_OK_STATUS).send(result);
+});
 
 app.listen(PORT, () => {
   console.log('Online');
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,4 @@
 const router = require('express').Router();
-const bodyParser = require('body-parser');
-
-router.use(bodyParser.json());
-const fs = require('fs').promises;
 const crypto = require('crypto');
 
 const {
@@ -10,15 +6,8 @@ const {
     isValidPassword,
   } = require('./middleware');
 
-  const HTTP_OK_STATUS = 200;
-
-const talkerCaller = async () => {
-    const data = await fs.readFile('./talker.json', 'utf-8');
-    return JSON.parse(data);
-};
-
 router.post(
-    'login',
+    '/login',
     isValidEmail,
     isValidPassword,
     (_req, res) => { 
@@ -28,27 +17,34 @@ router.post(
     },
 );
 
-router.get('talker', async (req, res) => {
-    const data = await talkerCaller();
-    res.status(HTTP_OK_STATUS).send(data);
-  });
+// const talkerCaller = async () => {
+//     const data = await fs.readFile('./talker.json', 'utf-8');
+//     return JSON.parse(data);
+// };
+
+// router.get('talker', async (req, res) => {
+//     const data = await talkerCaller();
+//     console.log(data);
+//     res.status(HTTP_OK_STATUS).send(data);
+//   });
   
-router.get('talker/:id', async (req, res) => {
-    const data = await talkerCaller();
-    const { id } = req.params;
-    const result = data.find((c) => c.id === Number(id));
-    if (!result) {
-    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
-    }
-    res.status(HTTP_OK_STATUS).send(result);
-});
-router.put('talker/:id', async (req, res) => {
-    const { id } = req.params;
-    // const { name, age, talk: { watchedAt }, talk: { rate } } = req.body;
-    const data = await talkerCaller();
-    console.log(data);
-    const result = data.find((dat) => dat.id === Number(id));
-    console.log(result);
-    res.status(200).json({ result });
-});
+// router.get('talker/:id', async (req, res) => {
+//     const data = await talkerCaller();
+//     const { id } = req.params;
+//     const result = data.find((c) => c.id === Number(id));
+//     if (!result) {
+//     return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+//     }
+//     res.status(HTTP_OK_STATUS).send(result);
+// });
+
+// router.put('talker/:id', async (req, res) => {
+//     const { id } = req.params;
+//     // const { name, age, talk: { watchedAt }, talk: { rate } } = req.body;
+//     const data = await talkerCaller();
+//     console.log(data);
+//     const result = data.find((dat) => dat.id === Number(id));
+//     console.log(result);
+//     res.status(200).json({ result });
+// });
   module.exports = router;
