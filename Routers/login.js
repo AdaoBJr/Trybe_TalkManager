d9e9edfe7f34616901690cdf3a83d51b9c026977
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-09-01 18:21:47 -0300

    faltou return no login

--- Routers/login.js
+++ Routers/login.js
@@ -12,7 +12,7 @@ login.post('/', (req, res) => {
   if (checkEmail !== 'ok') res.status(400).json(checkEmail);
   if (checkPassword !== 'ok') res.status(400).json(checkPassword);
 
-  res.status(200).json({
+  return res.status(200).json({
     token: gerarToken(),
   });
 });
