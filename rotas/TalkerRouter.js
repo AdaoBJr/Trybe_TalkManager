caeff516c8b31bb92fcd1afaf567bc32fb1777bd
Camila Arruda <camilaam1802@gmail.com>
2021-09-01 12:01:17 -0300

    desafio 2 completo

--- rotas/TalkerRouter.js
+++ rotas/TalkerRouter.js
@@ -6,6 +6,8 @@ const fs = require('fs').promises;
 
 const router = express.Router();
 
+const HTTP_OK_STATUS = 200;
+
 // Vai ler o meu arquivo e retorna uma promisse
 const readTalkers = async () => {
     const talkers = await fs.readFile('./talker.json', 'utf-8');
@@ -15,7 +17,18 @@ const readTalkers = async () => {
 // Resolvo a promisse anterior e andpoint GET
 router.get('/', async (req, res) => {
     const resolvedTalkers = await readTalkers();
-    res.status(200).send(resolvedTalkers);
+    res.status(HTTP_OK_STATUS).send(resolvedTalkers);
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkers = await readTalkers();
+    const talker = talkers.find((talk) => talk.id === Number(id));
+    if (!talker) {
+        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+    return res.status(HTTP_OK_STATUS).json(talker);
 });
 
 module.exports = router;
