881101a8349d91e5be6bc5534feeb28b7edf9b38
EderPaiva <edergomesdepaiva12@gmail.com>
2021-09-01 19:06:15 -0300

    requisito 3

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const authLogin = require('./middleware/login.js');
 const app = express();
 
 app.use(bodyParser.json());
+// não remova esse endpoint, e para o avaliador funcionar
+const HTTP_OK_STATUS = 200;
+app.get('/', (_request, response) => response.status(HTTP_OK_STATUS).send());
 app.use(authLogin);
 
-const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // Função para ler o Json!
@@ -27,60 +29,42 @@ const PORT = '3000';
 // const aleatoryToken = crypto.randomBytes(8).toString('hex');
 // return aleatoryToken;
 // };
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => 
-  response.status(HTTP_OK_STATUS).send());
+
 // está escutando as requisições feitas pelos usuários.
 
 //  REQUISITO 1
 
 app.get('/talker', async (_request, response) => {
   const talker = await getTalker();
+  console.log('entrei');
   return response.status(HTTP_OK_STATUS).send(talker);
 });
 
 // REQUISITO 2
 
-app.get('/talker/:id', rescue(async (_request, response) => {
-  const { id } = _request.params;
-  const talker = await getTalker();
-  const responseForUse = talker.find((AllTalkers) => AllTalkers.id === Number(id));
-  if (!responseForUse) {
-    return response.status(404).send({
-      message: 'Pessoa palestrante não encontrada',
-    });
-  }
-    return response.status(HTTP_OK_STATUS).send(responseForUse);
-}));
+// app.get('/talker/:id', rescue(async (_request, response) => {
+//   const { id } = _request.params;
+//   const talker = await getTalker();
+//   const responseForUse = talker.find((AllTalkers) => AllTalkers.id === Number(id));
+//   if (!responseForUse) {
+//     return response.status(404).send({
+//       message: 'Pessoa palestrante não encontrada',
+//     });
+//   }
+//     return response.status(HTTP_OK_STATUS).send(responseForUse);
+// }));
 
 // REQUISITO 3
 
-app.post('/login', (_request, response) => {
-  const token = crypto.randomBytes(8).toString('hex');
-  // const token = '7mqaVRXJSp886CGr'; 
+// app.post('/login', (_request, response) => {
+//   const token = crypto.randomBytes(8).toString('hex');
+//   // const token = '7mqaVRXJSp886CGr'; 
 
-  return response.status(HTTP_OK_STATUS).json({
-    token,
-  });
-});
+//   return response.status(HTTP_OK_STATUS).json({
+//     token,
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log('Online');
 });
-
-  // app.post('/login', (req, res, next) => {
-  //   const { email, password } = req.body;
-  //   const validEmail = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-  //   const minOfCaracteres = 6;
-  
-  //   if (email === '') {
-  //     return res.status(401).json({ message: 'O campo "email" é obrigatório' });
-  //   }
-  
-  //   if (validEmail.test(email) === false) {
-  //     return res.status(401).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  //   }
-  
-  //   if (password === '') {
-  //     return res.status(401).json({ message: 'O campo "password" é obrigatório' });
-  //   }
