62875dfb2eff86f7644be725a3b7d01797a0ab19
Aremtior <marcosleandrofl@gmail.com>
2021-09-01 11:50:48 -0300

    requirements 1 and 2 readFileSync

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,37 +10,37 @@ const PORT = '3000';
 
 const talkerData = 'talker.json';
 
-async function readData() {
+function readData() {
   try {
-    const data = await fs.readFile(talkerData);
+    const data = fs.readFileSync(talkerData, 'utf8');
     return data;
   } catch (err) {
     console.error(`Não foi possível ler o arquivo ${talkerData}\n Erro: ${err}`);
-    // process.exit(1);
+    process.exit(1);
   }
 }
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
-
-app.get('/talker', async (req, res) => {
-  const talkers = await readData();
+app.get('/talker', (req, res) => {
+  const talkers = JSON.parse(readData());
 
   if (!talkers) return res.status(HTTP_OK_STATUS).json({});
 
   res.status(HTTP_OK_STATUS).json(talkers);
 });
 
-app.get('/talker/:id', async (req, res) => {
+app.get('/talker/:id', (req, res) => {
   const { id } = req.params;
-  const talkers = await readData().json();
+  const talkers = JSON.parse(readData());
   const talker = talkers.find((t) => t.id === parseInt(id, 10));
 
   if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   res.status(HTTP_OK_STATUS).json(talker);
+});
+
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
 });
 
 app.listen(PORT, () => {
