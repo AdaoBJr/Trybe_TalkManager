50b9989b39397b3a18ac2f5c219488b70b665d25
Johnny batata <johnny.arguello007@gmail.com>
2021-09-01 16:24:14 -0300

    requirement 3

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 const talker = require('./talker.json'); 
 
+const validateUser = (req, res, next) => {
+  const { email, password } = req.body;
+  const mailFormat = /[a-z]+@[a-z]+.com/g;
+  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!email.match(mailFormat)) {
+ return res.status(400)
+  .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+}
+if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' }); 
+
+if (password.length < 6) {
+ return res.status(400)
+.json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+}
+next();
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -33,18 +50,4 @@ app.get('/talker', (req, res) => {
   return res.status(200).json(talker);
 });
 
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
-  const mailFormat = /[a-z]+@[a-z]+.com/g;
-  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-  if (!email.match(mailFormat)) {
- return res.status(400)
-  .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
-}
-if (password.length < 6) {
- return res.status(400)
-.json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
-}
-
-  return res.status(200).json({ token });
-});
+app.post('/login', validateUser, (req, res) => res.status(200).json({ token }));
