721823a2fd0650f224937510b757aa09615406bc
Roger Franco <rogerfrancogalt@gmail.com>
2021-08-31 22:17:21 -0400

    req 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises; // importei
 // const { promisify } = require('util'); // 
 const rescue = require('express-rescue'); //
+const { findId } = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
@@ -25,6 +26,18 @@ app.get('/talker', rescue(async (req, res) => {
     return res.status(200).json([]);
   }
 }));
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await talkerFile();
+  const talkerFind = findId(id, talker);
+  if (talkerFind) {
+    return res.status(200).json(talkerFind);
+  }
+    return res.status(400).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
