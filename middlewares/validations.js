cdba09102c6590b56f8bccc04cd3f0024c38c934
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-09-01 16:17:13 -0300

    correction on validatePassword middleware

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -28,11 +28,11 @@ const validatePassword = (req, res, next) => {
   const { password } = req.body;
 
   if (!password || password === '') {
-    res.status(400).json({ message: 'O campo "password" é obrigatório' });
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
 
   if (password.length < 6) {
-    res
+    return res
       .status(400)
       .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
