31ec349d23ea2afc832f5465645754aa74cf9f46
Cristian Bugs <cristian.bugs@gmail.com>
2021-09-01 17:46:06 -0300

    changes to userRouter

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises;
 const router = require('./routes/userRouter');
 
 // const util = require('util');
@@ -17,31 +16,26 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.use('/login', router);
+app.use('/', router);
 
-const talkerCaller = async () => {
-  const data = await fs.readFile('./talker.json', 'utf-8');
-  return JSON.parse(data);
-};
+// const talkerCaller = async () => {
+//   const data = await fs.readFile('./talker.json', 'utf-8');
+//   return JSON.parse(data);
+// };
 
-app.get('/talker', async (req, res) => {
-  const data = await talkerCaller();
-  res.status(HTTP_OK_STATUS).send(data);
-});
+// app.get('/talker', async (req, res) => {
+//   const data = await talkerCaller();
+//   res.status(HTTP_OK_STATUS).send(data);
+// });
 
-app.get('/talker/:id', async (req, res) => {
-  const data = await talkerCaller();
-  const { id } = req.params;
-  const result = data.find((c) => c.id === Number(id));
-  if (!result) {
-  return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
-  }
-  res.status(HTTP_OK_STATUS).send(result);
-});
-
-// app.post('/talker', (req, res) => {
-//   const { name, age, talk, watchedAt, rate } = req.body;
-//   console.log(req.headers);
+// app.get('/talker/:id', async (req, res) => {
+//   const data = await talkerCaller();
+//   const { id } = req.params;
+//   const result = data.find((c) => c.id === Number(id));
+//   if (!result) {
+//   return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+//   }
+//   res.status(HTTP_OK_STATUS).send(result);
 // });
 
 app.listen(PORT, () => {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.json());
+const fs = require('fs').promises;
 const crypto = require('crypto');
 
 const {
@@ -6,8 +10,15 @@ const {
     isValidPassword,
   } = require('./middleware');
 
-  router.post(
-    '',
+  const HTTP_OK_STATUS = 200;
+
+const talkerCaller = async () => {
+    const data = await fs.readFile('./talker.json', 'utf-8');
+    return JSON.parse(data);
+};
+
+router.post(
+    'login',
     isValidEmail,
     isValidPassword,
     (_req, res) => { 
@@ -15,5 +26,29 @@ const {
     console.log(tok);
     res.status(200).json({ token: tok });
     },
-  );
+);
+router.put('talker/:id', async (req, res) => {
+    const { id } = req.params;
+    // const { name, age, talk: { watchedAt }, talk: { rate } } = req.body;
+    const data = await talkerCaller();
+    console.log(data);
+    const result = data.find((dat) => dat.id === Number(id));
+    console.log(result);
+    res.status(200).json({ result });
+});
+
+router.get('/talker', async (req, res) => {
+    const data = await talkerCaller();
+    res.status(HTTP_OK_STATUS).send(data);
+  });
+  
+router.get('/talker/:id', async (req, res) => {
+    const data = await talkerCaller();
+    const { id } = req.params;
+    const result = data.find((c) => c.id === Number(id));
+    if (!result) {
+    return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
+    }
+    res.status(HTTP_OK_STATUS).send(result);
+});
   module.exports = router;
