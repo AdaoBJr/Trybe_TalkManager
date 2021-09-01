76ce34b8ff975d97510396a953af9d8ec6323970
joao966 <joaooliveira966@gmail.com>
2021-09-01 03:23:45 -0300

    .

--- middlewares/req-4.js
+++ middlewares/req-4.js
@@ -4,10 +4,10 @@ const validaToken = (req, res, next) => {
   const { authorization } = req.headers;
   console.log(authorization);
   if (!authorization) {
-    return res.status(401).json('Token não encontrado');
+    return res.status(401).json({ message: 'Token não encontrado' });
   } 
   if (authorization.length !== 16) {
-    return res.status(401).json('Token inválido');
+    return res.status(401).json({ message: 'Token inválido' });
   } 
   next();
 };
