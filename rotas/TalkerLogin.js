eee15d23b39ceb691716fdd493e70c7dfbb9ff8b
Camila Arruda <camilaam1802@gmail.com>
2021-09-01 14:24:10 -0300

    consertando req 3 de novo

--- rotas/TalkerLogin.js
+++ rotas/TalkerLogin.js
@@ -10,11 +10,11 @@ const emailValidation = (req, res, next) => {
     const validEmail = new RegExp(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i);
 
     if (!email) {
-        return res.status(404).json({ message: 'O campo "email" é obrigatório' });
+        return res.status(400).json({ message: 'O campo "email" é obrigatório' });
     }
     // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
     if (!validEmail.test(email)) {
-        return res.status(404).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+        return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
     }
     return next();
 };
