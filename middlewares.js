e90f9f80ae45a051d3bb9d53dd447e8d5df3cbd3
legalvao1 <legalvao1@gmail.com>
2021-08-31 23:26:30 -0300

    Requisito 4 - Rota para adicionar uma nova pessoa palestrante ao talker.json

--- middlewares.js
+++ middlewares.js
@@ -4,17 +4,32 @@ const crypto = require('crypto');
 const readTalkerJson = () => fs.readFile('./talker.json', 'utf-8')
   .then((fileContent) => JSON.parse(fileContent));
 
+const saveNewPalestrante = (content) => fs.writeFile('./talker.json', JSON.stringify(content));
+
 const generateToken = () => crypto.randomBytes(8).toString('hex');
+
+const isValidToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  if (!/^[a-zA-Z0-9]{16}$/.test(token)) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
 
 // https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
 function validateEmail(email) {
  const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
  if (reg.test(email)) {
- return true; 
+ return true;
 }
- 
+
  return false;
-} 
+}
 
 const isValidEmail = (req, res, next) => {
   const { email } = req.body;
@@ -32,7 +47,7 @@ const isValidEmail = (req, res, next) => {
 
 const isValidPassword = (req, res, next) => {
   const { password } = req.body;
- 
+
   if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
@@ -44,9 +59,71 @@ const isValidPassword = (req, res, next) => {
   next();
 };
 
+const validName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const validAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+const validTalk = (req, res, next) => {
+  const { talk } = req.body;
+   if (!talk || !talk.watchedAt || !talk.rate) {
+    return res.status(400).json(
+      { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
+    );
+  }
+
+  next();
+};
+
+const validaData = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const checkData = /^(\d{1,2})\/(\d{1,2})\/(\d{4})$/;
+  if (!checkData.test(watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  next();
+};
+
+const validRate = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+  const rateEntreUmeCinco = /^[1-5]*$/;
+
+  if (!rateEntreUmeCinco.test(rate)) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
 module.exports = {
   readTalkerJson,
   generateToken,
   isValidEmail,
   isValidPassword,
+  isValidToken,
+  validName,
+  validAge,
+  validTalk,
+  validaData,
+  validRate,
+  saveNewPalestrante,
 };
--- talkersRouter.js
+++ talkersRouter.js
@@ -4,6 +4,13 @@ const router = express.Router();
 
 const { 
   readTalkerJson,
+  isValidToken,
+  validName,
+  validAge,
+  validTalk,
+  validaData,
+  validRate,
+  saveNewPalestrante,
 } = require('./middlewares');
 
 router.get('/', async (_req, res) => {
@@ -23,6 +30,18 @@ router.get('/:id', async (req, res) => {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
   res.status(200).json(...getPalestranteByID);
+});
+
+router.post('/', 
+  isValidToken, validName, validAge, validTalk, validaData, validRate, async (req, res) => {
+  const { name, age, talk } = req.body;
+  const palestrantes = await readTalkerJson();
+  const id = palestrantes.length + 1;
+  const newPalestrante = { id, name, age, talk };
+  palestrantes.push(newPalestrante);
+  saveNewPalestrante(palestrantes);
+
+  res.status(201).json(newPalestrante);
 });
 
 module.exports = router;
