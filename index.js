c3e41dc98f0c22419d52bf2aaad2a5cb4d15b026
Lara Capila <laracapilak@gmail.com>
2021-09-01 16:32:39 -0300

    Implementa requisito 3

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { getTalkers, getTalkerById } = require('./middlewares/index'); 
+const {
+  getTalkers,
+  getTalkerById,
+  validationEmail,
+  validationPassword,
+  generateRandomToken,
+} = require('./middlewares/index'); 
 
 const app = express();
 app.use(bodyParser.json());
@@ -21,3 +27,22 @@ app.listen(PORT, () => {
 app.get('/talker/:id', getTalkerById);
 
 app.get('/talker', getTalkers);
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  const validateEmail = validationEmail(email);
+  const validatePassword = validationPassword(password);
+  console.log(email, password);
+  console.log(validationEmail(email), validationPassword(password));
+
+  // Estava fazendo assim mas deu erro
+  // if (validateEmail.includes('O')) res.status(400).json(validateEmail);
+  // if (validatePassword.includes('O')) res.status(400).json(validatePassword);
+
+  if (validateEmail !== 'email ok') res.status(400).json(validateEmail);
+  if (validatePassword !== 'password ok') res.status(400).json(validatePassword);
+
+  res.status(200).json({
+    token: generateRandomToken(),
+  });
+});
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,8 +1,15 @@
+const express = require('express');
+const crypto = require('crypto');
+
+const app = express();
+
+app.use(express.json());
 const fs = require('fs').promises;
 
 const HTTP_OK_STATUS = 200;
 const readTalkers = () => fs.readFile('./talker.json', 'utf8');
 
+// REQUISITO 2
 const getTalkerById = async (req, res) => {
   const { id } = req.params;
   const data = await readTalkers()
@@ -15,6 +22,7 @@ const getTalkerById = async (req, res) => {
   return res.status(HTTP_OK_STATUS).send(data);
 };
 
+// REQUISITO 1
 const getTalkers = (_req, res) => {
   readTalkers().then((talkers) => res.status(HTTP_OK_STATUS).send(JSON.parse(talkers)))
   .catch((err) => res.status(401)
@@ -25,7 +33,35 @@ const getTalkers = (_req, res) => {
   }
 };
 
+// REQUISITO 3
+const validationEmail = (email) => {
+  const messageRequired = { message: 'O campo "email" é obrigatório' };
+  const messageForm = { message: 'O "email" deve ter o formato "email@email.com"' };
+  const regex = new RegExp(/^[\w.]+@[a-z]+.\w{3}$/g);
+
+  if (!email) return messageRequired;
+  if (!regex.test(email)) return messageForm;
+  return 'email ok';
+};
+
+const validationPassword = (password) => {
+  const messageRequired = { message: 'O campo "password" é obrigatório' };
+  const messageForm = { message: 'O "password" deve ter pelo menos 6 caracteres' };
+
+  if (!password) return messageRequired;
+  if (password.length < 6) return messageForm;
+  return 'password ok';
+};
+
+// https://www.youtube.com/watch?v=Rcpy2Den2Ik&ab_channel=DevNami
+function generateRandomToken() {
+  return crypto.randomBytes(16).toString('hex');
+}
+
 module.exports = {
   getTalkers,
   getTalkerById,
+  validationEmail,
+  validationPassword,
+  generateRandomToken,
 };
