cd2eeb98f82caa1c52d37244c76a1ff66318b23c
renanpbraga <renanpbraga@gmail.com>
2021-09-01 17:18:03 -0300

    adiciona requisito 4

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { validateEmail, validatePassword, createToken } = require('./middleware/validations');
+const fs2 = require('fs').promises;
+
+const {
+  validateEmail,
+  validatePassword,
+  createToken,
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
+} = require('./middleware/validations');
 
 const app = express();
 app.use(bodyParser.json());
@@ -24,6 +36,38 @@ app.get('/talker', (_req, res) => {
   });
 });
 
+app.post('/login', validateEmail, validatePassword, (_req, res) => res.status(200).json(
+  {
+    token: createToken(),
+  },
+  ));
+
+app.post(
+  '/talker',
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateRate,
+  validateWatchedAt,
+  async (req, res) => {
+    const { name, age, talk } = req.body;
+    const talkers = await fs2.readFile('./talker.json', 'utf-8');
+    const parseTalkers = JSON.parse(talkers);
+    const id = parseTalkers.length + 1;
+    console.log(id, 'IDDDDDDDDDDDDDDDDDDD')
+    const newTalker = {
+      id,
+      name,
+      age,
+      talk,
+    };
+    const addTalker = JSON.stringify([...parseTalkers, newTalker])
+      await fs2.writeFile('./talker.json', addTalker);
+      return res.status(201).json(newTalker);
+    },
+);
+
 app.get('/talker/:id', (req, res) => {
   fs.readFile('./talker.json', 'utf-8', (_err, content) => {
     const data = JSON.parse(content);
@@ -38,12 +82,6 @@ app.get('/talker/:id', (req, res) => {
     return res.status(200).json(findId);
   });
 });
-
-app.post('/login', validateEmail, validatePassword, (_req, res) => res.status(200).json(
-  {
-    token: createToken(),
-  },
-  ));
 
 app.listen(PORT, () => {
   console.log('Online');
