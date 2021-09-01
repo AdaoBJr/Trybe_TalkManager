b1939446e55c836307abcd00a8e8e9be5ad75205
Fernanda Porto Oliveira <fernanda.oliveira.mec@gmail.com>
2021-08-31 21:12:46 -0300

    Requisito 3 - REFATORAR TUDO

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs').promises;
-// const rescue = require('express-rescue');
+const crypto = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// Source: https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js
+
+function genarateRandomToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
+
+// const validateToken = (req, res, next) => {
+//   const token = req.headers.authorization;
+//   if (token !== '7mqaVRXJSp886CGr') return res.status(401).json({ message: 'Unauthorized!' });
+//   next();
+// };
+
+const validateParams = (req, res) => {
+  const { email, password } = req.body;
+  if (!email) { return res.status(400).json({ message: 'O campo "email" é obrigatório' }); }
+  if (!email.match(/\S+@\S+\.\S+/)) { 
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+}
+  if (!password) { return res.status(400).json({ message: 'O campo "password" é obrigatório' }); }
+  if (password.length < 6) { 
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' }); 
+}
+  return res.status(200).json({ token: genarateRandomToken() });
+};
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -34,9 +59,19 @@ app.get('/talker/:id', (req, res) => {
       }
       return res.status(200).send(content[talkerIndex]);
   })
-    .catch((_err) => res.status(404)
-    .json({ message: 'Pessoa palestrante não encontrada' }));
+    .catch((err) => res.status(401)
+    .json({ message: err.message }));
 });
+
+app.post('/login', validateParams);
+
+// app.post('/talker', (req, res, next) => {});
+
+// app.put('/talker/:id', (req, res, next) => {});
+
+// app.delete('/talker/:id', (req, res, next) => {});
+
+// app.get('/talker/search?q=searchTerm', (req, res, next) => {});
 
 app.listen(PORT, () => {
   console.log('Online');
