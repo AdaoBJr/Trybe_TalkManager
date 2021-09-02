67ec7540ab7f728be7fe2fba33977540301192b8
Isabella Paz <bellapaz.iorp@gmail.com>
2021-09-02 07:42:12 -0300

    index dos middlewares

---
+++ Middlewares/index.js
@@ -0,0 +1,15 @@
+const createTalker = require('./createTalker');
+const deleteTalker = require('./deleteTalker');
+const editTalker = require('./editTalker');
+const getTalkersData = require('./getTalkersData');
+const searchTalkerById = require('./searchTalkerById');
+const searchTalkerByName = require('./searchTalkerByName');
+
+module.exports = {
+  createTalker,
+  deleteTalker,
+  editTalker,
+  getTalkersData,
+  searchTalkerById,
+  searchTalkerByName,
+};
