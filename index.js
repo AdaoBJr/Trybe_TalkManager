dfdd827bcc7beb496a644026929e49b5fb3ede46
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-02 19:21:55 -0300

    Refactor: Modifica o parse json

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (req, res, next) => {
+app.get('/talker', async (_req, res, next) => {
   try {
-    const file = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+    const file = await fs.readFile('./talker.json', 'utf-8');
 
     res.status(HTTP_OK_STATUS).json(file);
 
