f73b061a64a3bf0b622c7af691edbb190c329f18
Breno3B <brenorm@gmail.com>
2021-09-02 02:27:13 -0300

    3º Requisito: Nossa senhora do Regex quebrado em linhas!!!

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,6 +1,11 @@
 // Source: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+// +
+// Source: https://stackoverflow.com/questions/12317049/how-to-split-a-long-regular-expression-into-multiple-lines-in-javascript
 function regexEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line
+  const re = new RegExp(['^(([^<>()[\\]\\.,;:\\s@"]+(\\.[^<>()[\\]\\.,;:\\s@"]+)*)',
+                         '|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]',
+                         '{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'].join(''));
+
   return re.test(String(email).toLowerCase());
 }
 
