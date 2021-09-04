0d8e9be8c8c40de5cf48ae5f935f44eacb43a547
Thiago Leite <leite.tjs@gmail.com>
2021-09-03 23:02:18 -0300

    testing 3rd requi 2

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.post(
   (req, res) => {
     const token = crypto.randomBytes(8).toString('hex');
 
-    res.status(200).json({ message: token });
+    res.status(200).json({ token });
   },
 );
 
