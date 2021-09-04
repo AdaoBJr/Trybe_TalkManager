0f1cc26a50351b87a3ff57aea6af5bf3b5a210e8
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-09-04 14:17:23 -0300

    req 2 lint corrigido

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { response } = require('express');
 const { readTalkerFunc } = require('./readTalker');
 
 const app = express();
