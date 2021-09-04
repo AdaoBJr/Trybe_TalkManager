4927e9291b41210ea322e0b2a1b286b525b06ea8
Diegho <dieghonm@gmail.com>
2021-09-04 19:47:35 -0300

    Requisito 3

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises;
+const { randomBytes } = require('crypto'); // token
+const primeiroGet = require('./routers/rota66');
+
+const token = randomBytes(8).toString('hex'); // token
 
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
-const HTTP_ERROR_STATUS = 404;
 const PORT = '3000';
+const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_LOGUIN = 400;
 
-app.get('/talker', async (req, res) => res.status(HTTP_OK_STATUS)
-  .send(JSON.parse(await fs.readFile('talker.json', 'utf-8'))));
+const emailRegex = RegExp(/[a-z0-9]+@[a-z0-9]+\.[a-z0-9]{2,3}(\.[a-z0-9]+)?$/);
+const MinPassword = 6;
 
-app.get('/talker/:id', async (req, res) => { 
-  const { id } = req.params;
-  const DB = JSON.parse(await fs.readFile('talker.json', 'utf-8'));
-  const resp = DB.find((item) => item.id === +id);
-  console.log(resp);
-  if (!resp) {
-    return res.status(HTTP_ERROR_STATUS).send({ message: 'Pessoa palestrante não encontrada' });
-  }
-  return res.status(HTTP_OK_STATUS).send(DB.find((item) => item.id === +id));
+app.use('/talker', primeiroGet);
+
+app.post('/loguin', (req, res) => {
+ const { email, password } = req.body; 
+ if (!email) {
+  return res.status(HTTP_ERROR_LOGUIN).send({ message: 'O campo "email" é obrigatório' });
+ }
+ if (!emailRegex.test(email)) {
+  return res.status(HTTP_ERROR_LOGUIN).send({ 
+    message: 'O "email" deve ter o formato "email@email.com"', 
+  });
+ }
+ if (!password) {
+  return res.status(HTTP_ERROR_LOGUIN).send({ message: 'O campo "password" é obrigatório' });
+ }
+ if (password.length < MinPassword) {
+  return res.status(HTTP_ERROR_LOGUIN).send({ 
+    message: 'O "password" deve ter pelo menos 6 caracteres', 
+  });
+ }
+  res.status(HTTP_OK_STATUS).send({ token });
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -31,7 +46,3 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
-
-// const rescue = require('express-rescue');
-// const randomBytes = require('crypto');
-// const token = randomBytes(8).toString('hex'); 
