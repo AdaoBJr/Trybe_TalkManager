67da7dd1ec31ccf4ac43e06e2e22700e3eea7693
Mariana Savoldi <marianasavodi123@gmail.com>
2021-09-03 22:04:34 -0300

    Rquisito 5 (finalmente) completo

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const {
 const app = express();
 app.use(bodyParser.json());
 
+const json = 'talker.json';
 const HTTP_OK_STATUS = 200;
 const HTTP_ERROR_STATUS = 404;
 const PORT = '3000';
@@ -39,7 +40,7 @@ app.get('/talker/:id', async (req, res) => {
 // https://stackoverflow.com/questions/17604866/difference-between-readfile-and-readfilesync
 app.get('/talker', (_req, res) => {
   try {
-    const talkers = fs.readFileSync('talker.json');
+    const talkers = fs.readFileSync(json);
     return res.status(HTTP_OK_STATUS).json(JSON.parse(talkers));
   } catch (error) {
     return res.status(HTTP_OK_STATUS).json(JSON.parse([]));
@@ -53,15 +54,29 @@ app.post('/login', validEmail, validPassword, (_req, res) => {
 
 app.post('/talker', validToken, validName, validAge,
   validTalk, validRate, validWatchedAt, (req, res) => {
-  const newTalker = fs.readFileSync('talker.json', 'utf8');
+  const newTalker = fs.readFileSync(json, 'utf8');
   const newTalkerJSON = JSON.parse(newTalker);
   const { name, age, talk } = req.body;
   const id = 5;
   const newObj = ({ id, name, age, talk });
   newTalkerJSON.push(newObj);
   // writeFileSync "escreve" o novo obj no arquivo json / stringfy para "stringar" o arquivo json
-  fs.writeFileSync('talker.json', JSON.stringify(newTalkerJSON));
+  fs.writeFileSync(json, JSON.stringify(newTalkerJSON));
   return res.status(201).json({ id, name, age, talk });
+});
+
+app.put('/talker/:id', validToken, validName, validAge,
+validTalk, validRate, validWatchedAt, async (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const upTalker = await fsp.readFile(json, 'utf8');
+  const upTalkerJSON = await JSON.parse(upTalker); // array
+  const findTalker = upTalkerJSON.find((e) => e.id === Number(id)); // objeto
+  const talkerIndex = upTalkerJSON.indexOf(findTalker, 0);
+  console.log(talkerIndex);
+  upTalkerJSON[talkerIndex] = { ...upTalkerJSON[talkerIndex], name, age, talk };
+  await fsp.writeFile(json, JSON.stringify(upTalkerJSON));
+  return res.status(200).json(upTalkerJSON[talkerIndex]);
 });
 
 app.use((err, _req, res, _next) => res.status(500).json({ error: `Erro: ${err.message}` }));
