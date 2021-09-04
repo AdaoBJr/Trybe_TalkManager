bacc326227720c5fa41f0d7140940b38cc50571f
Thiago Leite <leite.tjs@gmail.com>
2021-09-03 23:01:06 -0300

    testing 3rd requi

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const fs = require('fs').promises;
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const { validateEmail, validatePassword } = require('./middleware');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+// não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
 
 const getTalkers = () => fs.readFile('./talker.json', 'utf-8').then((res) => JSON.parse(res));
 
@@ -15,11 +21,11 @@ app.get('/talker/:id', async (req, res) => {
 
   const { id } = req.params;
   const talkerFiltered = talkerList.filter((manager) => parseInt(id, 10) === manager.id);
-  
+
   if (talkerFiltered.length === 0) {
     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   }
-  
+
   return res.status(HTTP_OK_STATUS).json(...talkerFiltered);
 });
 
@@ -29,46 +35,22 @@ app.get('/talker', async (req, res) => {
   if (!talkerList === []) {
     return res.status(HTTP_OK_STATUS).json([]);
   }
-  return (
-    res.status(HTTP_OK_STATUS).json(talkerList)
-  );
+  return res.status(HTTP_OK_STATUS).json(talkerList);
 });
 
-const validateEmail = (email) => {
-  const emailTester = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/i;
-  if (emailTester.test(email)) return true; 
-};
+app.use(validateEmail, validatePassword);
 
-const validatePassword = (password) => {
-  const PASSWORD_LENGTH = 6;
-  if (password.length >= PASSWORD_LENGTH) return true; 
-};
+app.post(
+  '/login',
+  validateEmail,
+  validatePassword,
+  (req, res) => {
+    const token = crypto.randomBytes(8).toString('hex');
 
-// app.use(authMiddleware);
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.headers;
-
-  if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
-
-  if (!password) return res.status(400).json({ message: 'O campo "passoword" é obrigatório' });
-
-  if (!validateEmail(email)) {
-    return res.status(400).json({ message: 'O "email" deve ter o formato email@email.com' });
-  }
-
-  if (!validatePassword(password)) {
-    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  }
-
-  return res.status(HTTP_OK_STATUS).json({ token: '7mqaVRXJSp886CGr' });
-});
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
-});
+    res.status(200).json({ message: token });
+  },
+);
 
 app.listen(PORT, () => {
-  console.log('Online');
+  console.log(`Online na porta: ${PORT}`);
 });
---
+++ middleware.js
@@ -0,0 +1,37 @@
+const validatePassword = (req, res, next) => {
+  const { password } = req.headers;
+  
+  const PASSWORD_LENGTH = 6;
+
+  if (!password) return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+
+  if (password.length < PASSWORD_LENGTH) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+};
+
+const validateEmail = (req, res, next) => {
+  const { email } = req.headers;
+
+  const emailTester = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/i;
+
+  if (!email) return res.status(401).json({ message: 'O campo "email é obrigatório' });
+
+  if (emailTester.test(email) === false) {
+    return res.status(401).json({ message: 'O "email deve ter o formato "email@"email.com' });
+  }
+  next();
+};
+
+// const generateToken = (req, res, next) => {
+//   const token = crypto.randomBytes(8).toString('hex');
+//   next();
+// };
+
+module.exports = {
+  validatePassword,
+  validateEmail,
+  // generateToken,
+};
