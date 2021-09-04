55b01eb18c2259427bc461da3d3804855e5b5441
Adão Benites <adaobjr@gmail.com>
2021-09-04 09:47:35 -0400

    refactor: :art: refactored files in middlewares folder

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,3 +1,21 @@
-import getAllTalkers from './getAllTalkers';
+import fs from 'fs';
 
-export default getAllTalkers;
+// REQUISITO 1 -- GET ALL TALKERS
+export const getAllTalkers = (_req, res) => {
+  const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
+
+  if (talkers.length > 0) { return res.status(200).json(talkers); }
+  if (talkers.length === 0) { return res.status(200).json([]); }
+};
+
+// REQUISITO 2 -- GET TALKERS BY ID
+export const getTalkersById = (req, res) => {
+  const { id } = req.params;
+
+  const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
+
+  const talker = talkers.find((person) => person.id === +id);
+
+  if (talker) { return res.status(200).json(talker); }
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+};
