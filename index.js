453e3d7460a219f5e1e93f84c888504e482fdc75
Aladino Borges <74330016+AladinoBorges@users.noreply.github.com>
2021-09-04 20:14:06 -0300

    Requisito 3: 100%, service handlers para o signup e validators.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 
 const { searchById } = require('./services/contentHandlers.js');
 const {
+  handleSignupInfo,
+  emailValidator,
+  passwordValidator,
+} = require('./services/signupAndLoginHandlers');
+const {
   handleFileReading,
   // handleFileWriting,
 } = require('./services/readAndWriteFilesHandler');
@@ -11,7 +16,8 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const HTTP_NOT_FIND_STATUS = 404;
+const HTTP_NOT_FOUND_STATUS = 404;
+const HTTP_BAD_REQUEST_STATUS = 400;
 const PORT = '3000';
 
 const filePaths = {
@@ -37,7 +43,6 @@ app.route('/talker').get(async (_request, response) => {
 // todo: Requisito 2.
 app.route('/talker/:id').get(async (request, response) => {
   const { id } = request.params;
-
   const talkersDatabase = await handleFileReading(filePaths.talker);
   const findedTalker = searchById(talkersDatabase, id);
 
@@ -46,8 +51,30 @@ app.route('/talker/:id').get(async (request, response) => {
   }
 
   return response
-    .status(HTTP_NOT_FIND_STATUS)
+    .status(HTTP_NOT_FOUND_STATUS)
     .json({ message: 'Pessoa palestrante não encontrada' });
+});
+
+// todo: Requisito 3.
+app.route('/login').post((request, response) => {
+  const { email, password } = request.body;
+  const validateEmail = emailValidator(email);
+  const validatePassword = passwordValidator(password);
+
+  if (validateEmail !== true) {
+    return response
+      .status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: validateEmail });
+  }
+
+  if (validatePassword !== true) {
+    return response
+      .status(HTTP_BAD_REQUEST_STATUS)
+      .json({ message: validatePassword });
+  }
+
+  const getToken = handleSignupInfo(email, password);
+  return response.status(HTTP_OK_STATUS).json({ token: getToken.token });
 });
 
 app.listen(PORT, () => {
---
+++ services/signupAndLoginHandlers.js
@@ -0,0 +1,63 @@
+const { randomBytes } = require('crypto');
+
+const MIN_PASSWORD_LENGTH = 6;
+
+const errorMessages = {
+  emptyEmail: 'O campo "email" é obrigatório',
+  invalidEmail: 'O "email" deve ter o formato "email@email.com"',
+  emptyPassword: 'O campo "password" é obrigatório',
+  invalidPassword: 'O "password" deve ter pelo menos 6 caracteres',
+};
+
+function emailValidator(email) {
+  const { emptyEmail, invalidEmail } = errorMessages;
+
+  const REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (!email || email === '') {
+    return emptyEmail;
+  }
+
+  const isValidEmail = REGEX.test(email);
+
+  if (!isValidEmail) {
+    return invalidEmail;
+  }
+
+  return isValidEmail;
+}
+
+function passwordValidator(password) {
+  const { emptyPassword, invalidPassword } = errorMessages;
+
+  if (!password) {
+    return emptyPassword;
+  }
+
+  const isValidPassword = password.length >= MIN_PASSWORD_LENGTH;
+
+  if (!isValidPassword) {
+    return invalidPassword;
+  }
+
+  return isValidPassword;
+}
+
+function uniqueTokenGenerator() {
+  const newToken = randomBytes(8).toString('hex');
+
+  return newToken;
+}
+
+function handleSignupInfo(email, password) {
+  const newUserToken = uniqueTokenGenerator();
+  const newUser = {
+    email,
+    password,
+    token: newUserToken,
+  };
+
+  return newUser;
+}
+
+module.exports = { handleSignupInfo, emailValidator, passwordValidator };
