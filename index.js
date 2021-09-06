c847dec40469811abbd303059104b19d4575dca4
Diegho <dieghonm@gmail.com>
2021-09-05 23:07:14 -0300

    Teste

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const primeiroGet = require('./routers/rotaTalker');
+// const primeiroGet = require('./routers/rotaTalker');
 const login = require('./routers/rotaLogin');
 
 const app = express();
@@ -10,7 +10,7 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 app.use('/login', login);
-app.use('/talker', primeiroGet);
+// app.use('/talker', primeiroGet);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
