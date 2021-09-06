ab46776c51310849e6fba8c564cb266dd8d786a5
Diegho <dieghonm@gmail.com>
2021-09-06 16:45:14 -0300

    Refazendo o 4 para tentar passar no avaliador, inicio

--- index.js
+++ index.js
@@ -1,151 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const talker = require('./routers/rotaTalker');
-// const login = require('./routers/rotaLogin');
+const talker = require('./routers/rotaTalker');
+const login = require('./routers/rotaLogin');
 
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = '3000';
 const HTTP_OK_STATUS = 200;
-const HTTP_ERROR_LOGUIN = 400;
 
-// app.use('/login', login);
-// app.use('/talker', talker);
-
-const { randomBytes } = require('crypto'); // token
-
-const emailRegex = RegExp(/[a-z0-9]+@[a-z0-9]+\.[a-z0-9]{2,3}(\.[a-z0-9]+)?$/);
-const MinPassword = 6;
-const token = randomBytes(8).toString('hex'); // token
-const fs = require('fs').promises;
-
-const HTTP_OK_POST = 201;
-const HTTP_ERROR_VALUE = 400;
-const HTTP_ERROR_TOKEN = 401;
-const HTTP_ERROR_STATUS = 404;
-const TOKEN_LENGTH = 16;
-const DBString = fs.readFile('talker.json', 'utf-8');
-
-app.get('/talker', async (req, res) => res.status(HTTP_OK_STATUS)
-  .send(JSON.parse(await fs.readFile('talker.json', 'utf-8'))));
-
-app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-  const DB = JSON.parse(await DBString);
-  const resp = DB.find((item) => item.id === +id);
-  if (!resp) {
-    return res.status(HTTP_ERROR_STATUS).send({ message: 'Pessoa palestrante não encontrada' });
-  }
-  return res.status(HTTP_OK_STATUS).send(DB.find((item) => item.id === +id));
-});
-
-function TokenCheck(req, res) {
-  const { authorization } = req.headers;
-  if (!authorization) {
-    return res.status(HTTP_ERROR_TOKEN).send({ message: 'Token não encontrado' });
-  }
-  if (authorization.length !== TOKEN_LENGTH) {
-    return res.status(HTTP_ERROR_TOKEN).send({ message: 'Token inválido' });
-  }
-}
-
-function NameCheck(name, res) {
-  if (!name) { 
-    return res.status(HTTP_ERROR_VALUE).send({ message: 'O campo "name" é obrigatório' });
-  }
-  if (name.length < 3) {
-    return res.status(HTTP_ERROR_VALUE)
-      .send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-  }
-}
-
-function AgeCheck(age, res) {
-  if (!age) { 
-    return res.status(HTTP_ERROR_VALUE).send({ message: 'O campo "age" é obrigatório' });
-  }
-  if (age < 18) {
-    return res.status(HTTP_ERROR_VALUE)
-      .send({ message: 'A pessoa palestrante deve ser maior de idade' });
-  }
-}
-
-function watchedAtChech(watchedAt, res) {
-  const dataRegex = RegExp(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/);
-  if (!dataRegex.test(watchedAt)) {
-    return res.status(HTTP_ERROR_VALUE)
-      .send({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
-  }
-}
-
-function TalkCheck(talk, res) {
-  if (!talk) {
-    return res.status(HTTP_ERROR_VALUE)
-    .send({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-  }
-  const { watchedAt, rate } = talk;
-  if (!talk.watchedAt || !talk.rate) {
-    return res.status(HTTP_ERROR_VALUE)
-    .send({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-  }
-  watchedAtChech(watchedAt, res);
-  const rateNumber = [1, 2, 3, 4, 5];
-  if (!rateNumber.includes(rate)) {
-    return res.status(HTTP_ERROR_VALUE)
-      .send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  }
-} 
-
-app.put('/talker/:id', async (req, res) => {
-  TokenCheck(req, res);
-  const { id } = req.params;
-  const oldData = JSON.parse(await DBString);
-  const index = oldData.findIndex((data) => data.id === +id);
-  const { name, age, talk } = req.body;
-  NameCheck(name, res);
-  AgeCheck(age, res);
-  TalkCheck(talk, res);
-  oldData[index] = { id: +id, name, age, talk };
-  await fs.writeFile('./talker.json', JSON.stringify(oldData));
-  return res.status(HTTP_OK_STATUS).send({ id: +id, name, age, talk });
-  // return res.status(index).send(name, age, talk);
-});
-
-app.post('/talker/', async (req, res) => {
-  const { name, age, talk } = req.body;
-  TokenCheck(req, res);
-  NameCheck(name, res);
-  AgeCheck(age, res);
-  TalkCheck(talk, res);
-  const oldData = JSON.parse(await DBString);
-  const newId = 1 + oldData.length;
-  const newData = { id: newId, name, age, talk };
-  oldData.push(newData);
-  await fs.writeFile('./talker.json', JSON.stringify(oldData));
-  return res.status(HTTP_OK_POST).send(newData);
-//  res.status(HTTP_OK_POST).send(name, age, talk);
-});
-
-app.post('/login', (req, res) => {
-  const { email, password } = req.body; 
-  if (!email) {
-   return res.status(HTTP_ERROR_LOGUIN).send({ message: 'O campo "email" é obrigatório' });
-  }
-  if (!emailRegex.test(email)) {
-   return res.status(HTTP_ERROR_LOGUIN).send({ 
-     message: 'O "email" deve ter o formato "email@email.com"', 
-   });
-  }
-  if (!password) {
-   return res.status(HTTP_ERROR_LOGUIN).send({ message: 'O campo "password" é obrigatório' });
-  }
-  if (password.length < MinPassword) {
-   return res.status(HTTP_ERROR_LOGUIN).send({ 
-     message: 'O "password" deve ter pelo menos 6 caracteres', 
-   });
-  }
-   res.status(HTTP_OK_STATUS).send({ token });
- });
+app.use('/login', login);
+app.use('/talker', talker);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
