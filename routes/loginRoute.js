9e76a6517fcc7d39677adcce7b9995bc9c5c4e50
CIGR2021 <appencigr@gmail.com>
2021-09-06 13:43:25 -0300

    Requisito 3 - Fix consertei as validaçoes email, password e token

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -9,32 +9,41 @@ const HTTP_ERROR_LOGUIN = 400;
 const isValidEmail = (req, res, next) => {
   const { email } = req.body;
   const emailRegex = /\S+@\S+\.\S+/i.test(email);
-  const fieldEmail = res.status(HTTP_ERROR_LOGUIN)
-    .send({ message: 'O campo "email" é obrigatório' });
-  const invalidEmail = res.status(HTTP_ERROR_LOGUIN)
-    .send({ message: 'O "email" deve ter o formato "email@email.com"' });
-
-  if (!email) return fieldEmail;
-  if (!emailRegex) return invalidEmail;
-
+  if (!email) {
+    return res.status(HTTP_ERROR_LOGUIN)
+      .json({ message: 'O campo "email" é obrigatório' });
+    }
+  if (!emailRegex) {
+    return res.status(HTTP_ERROR_LOGUIN)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+  }
   next();
 };
 
 const isValidPassword = (req, res, next) => {
   const { password } = req.body;
   const passowrdRegex = /[\w\D]{6}/.test(password);
-  const fieldPassword = res.status(HTTP_ERROR_LOGUIN)
-    .send({ message: 'O campo "password" é obrigatório' });
-  const invalidPassword = res.status(HTTP_ERROR_LOGUIN)
-    .send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
 
-  if (!password) return fieldPassword;
-  if (!passowrdRegex) return invalidPassword;
+  if (!password) {
+    return res.status(HTTP_ERROR_LOGUIN)
+      .json({ message: 'O campo "password" é obrigatório' });
+    }
+  if (!passowrdRegex) {
+    return res.status(HTTP_ERROR_LOGUIN)
+      .send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
 
   next();
 };
 
-loginRoute.post('/', isValidEmail, isValidPassword, (_req, res) => res.status(HTTP_OK_STATUS)
-  .json({ token: generatorToken() })); // Validando e-mail e password e gerando token
+loginRoute.post('/', isValidEmail, isValidPassword, async (req, res) => {
+  const { email, password } = req.body;
+  const getToken = await generatorToken();
+  return res.status(HTTP_OK_STATUS).json({ 
+    email,
+    password,
+    token: getToken,
+   });
+}); // Validando e-mail e password e gerando token
 
 module.exports = loginRoute;
