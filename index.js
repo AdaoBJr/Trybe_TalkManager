6e0ef3a4141f7e9d74908fdd48f81b54c5541ae6
Diegho <dieghonm@gmail.com>
2021-09-05 23:10:05 -0300

    Teste

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ const PORT = '3000';
 const HTTP_OK_STATUS = 200;
 
 app.use('/login', login);
-
-app.use('/talker', primeiroGet);
-
-app.use('/login', login);
 // app.use('/talker', primeiroGet);
 
 // não remova esse endpoint, e para o avaliador funcionar
