4ebd71b921dea1f489ca997a33d3ab1b35d7df5e
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-09-06 12:27:17 -0300

    Fix the require 4, try 1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const fs = require('fs').promises;
 // Busquei ajuda para utilizar o crypto nesse link:https://qastack.com.br/programming/8855687/secure-random-token-in-node-js;
 const crypto = require('crypto');
+// const { writeFile } = require('fs');
 // const authLoginAndPassword = require('./login');
 
 const app = express();
@@ -55,7 +56,6 @@ const validaEmail = (req, res, next) => {
           .status(HTTP_NOT_OK_STATUS)
             .json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
-
   next();
 };
 const validaSenha = (req, res, next) => {
@@ -73,15 +73,110 @@ const validaSenha = (req, res, next) => {
   next();
 };
 app.post('/login', validaEmail,
-  validaSenha,
-  (_req, res) => {
-    const tolken = crypto.randomBytes(8).toString('hex');
-    return res.status(200).json({ token: tolken });
-  });
+validaSenha,
+(_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ token });
+});
 
-// app.use((err, _req, res, _next) => {
-//   res.status(500).json({ message: `Opa! Deu ruim: ${err.message}` });
-// });
+// REQUISITO 4
+function validaToken(req, res, next) {
+  const currToken = req.headers.authorization;
+    if (!currToken) {
+      return res.status(401).json({ message: 'Token não encontrado' });
+    }
+    if (currToken.length !== 16) {
+      return res.status(401).json({ message: 'Token inválido' });
+    }
+  next();
+}
+
+function validaName(req, res, next) {
+  const { name } = req.body;
+
+  if (name === '' || name === undefined) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+}
+
+function validaIdade(req, res, next) {
+  const { age } = req.body;
+
+  if (Number.isInteger(age) === false || !age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+}
+
+function validaSeTalkExiste(req, res, next) {
+  const { talk } = req.body;
+  if (!talk || talk === undefined) {
+    return res
+    .status(400)
+    .json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  const { talk: { watchedAt, rate } } = req.body;
+  if (!watchedAt || !rate) {
+    return res
+      .status(400)
+        .json({
+          message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+        });
+  }
+
+  next();
+}
+
+function validaTalk(req, res, next) {
+  const { talk: { watchedAt, rate } } = req.body;
+  const arrDate = watchedAt.split('/');
+  if (arrDate[0].length > 2) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (Number.isInteger(rate) === false || (rate < 1 || rate > 5)) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+}
+
+app.post('/talker',
+  validaToken, validaName, validaIdade, validaSeTalkExiste, validaTalk, async (req, res) => {
+  const { name, age, talk: { watchedAt, rate } } = req.body;
+  const talkers = await fs.readFile('talker.json'); 
+  const indexTalker = JSON.parse(talkers);
+  const newObj = {
+    name,
+    age,
+    id: (indexTalker.length + 1),
+    talk: {
+      watchedAt,
+      rate,
+    },
+  };
+  const newArray = [...indexTalker, newObj];
+  await fs.writeFile('talker.json', newArray);
+  const newFieldAdded = await fs.readFile('talker.json');
+  const newFieldJson = JSON.parse(newFieldAdded);
+  const newField = newFieldJson.find((field) => field.name === name);
+  return res.status(201).json({ newField });
+});
+
+// Middleware de erro
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ message: `Opa! Deu ruim: ${err.message}` });
+});
 
 app.listen(PORT, () => {
   console.log('Online');
