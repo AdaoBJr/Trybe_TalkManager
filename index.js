935f741a0c8979ef409358615fb4d6e5e3bbb359
Diegho <dieghonm@gmail.com>
2021-09-06 19:25:00 -0300

    Refazendo 4, salva dados, sem validaçao 4

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ app.use(bodyParser.json());
 
 const PORT = '3000';
 const HTTP_OK_STATUS = 200;
-
-app.use('/login', login);
-app.use('/talker', talker);
-
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.use('/login', login);
+app.use('/talker', talker);
 
 app.listen(PORT, () => {
   console.log('Online');
