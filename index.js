125c1350aee26ab938e3a1029ffa953ae2739b24
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-09-06 10:17:16 -0300

    requisitos 4

--- index.js
+++ index.js
@@ -135,6 +135,12 @@ app.post('/talker',
   (request, response, next) => {
     const { talk } = request.body;
     
+    if (!talk) {
+      return response.status(400).json({
+        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+      });
+    }
+
     if (!(talk.watchedAt && talk.rate)) {
       return response.status(400).json({
         message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
