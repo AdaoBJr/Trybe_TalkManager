a5b52b2a30cf2b8c51db5d5c7d8e3865dccd6294
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-09-06 12:37:40 -0300

    requisitos 4

--- index.js
+++ index.js
@@ -6,12 +6,123 @@ const fs = require('fs').promises;
 const app = express();
 app.use(bodyParser.json());
 
+const runRead = () => fs.readFile('./talker.json', 'utf-8');
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const validToken = (request, response, next) => {
+  const token = request.headers.authorization;
+  
+  if (!token) {
+    return response.status(401).json({
+      message: 'Token não encontrado',
+    });
+  }
+
+  if (token.length !== 16) {
+    return response.status(401).json({
+      message: 'Token inválido',
+    });
+  }
+
+  next();
+};
+
+const validName = (request, response, next) => {
+  const { name } = request.body;
+  
+  if (!name) {
+    return response.status(400).json({
+      message: 'O campo "name" é obrigatório',
+    });
+  }
+
+  if (name.length < 3) {
+    return response.status(400).json({
+      message: 'O "name" deve ter pelo menos 3 caracteres',
+    });
+  }
+
+  next();
+};
+
+const validAge = (request, response, next) => {
+  const { age } = request.body;
+  
+  if (!age) {
+    return response.status(400).json({
+      message: 'O campo "age" é obrigatório',
+    });
+  }
+
+  if (typeof age !== 'number' && age < 18) {
+    return response.status(400).json({
+      message: 'A pessoa palestrante deve ser maior de idade',
+    });
+  }
+
+  next();
+};
+
+const validTalk = (request, response, next) => {
+  const { talk } = request.body;
+  
+  if (!talk) {
+    return response.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+
+  if (!(talk.watchedAt && talk.rate)) {
+    return response.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  
+  next();
+};
+
+const validTalkKeys = (request, response, next) => {
+  const { talk } = request.body;
+  const formartDat = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+  
+  if (!formartDat.test(talk.watchedAt)) {
+    return response.status(400).json({
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
+    });
+  }
+  console.log(talk.rate);
+  if (typeof talk.rate !== 'number' && (talk.rate > 5 || talk.rate < 1)) {
+    return response.status(400).json({
+      message: 'O campo "rate" deve ser um inteiro de 1 à 5',
+    });
+  }
+  
+  next();
+};
+
+// requisito 5
+
+app.put('/talker/:id', validToken, validName, validAge, validTalk, validTalkKeys,
+  
+  async (request, response) => {
+    const { id } = request.params;
+    const { name, age, talk } = request.body;
+    const arrayTalker = await runRead();
+    const talker = JSON.parse(arrayTalker);
+    const talkerIndex = talker.findIndex((item) => item.id === parseInt(id, 10));
+    
+    talker[talkerIndex] = { ...talker[talkerIndex], name, age, talk };
+
+    await fs.writeFile('./talker.json', JSON.stringify(talker));
+
+    response.status(201).json(talker[talkerIndex]);
+  });
+
 // requisito 2
+
 app.get('/talker/:id', async (request, response) => {
-  const arrayTalker = await fs.readFile('./talker.json', 'utf-8');
+  const arrayTalker = await runRead();
   const talker = JSON.parse(arrayTalker);
   const { id } = request.params;
   const newArray = talker.find((req) => req.id === parseInt(id, 10));
@@ -27,7 +138,7 @@ app.get('/talker/:id', async (request, response) => {
 
 // requisito 1
 app.get('/talker', async (_request, response) => {
-  const arrayTalker = await fs.readFile('./talker.json', 'utf-8');
+  const arrayTalker = await runRead();
   const talker = JSON.parse(arrayTalker);
   if (arrayTalker.length === 0) {
     return response.status(200).json([]);
@@ -80,98 +191,12 @@ app.post('/login',
 
 // requisito 4
 
-app.post('/talker', 
-  (request, response, next) => {
-    const token = request.headers.authorization;
-    
-    if (!token) {
-      return response.status(401).json({
-        message: 'Token não encontrado',
-      });
-    }
-
-    if (token.length !== 16) {
-      return response.status(401).json({
-        message: 'Token inválido',
-      });
-    }
-
-    next();
-  },
-  (request, response, next) => {
-    const { name } = request.body;
-    
-    if (!name) {
-      return response.status(400).json({
-        message: 'O campo "name" é obrigatório',
-      });
-    }
-
-    if (name.length < 3) {
-      return response.status(400).json({
-        message: 'O "name" deve ter pelo menos 3 caracteres',
-      });
-    }
-
-    next();
-  },
-  (request, response, next) => {
-    const { age } = request.body;
-    
-    if (!age) {
-      return response.status(400).json({
-        message: 'O campo "age" é obrigatório',
-      });
-    }
-
-    if (typeof age === 'number' && age < 18) {
-      return response.status(400).json({
-        message: 'A pessoa palestrante deve ser maior de idade',
-      });
-    }
-
-    next();
-  },
-  (request, response, next) => {
-    const { talk } = request.body;
-    
-    if (!talk) {
-      return response.status(400).json({
-        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-      });
-    }
-
-    if (!(talk.watchedAt && talk.rate)) {
-      return response.status(400).json({
-        message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
-      });
-    }
-    
-    next();
-  },
-  (request, response, next) => {
-    const { talk } = request.body;
-    const formartDat = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
-    
-    if (!formartDat.test(talk.watchedAt)) {
-      return response.status(400).json({
-        message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"',
-      });
-    }
-
-    if (typeof talk.rate === 'number' && talk.rate < 5 && talk.rate > 1) {
-      return response.status(400).json({
-        message: 'O campo "rate" deve ser um inteiro de 1 à 5',
-      });
-    }
-    
-    next();
-  },
+app.post('/talker', validToken, validName, validAge, validTalk, validTalkKeys,
+  
   async (request, response) => {
     const { name, age, talk } = request.body;
-    const arrayTalker = await fs.readFile('./talker.json', 'utf-8');
+    const arrayTalker = await runRead();
     const talker = JSON.parse(arrayTalker);
-    
     const number = talker.length;
 
     const newTalker = {
