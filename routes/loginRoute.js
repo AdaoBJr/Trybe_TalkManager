9686cb47e1e34ed276b2b3b44daf2b81ecb58fb7
CIGR2021 <appencigr@gmail.com>
2021-09-06 14:47:43 -0300

    Requisito 4 - Validaçoes: Adicionei as validaçoes

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -4,17 +4,17 @@ const generatorToken = require('../generatorToken');
 const loginRoute = express.Router();
 
 const HTTP_OK_STATUS = 200;
-const HTTP_ERROR_LOGUIN = 400;
+const HTTP_BAD_REQUEST = 400;
 
 const isValidEmail = (req, res, next) => {
   const { email } = req.body;
   const emailRegex = /\S+@\S+\.\S+/i.test(email);
   if (!email) {
-    return res.status(HTTP_ERROR_LOGUIN)
+    return res.status(HTTP_BAD_REQUEST)
       .json({ message: 'O campo "email" é obrigatório' });
     }
   if (!emailRegex) {
-    return res.status(HTTP_ERROR_LOGUIN)
+    return res.status(HTTP_BAD_REQUEST)
       .json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
   }
   next();
@@ -25,11 +25,11 @@ const isValidPassword = (req, res, next) => {
   const passowrdRegex = /[\w\D]{6}/.test(password);
 
   if (!password) {
-    return res.status(HTTP_ERROR_LOGUIN)
+    return res.status(HTTP_BAD_REQUEST)
       .json({ message: 'O campo "password" é obrigatório' });
     }
   if (!passowrdRegex) {
-    return res.status(HTTP_ERROR_LOGUIN)
+    return res.status(HTTP_BAD_REQUEST)
       .send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
--- routes/talkerRoute.js
+++ routes/talkerRoute.js
@@ -7,6 +7,7 @@ const filePath = join('talker.json');
 
 const HTTP_OK_STATUS = 200;
 const HTTP_CREATED_STATUS = 201;
+const HTTP_BAD_REQUEST = 400;
 const HTTP_ERROR_NOT_FOUND = 404;
 
 const getTalkers = () => {
@@ -18,6 +19,95 @@ const getTalkers = () => {
   } catch (error) {
     return [];
   }
+};
+
+const saveTalker = (talkers) => {
+  fs.writeFileSync(filePath, JSON.stringify(talkers, null, '\t'));
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  const DEFAULT_AGE = 18;
+
+  if (!age) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < DEFAULT_AGE) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ mesage: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const validateDate = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const dateRegex = /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/g;
+
+  if (!dateRegex.test(watchedAt)) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  const DEFAULT_LENGTH = 3;
+
+  if (!name) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < DEFAULT_LENGTH) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O campo "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const validateRate = (req, res, next) => {
+  const { rate } = req.body.talk;
+
+  if (!Number.isInteger(rate) && rate >= 1 && rate <= 5) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'O campo "rate" deve ser um inteiro de 1 a 5' });
+  }
+  next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk, rate, talk: { watchedAt } } = req.body;
+
+  if (!talk || typeof rate !== 'number' || !watchedAt) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({
+        message: `O campo "talk"
+      é obrigatório e "watchedAt" e "rate" não podem ser vazios` });
+  }
+  next();
+};
+
+const validateToken = (req, res, next) => {
+  const { authorization } = req.headers;
+  const DEFAULT_LENGTH = 16;
+
+  if (!authorization) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'Token não encontrado' });
+  }
+
+  if (authorization.length < DEFAULT_LENGTH) {
+    return res.status(HTTP_BAD_REQUEST)
+      .json({ message: 'Token inválido' });
+  }
+
+  next();
 };
 
 talkerRoute.get('/', (_req, res) => {
@@ -36,16 +126,32 @@ talkerRoute.get('/:id', (req, res) => {
   return result;
 }); // Filtrando por Id de usuário
 
-const saveTalker = (talkers) => {
-  fs.writeFileSync(filePath, JSON.stringify(talkers, null, '\t'));
-};
+talkerRoute.post('/',
+  validateAge,
+  validateDate,
+  validateName,
+  validateRate,
+  validateTalk,
+  validateToken,
+  (req, res) => {
+    const talkers = getTalkers();
+    talkers.push(req.body);
+    saveTalker(talkers);
+
+    return res.status(HTTP_CREATED_STATUS).json(talkers);
+  }); // Adicionando usuários
 
 talkerRoute.post('/', (req, res) => {
   const talkers = getTalkers();
-  talkers.push(req.body);
   saveTalker(talkers);
+  saveTalker(talkers.map((talker) => {
+    if (talker.id === req.params.id) {
+      return { ...talker, ...req.body };
+    }
+    return talker;
+  }));
 
   return res.status(HTTP_CREATED_STATUS).send(talkers);
-}); // Adicionando usuários
+}); // Atualizando usuários
 
 module.exports = talkerRoute;
