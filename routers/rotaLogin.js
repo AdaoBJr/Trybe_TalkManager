a76171ab0005748a0d50cccb2dd210593ec82d68
Diegho <dieghonm@gmail.com>
2021-09-06 20:04:50 -0300

    Refazendo 4, salva dados, sem validaçao 9

--- routers/rotaLogin.js
+++ routers/rotaLogin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { randomBytes } = require('crypto'); // token
 
 const HTTP_OK_STATUS = 200;
-const HTTP_ERROR_LOGUIN = 400;
+const HTTP_ERROR_LOGIN = 400;
 
 const emailRegex = RegExp(/[a-z0-9]+@[a-z0-9]+\.[a-z0-9]{2,3}(\.[a-z0-9]+)?$/);
 const MinPassword = 6;
@@ -13,22 +13,22 @@ const token = randomBytes(8).toString('hex'); // token
 router.post('/', (req, res) => {
   const { email, password } = req.body; 
   if (!email) {
-   return res.status(HTTP_ERROR_LOGUIN).send({ message: 'O campo "email" é obrigatório' });
+   return res.status(HTTP_ERROR_LOGIN).send({ message: 'O campo "email" é obrigatório' });
   }
   if (!emailRegex.test(email)) {
-   return res.status(HTTP_ERROR_LOGUIN).send({ 
+   return res.status(HTTP_ERROR_LOGIN).send({ 
      message: 'O "email" deve ter o formato "email@email.com"', 
    });
   }
   if (!password) {
-   return res.status(HTTP_ERROR_LOGUIN).send({ message: 'O campo "password" é obrigatório' });
+   return res.status(HTTP_ERROR_LOGIN).send({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < MinPassword) {
-   return res.status(HTTP_ERROR_LOGUIN).send({ 
+   return res.status(HTTP_ERROR_LOGIN).send({ 
      message: 'O "password" deve ter pelo menos 6 caracteres', 
    });
   }
-   res.status(HTTP_OK_STATUS).send({ token });
+   return res.status(HTTP_OK_STATUS).send({ token });
  });
 
 module.exports = router;
