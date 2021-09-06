962040b2b30e69ade25001f8d9f58d9fb6ccec71
Diegho <dieghonm@gmail.com>
2021-09-06 17:33:03 -0300

    Refazendo 4, salva dados, sem validaçao

--- routers/rotaTalker.js
+++ routers/rotaTalker.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const { promisify } = require('util');
 
-const router = express.Router();
-const fs = require('fs').promises;
+const Talker = express.Router();
+const fs = require('fs');
 
 const HTTP_OK_STATUS = 200;
 // const HTTP_OK_POST = 201;
@@ -9,14 +10,14 @@ const HTTP_OK_STATUS = 200;
 // const HTTP_ERROR_TOKEN = 401;
 const HTTP_ERROR_STATUS = 404;
 // const TOKEN_LENGTH = 16;
-const DBString = fs.readFile('talker.json', 'utf-8');
+const DBString = 'talker.json';
 
-router.get('/', async (req, res) => res.status(HTTP_OK_STATUS)
-  .send(JSON.parse(await fs.readFile('talker.json', 'utf-8'))));
+Talker.get('/', async (req, res) => res.status(HTTP_OK_STATUS)
+  .send(JSON.parse(await fs.readFile(DBString, 'utf-8'))));
 
-router.get('/:id', async (req, res) => {
+Talker.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const DB = JSON.parse(await DBString);
+  const DB = JSON.parse(await fs.readFile(DBString, 'utf-8'));
   const resp = DB.find((item) => item.id === +id);
   if (!resp) {
     return res.status(HTTP_ERROR_STATUS).send({ message: 'Pessoa palestrante não encontrada' });
@@ -24,4 +25,17 @@ router.get('/:id', async (req, res) => {
   return res.status(HTTP_OK_STATUS).send(DB.find((item) => item.id === +id));
 });
 
-module.exports = router;
+Talker.post('/', async (req, res) => {
+  // const DB = JSON.parse(await fs.readFile(DBString, 'utf-8'));
+  // console.log(DB, 'entrei');
+
+  fs.readFile(DBString, 'utf-8', promisify((err, content) => {
+    const { body } = req;
+    const DB = JSON.parse(content);
+    const NewDB = [...DB, body];
+    fs.writeFileSync(DBString, JSON.stringify(NewDB));
+    res.status(200).send({ message: 'Teste OK' });
+  }));
+});
+
+module.exports = Talker;
