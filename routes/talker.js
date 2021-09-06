ae1bf7abfac18f3ba873b92b26731c01a6be322b
alantanaka <alanctnk@gmail.com>
2021-09-06 00:12:21 -0300

    req 5

--- routes/talker.js
+++ routes/talker.js
@@ -6,9 +6,11 @@ const route = express.Router();
 const getListTalker = require('../middlewares/getListTalker');
 const getTalkerId = require('../middlewares/getTalkerId');
 const createTalker = require('../middlewares/createTalker');
+const editTalker = require('../middlewares/editTalker');
 
 route.get('/:id', getTalkerId);
 route.get('/', getListTalker);
 route.post('/', createTalker);
+route.put('/:id', editTalker);
 
 module.exports = route;
