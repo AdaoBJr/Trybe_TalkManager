89630858ae41a5cb5b5eea38d23f8960c6b45e49
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-09-07 10:46:08 -0300

    requisito 2 funcionando

--- routes/talker.js
+++ routes/talker.js
@@ -10,4 +10,14 @@ app.get('/', (req, res) => {
    res.status(200).json(talkers);
 });
 
+app.get('/:id', (req, res) => {
+  const idParam = req.params.id;
+  const talkers = JSON.parse(fs.readFileSync(`${__dirname}/../talker.json`));
+  const talker = talkers.find(({ id }) => id === Number(idParam));
+  if (talker) {
+    return res.status(200).json(talker);
+  }
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
 module.exports = app;
