a3337f337c40f576a52bc3b7ee0d38a938eca999
Aremtior <marcosleandrofl@gmail.com>
2021-09-07 00:46:33 -0300

    requirement 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const randtoken = require('rand-token');
+const date = require('date-and-time');
 const fs = require('fs');
 
 const app = express();
@@ -34,6 +35,52 @@ function validateData(req, res, next) {
    next(); 
 }
 
+function validateRate(rate) {
+  if (rate < 1 || rate > 5) {
+    return 'O campo "rate" deve ser um inteiro de 1 à 5';
+  }
+  return false;
+}
+
+function validateWatchedAt(talk) {
+  if (!date.isValid(talk.watchedAt, 'DD-MM-YYYY')) {
+    return 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"';
+  }
+  return validateRate(talk.rate);
+}
+
+function validateTalk(talk) {
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios';
+  }
+  return validateWatchedAt(talk);
+}
+
+function validateAge(age, talk) {
+  if (!age || age.trim() === '') return 'O campo "age" é obrigatório';
+  if (age < 18) return 'A pessoa palestrante deve ser maior de idade';
+  return validateTalk(talk);
+}
+
+function validateName(name, age, talk) {
+  if (!name || name.trim() === '') return 'O campo "name" é obrigatório';
+  if (name.length < 3) return 'O "name" deve ter pelo menos 3 caracteres';
+  return validateAge(age, talk);
+}
+
+function validateToken(req, res, next) {
+  const { token } = req.headers;
+  const { name, age, talk } = req.body;
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  const message = validateName(name, age, talk);
+  if (message) {
+   return res.status(400).json({ message });
+  }
+  
+  next(); 
+}
+
 function readData() {
   try {
     const data = fs.readFileSync(talkerData, 'utf8');
@@ -45,7 +92,9 @@ function readData() {
 }
 
 app.post('/login', validateData, (req, res) => {
-  res.status(HTTP_OK_STATUS).json({ token: randtoken.generate(16) });
+  const token = randtoken.generate(16);
+  res.status(HTTP_OK_STATUS).json({ token });
+  req.headers.token = token;
 });
 
 app.get('/talker', (req, res) => {
@@ -64,6 +113,13 @@ app.get('/talker/:id', (req, res) => {
   if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 
   res.status(HTTP_OK_STATUS).json(talker);
+});
+
+app.post('/talker', validateToken, (req, res) => {
+  const { name, age, talk } = req.body;
+  const data = { id: 1, name, age, talk };
+  fs.writeFileSync(talkerData, data);
+  res.status(201).json(data);
 });
 
 // não remova esse endpoint, e para o avaliador funcionar
