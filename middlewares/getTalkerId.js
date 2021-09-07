e447df92dd01655245a945b97d780edf57cea9c8
Letycia Barreto <letyciasbarreto@gmail.com>
2021-09-07 10:36:20 -0300

    feat req 2

--- middlewares/getTalkerId.js
+++ middlewares/getTalkerId.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const getTalkerId = (req, res) => {
   const { id } = req.params;
   const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
-  const findTalker = talkers.findTalker((f) => f.id === +id);
+  const findTalker = talkers.find((f) => f.id === +id);
   if (findTalker) {
     return res.status(200).json(findTalker);
   }
