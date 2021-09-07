6d4a1cc24658a4259052ee6e47e5613f0cbed479
Diegho <dieghonm@gmail.com>
2021-09-06 22:46:32 -0300

    Requisito 2 refeito

--- routers/rotaTalker.js
+++ routers/rotaTalker.js
@@ -4,6 +4,7 @@ const express = require('express');
 const Talker = express.Router();
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 404;
 
 const OldDB = async () => {
   const DB = await fs.readFile('./talker.json', 'utf-8');
@@ -14,3 +15,15 @@ Talker.get('/', async (req, res) => {
   const DB = await OldDB();
   return res.status(HTTP_OK_STATUS).send(DB);
 });
+
+Talker.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const DB = await OldDB();
+  const resp = DB.find((item) => item.id === +id);
+  if (!resp) {
+    return res.status(HTTP_ERROR_STATUS).send({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(HTTP_OK_STATUS).send(DB.find((item) => item.id === +id));
+});
+
+module.exports = Talker;
