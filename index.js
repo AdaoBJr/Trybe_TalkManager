a6e98a3db446a7e063ec6e33e81f81c6ec5c7faa
marcos <marcosd3wow@gmail.com>
2021-09-08 08:48:47 -0300

    subindo req 1 para test

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const talker = require('./talker.json');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (_req, res) => {
+  !talker ? res.status(200).json([]) : res.status(200).json(talker);
+});
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
