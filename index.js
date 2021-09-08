4c02467dc6a178261553780382a00ac4df45be34
Pedro Toselli <pedrotrybe@gmail.com>
2021-09-08 14:27:34 -0300

    Requisito 1 - refatorado

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', getAllTalkers);
+
 app.listen(PORT, () => {
   console.log('Online');
 });
-
-app.get('/talker', getAllTalkers);
--- routers/getAllTalkers.js
+++ routers/getAllTalkers.js
@@ -1,15 +1,12 @@
-const express = require('express');
 const fs = require('fs');
-
-const router = express.router();
 
 const data = 'talker.json';
 
-router.get('/talker', (_req, res) => {
+const getAlltakers = (_req, res) => {
   fs.readFile(data, 'utf-8')
     .then((response) => JSON.parse(response))
     .then((result) => res.status(200).json(result))
     .catch((_error) => []);
-});
+};
 
-module.exports = router;
+module.exports = getAlltakers;
