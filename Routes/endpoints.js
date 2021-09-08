0f5d87570f846cc386ba13268007b4ece5ea71f0
Holistic <paulofreire.si@outlook.com>
2021-09-08 13:40:46 -0300

    req4

--- Routes/endpoints.js
+++ Routes/endpoints.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const fs = require('fs').promises;
+const { token, data, Age, Name, thisTalk } = require('
 
 const readFile = () => {
     const file = fs.readFile('./talker.json', 'utf8')
@@ -21,6 +22,27 @@ router.get('/:id', async (req, res) => {
   const filtered = readX.find((resp) => Number(resp.id) === Number(id));
   if (!filtered) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   res.status(200).json(filtered);
+});
+
+router.post('/',
+  token,
+  Name,
+  Age,
+  data,
+  thisTalk,
+  async (req, res) => {
+    const file = await readFile();
+    const id = file.length + 1;
+    const { name, age, talk } = req.body;
+    const result = {
+      id,
+      name,
+      age,
+      talk,
+    };
+    const newTalkers = JSON.stringify([...file, result]);
+    await fs.writeFile('talker.json', newTalkers);
+    res.status(201).json(result);
 });
 
 module.exports = router;
---
+++ Routes/post.js
@@ -0,0 +1,55 @@
+const regexData = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+
+const token = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (token.length !== 16) return res.status(401).json({ message: 'Token inválido' });
+  next();
+};
+
+const Name = (req, res, next) => {
+  const { Name } = req.body;
+  if (!Name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (Name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+const Age = (req, res, next) => {
+  const { age } = req.body;
+  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (Number(age) < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de age' });
+  } 
+  next();
+};
+
+const data = (req, res, next) => {
+  const { talk: { watchedAt, rate } } = req.body;
+  if (!watchedAt.match(regexData)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const thisTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk || Object.keys(talk).length < 2) {
+    return res.status(400).json(
+      { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
+    );
+  }
+  next();
+};
+
+module.exports = {
+  token,
+  Name,
+  Age,
+  data,
+  thisTalk,
+};
