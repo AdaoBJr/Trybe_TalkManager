cb86f71a4256c51e9a3ec1d6233379aa3a1b4e9e
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-07 21:22:23 -0300

    Feat: Tentativa 1 requisito 4

--- middlewares/index.js
+++ middlewares/index.js
@@ -4,12 +4,27 @@ const crypto = require('crypto');
 
 const token = crypto.randomBytes(8).toString('hex');
 
+const tokenValid = (req, res, next) => {
+  const { authorization } = req.headers;
+ 
+  if (!authorization) {
+   return res.status(401).json({ message: 'Token não encontrado' });
+  }
+ 
+  if (authorization.length !== 16) {
+   return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+ };
+
+const fileCall = './talker.json';
+
 const HTTP_OK_STATUS = 200;
 const HTPP_ERROR_STATUS = 404;
 
 // 1
 const getAll = async (_req, res) => {
-  const file = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+  const file = JSON.parse(await fs.readFile(fileCall, 'utf-8'));
 
   res.status(HTTP_OK_STATUS).json(file);
 };
@@ -17,7 +32,7 @@ const getAll = async (_req, res) => {
 // 2
 const getFilterId = async (req, res) => {
   const { id } = req.params;
-  const file = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+  const file = JSON.parse(await fs.readFile(fileCall, 'utf-8'));
   const fileFilter = file.find((item) => item.id === Number(id));
 
   if (!fileFilter) {
@@ -60,4 +75,93 @@ const postLogin = (_req, res) => {
   res.status(200).json({ token });
 };
 
-module.exports = { getAll, getFilterId, postLogin, emailValid, passwordValid };
+// 4
+const nameValid = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  next();
+};
+
+// 4
+const ageValid = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (age < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  next();
+};
+
+// 4
+const watchedAtValid = (req, res, next) => {
+  const { talk: { watchedAt } } = req.body;
+  const dataRegex = RegExp(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/);
+  
+  if (!watchedAt) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios', 
+    });
+  }
+  if (!dataRegex.test(watchedAt)) {
+    return res.status(400).json({ 
+      message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"', 
+    });
+  }
+  next();
+};
+
+const rateValid = (req, res, next) => {
+  const { talk: { rate } } = req.body;
+
+  if (!rate) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios', 
+  });
+  }
+
+  if (rate < 1 || rate > 5) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const postTalker = async (req, res) => {
+  const { name, age, talk } = req.body;
+  const file = JSON.parse(await fs.readFile(fileCall, 'utf-8'));
+  const newTalker = {
+    id: file.length + 1,
+    name,
+    age,
+    talk,
+  };
+
+  file.push(newTalker);
+  await fs.writeFile(fileCall, JSON.stringify(file));
+  
+  return res.status(201).json(newTalker);
+};
+
+module.exports = { 
+  getAll,
+  getFilterId, 
+  postLogin, 
+  emailValid, 
+  passwordValid,
+  tokenValid,
+  nameValid,
+  ageValid,
+  watchedAtValid,
+  rateValid,
+  postTalker, 
+};
--- routers/talkersRouter.js
+++ routers/talkersRouter.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
-const { getAll, getFilterId } = require('../middlewares/index');
+const { 
+  getAll,
+  getFilterId,
+  tokenValid,
+  nameValid,
+  ageValid,
+  watchedAtValid,
+  rateValid,
+  postTalker, 
+} = require('../middlewares/index');
 
 router.get('/', getAll);
 
 router.get('/:id', getFilterId);
+
+router.post('/', tokenValid, nameValid, ageValid, watchedAtValid, rateValid, postTalker);
 
 module.exports = router;
