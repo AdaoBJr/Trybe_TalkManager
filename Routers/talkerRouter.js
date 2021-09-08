e1a10e94da978bf6e80bb4b4975672c418ac6d6d
palenske <viniciuspalenske@gmail.com>
2021-09-08 14:07:47 -0300

    entrega desafio 4

--- Routers/talkerRouter.js
+++ Routers/talkerRouter.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const fs = require('fs').promises;
+const {
+  validateToken,
+  validateNameAge,
+  validateTalk,
+  validateDateRate,
+} = require('../auth/authPost');
 
 const router = express.Router();
 const getTalkers = async () => JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
+const putTalkers = (content) => fs.writeFile('./talker.json', JSON.stringify(content));
 
-router.get('/talker', async (_req, res) => {
-  const talkers = await getTalkers();
-  return talkers.length === 0
-  ? res.status(200).json([])
-  : res.status(200).json(talkers);
-});
+router.route('/talker')
+  .get(async (_req, res) => {
+    const talkers = await getTalkers();
+    return talkers.length === 0
+    ? res.status(200).json([])
+    : res.status(200).json(talkers);
+  })
+  .post(validateToken, validateNameAge, validateTalk, validateDateRate, async (req, res) => {
+    const talkers = await getTalkers();
+    talkers.push(req.body);
+    putTalkers(talkers);
+    res.status(201).json(req.body);
+  });
 
 router.get('/talker/:id', async (req, res) => {
   const talkers = await getTalkers();
