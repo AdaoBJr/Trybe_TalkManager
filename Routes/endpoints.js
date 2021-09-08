9c58e4deb8457f99f74f5d666060406fc0646bd3
Holistic <paulofreire.si@outlook.com>
2021-09-08 06:39:46 -0300

    eslint

--- Routes/endpoints.js
+++ Routes/endpoints.js
@@ -9,13 +9,13 @@ const reader = () => {
   return file;
 };
 
-router.get('/', async (req, res) => {
+router.get('/', async (_req, res) => {
   const read = await reader();
   if (!read) return res.status(200).json(Array.from([]));
 });
 
 router.get('/:id', async (req, res) => {
-  const { id } req.params;
+  const { id } = req.params;
   const read = await readFile();
   const filter = read.find((r) => Number(r.id) === Number(id));
   if (!filter) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
