a2e78e325696c8d381d66418afd58022f4e07c53
Holistic <paulofreire.si@outlook.com>
2021-09-08 13:22:51 -0300

    çld

--- Routes/login.js
+++ Routes/login.js
@@ -6,7 +6,7 @@ const crypto = require('crypto');
 const token = crypto.randomBytes(8).toString('hex');
 
 router.post('/', (req, res) => {
-  const { email, passwd } = req.body;
+  const { email, password } = req.body;
   const myToken = { token };
   if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
@@ -14,13 +14,13 @@ router.post('/', (req, res) => {
   if (!email.match(/\S+@\S+\.com/)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
-  if (!passwd) {
+  if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
-  if (passwd.length < 6) {
+  if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
   return res.status(200).json(myToken);
 });
 
-module.exports = router;
+module.exports = router; 
