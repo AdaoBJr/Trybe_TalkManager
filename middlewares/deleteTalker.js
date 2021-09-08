948c47836fbe5c797fc3f04748f98935f4ba7ef2
Letycia Barreto <letyciasbarreto@gmail.com>
2021-09-08 10:44:48 -0300

    testes

--- middlewares/deleteTalker.js
+++ middlewares/deleteTalker.js
@@ -1,16 +1,16 @@
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
 
-const deleteTalker = async (req, res) => {
-  const { id } = req.params;
-  const talker = JSON.parse(await fs.readFile('talker.json', 'utf-8'));
-  const findTalker = talker.findIndex((f) => f.id === Number(id));
-  if (findTalker === -1) {
-    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  }
-  talker.splice(findTalker, 1);
-  await fs.writeFileSync('talker.json', JSON.stringify(talker));
-  return res.status(200).send({ message: 'Pessoa palestrante deletada com sucesso' });
-};
-module.exports = deleteTalker;
+// const deleteTalker = async (req, res) => {
+//   const { id } = req.params;
+//   const talker = JSON.parse(await fs.readFile('talker.json', 'utf-8'));
+//   const findTalker = talker.findIndex((f) => f.id === Number(id));
+//   if (findTalker === -1) {
+//     return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+//   }
+//   talker.splice(findTalker, 1);
+//   await fs.writeFileSync('talker.json', JSON.stringify(talker));
+//   return res.status(200).send({ message: 'Pessoa palestrante deletada com sucesso' });
+// };
+// module.exports = deleteTalker;
 
-// requisito feito com ajuda de colegas
+// // requisito feito com ajuda de colegas
