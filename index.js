c88d5eb4771b8516a3ebdd80db12f2c3764cd1e5
palenske <viniciuspalenske@gmail.com>
2021-09-08 00:35:46 -0300

    entrega desafio 3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const genToken = require('./auth/genToken');
+const authLogin = require('./auth/authLogin');
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,5 +18,7 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.post('/login', authLogin, (_req, res) => res.status(200).json({ token: genToken() }));
 
 app.use('/', talkerRouter);
