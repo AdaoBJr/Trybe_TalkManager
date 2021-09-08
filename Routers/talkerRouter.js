0a5b94f2c04d323b782d287d0d2880f60e872627
palenske <viniciuspalenske@gmail.com>
2021-09-07 21:07:11 -0300

    [fix] mock substituido pelo framework fs

--- Routers/talkerRouter.js
+++ Routers/talkerRouter.js
@@ -1,34 +1,24 @@
 const express = require('express');
+const fs = require('fs/promises');
 
 const router = express.Router();
+const getTalkers = async () => JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
 
-const talkers = [
-  {
-    name: 'Henrique Albuquerque',
-    age: 62,
-    id: 1,
-    talk: { watchedAt: '23/10/2020', rate: 5 },
-  },
-  {
-    name: 'Heloísa Albuquerque',
-    age: 67,
-    id: 2,
-    talk: { watchedAt: '23/10/2020', rate: 5 },
-  },
-  {
-    name: 'Ricardo Xavier Filho',
-    age: 33,
-    id: 3,
-    talk: { watchedAt: '23/10/2020', rate: 5 },
-  },
-  {
-    name: 'Marcos Costa',
-    age: 24,
-    id: 4,
-    talk: { watchedAt: '23/10/2020', rate: 5 },
-  },
-];
+router.get('/talker', async (_req, res) => {
+  const talkers = await getTalkers();
+  return talkers.length === 0
+  ? res.status(200).json([])
+  : res.status(200).json(talkers);
+});
 
-router.get('/talker', (_req, res) => res.status(200).json(talkers));
+router.get('/talker/:id', async (req, res) => {
+  const talkers = await getTalkers();
+  const { id } = req.params;
+  const talker = talkers.find((t) => t.id === Number(id));
+
+  return !talker
+  ? res.status(404).json({ message: 'Pessoa palestrante não encontrada' })
+  : res.status(200).json(talker);
+});
 
 module.exports = router;
