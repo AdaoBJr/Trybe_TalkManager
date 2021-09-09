1f28c4348c943d3b131eb477ad21c2c68822a924
Pedro Toselli <pedrotrybe@gmail.com>
2021-09-09 10:31:12 -0300

    Requisito 1 - refatorado

--- middleweres/talkers.js
+++ middleweres/talkers.js
@@ -1,24 +1,32 @@
 const fs = require('fs');
 
 const file = './talker.json';
-const HTTP_OK_STATUS = 200;
+const OK_STATUS = 200;
 
-const getTalkers = async () => {
-  const data = fs.readFile(file, 'utf-8');
-  const talkers = await JSON.parse(data);
+// const getTalkers = async () => {
+//   const data = fs.readFile(file, 'utf-8');
+//   const talkers = await JSON.parse(data);
 
-  return talkers;
+//   return talkers;
+// };
+
+const getTalkers = () => {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      return null;
+    }
+    return data;
+  });
 };
 
 const getAllTalkers = async (_req, res) => {
   const talkers = await getTalkers();
   if (!talkers.length) {
-    return res.status(HTTP_OK_STATUS).send([]);
+    return res.status(OK_STATUS).send([]);
   }
-  return res.status(HTTP_OK_STATUS).json(talkers);
+  return res.status(OK_STATUS).json(talkers);
 };
 
 module.exports = {
-  getTalkers,
   getAllTalkers,
 };
