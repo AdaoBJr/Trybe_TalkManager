040810a7441f4fda7e1a3e688f84ca08a3c507cc
Pedro Toselli <pedrotrybe@gmail.com>
2021-09-09 09:48:36 -0300

    Requisito 1 - refatorado

--- routers/talkers.js
+++ routers/talkers.js
@@ -10,11 +10,19 @@ const getTalkers = async () => {
   return talkers;
 };
 
-const getAllTalkers = (_req, res) => {
-  fs.readFile(file, 'utf-8')
-    .then((response) => JSON.parse(response))
-    .then((result) => res.status(HTTP_OK_STATUS).json(result))
-    .catch((_err) => []);
+// const getAllTalkers = (_req, res) => {
+//   fs.readFile(file, 'utf-8')
+//     .then((response) => JSON.parse(response))
+//     .then((result) => res.status(HTTP_OK_STATUS).json(result))
+//     .catch((_err) => []);
+// };
+
+const getAllTalkers = async (_req, res) => {
+  const talkers = await getTalkers();
+  if (!talkers.length) {
+    return res.status(HTTP_OK_STATUS).send([]);
+  }
+  return res.status(HTTP_OK_STATUS).send(talkers);
 };
 
 module.exports = {
