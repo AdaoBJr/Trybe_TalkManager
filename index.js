8d71874392d1342f53e6b24fc59b5ddc692ade65
João Victor Pistorio <joao.pistorio@gmail.com>
2021-09-10 18:32:19 -0300

    Requisito 2

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { handleFileReading } = require('./services/readAndWrite');
+const { searchById } = require('./services/content');
 
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
+const HTTP_OK = 200;
+const HTTP_NOT_FIND = 400;
 const PORT = '3000';
 
 const filePaths = {
@@ -15,7 +17,7 @@ const filePaths = {
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(HTTP_OK).send();
 });
 
 // Requisito 1
@@ -27,6 +29,22 @@ app.route('/talker').get(async (_request, response) => {
   }
 
   response.status(200).json([]);
+});
+
+// Requisito 2 
+app.route('/talker/:id').get(async (request, response) => {
+  const { id } = request.params;
+
+  const talkersDatabase = await handleFileReading(filePaths.talker);
+  const findedTalker = searchById(talkersDatabase, id);
+
+  if (findedTalker) {
+    return response.status(HTTP_OK).json(findedTalker);
+  }
+
+  return response
+    .status(HTTP_NOT_FIND)
+    .json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 app.listen(PORT, () => {
