9289770b05073d57238bf134568f35edd58053d8
João Victor Pistorio <joao.pistorio@gmail.com>
2021-09-10 18:22:05 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { handleFileReading } = require('./services/readAndWrite');
+
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const filePaths = {
+  talker: './talker.json',
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+// Requisito 1
+app.route('/talker').get(async (_request, response) => {
+  const contentFromFile = await handleFileReading(filePaths.talker);
+
+  if (contentFromFile) {
+    return response.status(200).json(contentFromFile);
+  }
+
+  response.status(200).json([]);
 });
 
 app.listen(PORT, () => {
