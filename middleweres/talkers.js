68449cebbbf048eeab48ed260b43b25734493147
Pedro Toselli <pedrotrybe@gmail.com>
2021-09-10 16:06:24 -0300

    Requisito 5 - refatorado

--- middleweres/talkers.js
+++ middleweres/talkers.js
@@ -123,7 +123,7 @@ const verifyRate = (req, res, next) => {
     });
   }
 
-  if (rate <= 0 || rate > 5) {
+  if (rate < 1 || rate > 5) {
     return res.status(BAD_REQ).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
