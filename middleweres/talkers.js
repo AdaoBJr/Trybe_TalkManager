46b72fd3bf369ece1f1618ca84a622a9fdeb3fa5
Pedro Toselli <pedrotrybe@gmail.com>
2021-09-10 16:33:21 -0300

    Requisito 5

--- middleweres/talkers.js
+++ middleweres/talkers.js
@@ -115,15 +115,15 @@ const verifyWatchedAt = (req, res, next) => {
 };
 
 const verifyRate = (req, res, next) => {
-  const { talk: { rate } } = req.body;
+  const { talk } = req.body;
 
-  if (!rate || rate === '') {
+  if (!talk.rate || talk.rate === '') {
     return res.status(BAD_REQ).json({
       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
   }
 
-  if (rate > 5 || rate < 1) {
+  if (talk.rate > 5 || talk.rate < 1) {
     return res.status(BAD_REQ).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
 
