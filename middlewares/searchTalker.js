a7eec81ebd8da55d7ea95600c819afac0892587a
Letycia Barreto <letyciasbarreto@gmail.com>
2021-09-11 18:07:22 -0300

    req 7 test

--- middlewares/searchTalker.js
+++ middlewares/searchTalker.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 const searchTalker = (req, res, _next) => {
-  const talkers = JSON.parse(fs.readFileSync('talker.json'));
   const { q } = req.query;
-  if (!q) return res.status(200).JSON(talkers);
+  const talkers = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
   const filterTalker = talkers.filter((f) => f.name.includes(q));
   return res.status(200).JSON(filterTalker);
 };
+
 module.exports = searchTalker;
