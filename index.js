65224e06b8c783d396cbb4c0205b7fa1beda7930
Daniel Alves de Araujo <araujodaniel321@gmail.com>
2021-09-11 12:41:10 -0300

    fixing lint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { getAllTalkers, getSingleTalker  } = require('./middlewares');
+const { getAllTalkers, getSingleTalker } = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
