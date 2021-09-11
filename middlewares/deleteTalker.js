8b54c34b3cb5ea2a12affbe0f80e54a116000559
Letycia Barreto <letyciasbarreto@gmail.com>
2021-09-11 16:57:42 -0300

    req 6 test

--- middlewares/deleteTalker.js
+++ middlewares/deleteTalker.js
@@ -1,10 +1,11 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
 const deleteTalker = (req, res, _next) => {
-  const { id } = Number(req.params);
-  const talker = JSON.parse(fs.readFileSync('talker.json', 'utf-8'));
-  const filterTalker = talker.filter((f) => f.id !== id);
-  fs.writeFileSync('talker.json', JSON.stringify(filterTalker));
-  res.status(200).send({ message: 'Pessoa palestrante deletada com sucesso' });
+  const talkers = JSON.parse(fs.readFileSync('talker.json'));
+  const id = Number(req.params.id);
+  const newTalkers = talkers.filter((t) => t.id !== id);
+  fs.writeFileSync('talker.json', JSON.stringify(newTalkers));
+  res.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
 };
+
 module.exports = deleteTalker;
