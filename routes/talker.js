09b4cff9840efb2ab3753d0c57f7789461728b73
Luiz Paulo Lima <limaluizpaulo@live.com>
2021-09-11 09:39:04 -0300

    requisito 4

--- routes/talker.js
+++ routes/talker.js
@@ -5,6 +5,77 @@ const fs = require('fs');
 const app = express();
 app.use(bodyParser.json());
 
+const ageMidd = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) {
+    return res.status(400).send({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < 18) {
+    return res.status(400).send({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+  return next();
+};
+
+const nameMidd = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).send({ message: 'O campo "name" é obrigatório' });
+  }
+  if (name.length < 3) {
+    return res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+  return next();
+};
+
+// https://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy-with-leap-year-support
+const checkDate = (date) => {
+  const regex = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+  return regex.test(date);
+};
+
+const dateMidd = (req, res, next) => {
+  const { talk } = req.body;
+  const isValid = checkDate(talk.watchedAt);
+
+  if (!isValid) {
+    return res
+      .status(400)
+      .send({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+  if (!Number.isInteger(talk.rate) || talk.rate < 1 || talk.rate > 5) {
+    return res
+      .status(400)
+      .send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  return next();
+};
+
+const talkMidd = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk || (!talk.watchedAt || !talk.rate)) {
+    return res.status(400).send({
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+    });
+  }
+  return next();
+};
+
+const tokenMidd = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  } 
+  if (token !== '7mqaVRXJSp886CGr') {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  return next();
+}; 
+
 app.get('/', (req, res) => {
   const talkers = JSON.parse(fs.readFileSync(`${__dirname}/../talker.json`));
    res.status(200).json(talkers);
@@ -18,6 +89,15 @@ app.get('/:id', (req, res) => {
     return res.status(200).json(talker);
   }
   return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
+app.post('/', talkMidd, tokenMidd, nameMidd, ageMidd, dateMidd, (req, res) => {
+  const talker = req.body;
+  const talkers = JSON.parse(fs.readFileSync(`${__dirname}/../talker.json`));
+  talker.id = talkers.length + 1;
+  talkers.push(talker);
+  fs.writeFileSync(`${__dirname}/../talker.json`, JSON.stringify(talkers));
+  res.status(201).json(talker);
 });
 
 module.exports = app;
