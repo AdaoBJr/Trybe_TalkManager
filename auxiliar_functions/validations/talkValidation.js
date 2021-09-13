4eb69d49f96704785d74f14e27d32f89a7ea3d80
Pedro Toselli <pedrotrybe@gmail.com>
2021-09-13 12:06:21 -0300

    Requisito 4 - refatorado

--- auxiliar_functions/validations/talkValidation.js
+++ auxiliar_functions/validations/talkValidation.js
@@ -3,11 +3,13 @@ const { BAD_REQUEST } = require('../../http_status/status');
 const INVALID_TALK = 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios';
 
 const talkValidation = (req, res, next) => {
-  const { watchedAt, rate } = req.body.talk;
+  const { talk } = req.body;
 
-  if (!watchedAt) return res.status(BAD_REQUEST).json({ message: INVALID_TALK });
+  if (!talk) return res.status(BAD_REQUEST).json({ message: INVALID_TALK });
 
-  if (!rate) return res.status(BAD_REQUEST).json({ message: INVALID_TALK });
+  if (!talk.watchedAt) return res.status(BAD_REQUEST).json({ message: INVALID_TALK });
+
+  if (!talk.rate && talk.rate !== 0) return res.status(BAD_REQUEST).json({ message: INVALID_TALK });
 
   next();
 };
