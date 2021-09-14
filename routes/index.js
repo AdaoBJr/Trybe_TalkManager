0448620f24739b993e855f828935fdc1ac2293ae
walterlbf <walter.lbf.f@gmail.com>
2021-09-14 15:02:48 -0400

    endpoint de busca pelo id ok

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const fs = require('fs').promises;
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 404;
 
 const readFile = async () => {
     const talkers = await fs.readFile('./talker.json', 'utf8');
@@ -13,6 +14,17 @@ const readFile = async () => {
 router.get('/', async (_req, res) => {
     const talkers = await readFile();
     res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const talkers = await readFile();
+    const findTalker = talkers.find((talker) => talker.id === Number(id));
+
+    if (findTalker) {
+        return res.status(HTTP_OK_STATUS).json(findTalker);
+    }
+    return res.status(HTTP_ERROR_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = router;
