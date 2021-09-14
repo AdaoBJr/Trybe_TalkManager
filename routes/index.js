2097526f1bfef30b0ced18c345e351272f7366f5
walterlbf <walter.lbf.f@gmail.com>
2021-09-14 17:35:54 -0400

    validacao de add novo talker feito

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,93 @@ const fs = require('fs').promises;
 
 const HTTP_OK_STATUS = 200;
 const HTTP_ERROR_STATUS = 404;
+const HTTP_TOKEN_ERROR_STATUS = 401;
+const HTTP_USER_ERROR_STATUS = 400;
 
 const readFile = async () => {
     const talkers = await fs.readFile('./talker.json', 'utf8');
     return JSON.parse(talkers);
+};
+
+const authToken = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        res.status(HTTP_TOKEN_ERROR_STATUS).send({ message: 'Token não encontrado' });
+    }
+
+    if (authorization.length < 16) {
+        res.status(HTTP_TOKEN_ERROR_STATUS).send({ message: 'Token inválido' });
+    }
+
+    next();
+};
+
+const authName = (req, res, next) => {
+    const { name } = req.body;
+    
+    if (!name) res.status(HTTP_USER_ERROR_STATUS).send({ message: 'O campo "name" é obrigatório' });
+
+    if (name.length <= 3) {
+        res.status(HTTP_USER_ERROR_STATUS).send(
+            { message: 'O "name" deve ter pelo menos 3 caracteres' },
+        );
+    }
+
+    next();
+};
+
+const authAge = (req, res, next) => {
+    const { age } = req.body;
+
+    if (!age) {
+        res.status(HTTP_USER_ERROR_STATUS).send({ message: 'O campo "age" é obrigatório' });
+    }
+    if (Number(age) < 18) {
+        res.status(HTTP_USER_ERROR_STATUS).send({
+            message: 'A pessoa palestrante deve ser maior de idade', 
+        });
+    }
+
+    next();
+};
+
+const authTalk = (req, res, next) => {
+    const { talk } = req.body;
+    
+    if (!talk || talk.watchedAt.length === undefined || talk.rate.length === undefined) {
+        res.status(400).send(
+            {
+                message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+            },
+        );
+    }
+
+    next();
+};
+
+const authWatchedAt = (req, res, next) => {
+    const { talk } = req.body;
+
+    const dateRegex = new RegExp(/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i);
+    if (!dateRegex.test(talk.watchedAt)) {
+        res.status(400).send({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+    }
+
+    next();
+};
+
+const authRate = (req, res, next) => {
+    const { talk } = req.body;
+    
+    if (talk.rate <= 0 || talk.rate >= 6) {
+        res.status(400).send({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+    }
+    next();
+};
+
+const setNewTalker = async (talker) => {
+    await fs.writeFile('./talker.json', JSON.stringify(talker));
 };
 
 router.get('/', async (_req, res) => {
@@ -26,5 +109,21 @@ router.get('/:id', async (req, res) => {
     }
     return res.status(HTTP_ERROR_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
 });
+
+router.post('/',
+    authAge,
+    authName,
+    authRate,
+    authTalk,
+    authToken,
+    authWatchedAt,
+    async (req, res) => {
+        const talkers = await readFile();
+        const newTalker = { ...req.body, id: talkers.length + 1 };
+        const updatedTalkers = [...talkers, newTalker];
+        await setNewTalker(updatedTalkers);
+
+        res.status(201).json(newTalker);
+    });
 
 module.exports = router;
