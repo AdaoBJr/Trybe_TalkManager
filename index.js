5aecd46416dce036f81b9b35d2c98b4accb7f1e2
walterlbf <walter.lbf.f@gmail.com>
2021-09-14 15:43:36 -0400

    endpoint de login e token validados

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const talkers = require('./routes/index');
+const login = require('./routes/loginRoutes');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,6 +15,7 @@ app.get('/', (_request, response) => {
 });
 
 app.use('/talker', talkers);
+app.use('/login', login);
 
 app.listen(PORT, () => {
   console.log('Online');
---
+++ routes/loginRoutes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const router = express.Router();
+const crypto = require('crypto');
+
+const HTTP_OK_STATUS = 200;
+const HTTP_LOGIN_ERROR_STATUS = 400;
+
+const authEmail = (req, res, next) => {
+    const { email } = req.body;
+    const emailRegex = new RegExp(/^[\w.]+@[a-z]+.\w{2,3}$/g);
+
+    if (!email) { 
+        res.status(HTTP_LOGIN_ERROR_STATUS).json({ message: 'O campo "email" é obrigatório' });
+    }
+    if (!emailRegex.test(email)) {
+        res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+    }
+    next();
+};
+
+const authPassword = (req, res, next) => {
+    const { password } = req.body;
+
+    if (!password) {
+        res.status(HTTP_LOGIN_ERROR_STATUS).json(
+            { message: 'O "password" deve ter pelo menos 6 caracteres' },
+        );
+    }
+
+    if (password.length < 6) {
+        res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    }
+    next();
+};
+
+router.post('/', authEmail, authPassword, (req, res) => {
+    const newToken = crypto.randomBytes(8).toString('hex');
+    res.status(HTTP_OK_STATUS).json({ token: newToken }); 
+});
+
+module.exports = router;
