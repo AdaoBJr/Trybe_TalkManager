b982c6cfa460ea1a57759b01fdb33ecc145b93c5
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-09-16 14:21:41 -0300

    correçao req 3

--- middlewares/verificateLogin.js
+++ middlewares/verificateLogin.js
@@ -1,31 +1,25 @@
 const crypto = require('crypto');
 
-const genarateToken = crypto.randomBytes(8).toString('hex');
+const genarateToken = () => crypto.randomBytes(8).toString('hex');
 
-const validates = (req, res) => {
+const verificateLogin = (req, res) => {
   const { email, password } = req.body;
-
   const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-
   const validateEmail = regex.test(email);
 
-  if (email.length === null) {
+  if (!email) {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
   if (!validateEmail) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
-  if (password.length === 0) {
+  if (!password) {
     return res.status(400).json({ message: 'O campo "password" é obrigatório' });
   }
   if (password.length < 6) {
     return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
-};
-
-const verificateLogin = (req, res) => {
-  validates();
-  res.status(200).json({ token: genarateToken });
+  res.status(200).json({ token: genarateToken() });
 };
 
 module.exports = verificateLogin;
