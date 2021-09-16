fdc3b6e7870b0712c38dd534910738bd5919dc64
Max <gmaxgomes@gmail.com>
2021-09-16 16:40:53 -0300

    sad

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.use('/talkers', clientRouter);
+app.use('/talker', clientRouter);
 
 app.listen(PORT, () => {
   console.log('Online');
