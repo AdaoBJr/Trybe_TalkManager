25c801a06741a7b570df113a3679d3351f81294c
Bruna Campos <92brunacampos@gmail.com>
2021-09-17 17:38:01 -0300

    requisito 1 - teste

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const talkerFile = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +13,19 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+// ------------------------------------------ //
+
+app.get('/talker', (_req, res) => {
+  const talker = JSON.parse(fs.readFileSync(talkerFile, 'utf8'));
+  // res.status(200).json(talker);
+
+  if (talker.length > 0) {
+    return res.status(200).json(talker);
+  }
+  if (talker.length === 0) {
+    return res.status(200).json([]);
+  }
 });
 
 app.listen(PORT, () => {
