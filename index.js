d5305c2e03847be3bd49471d40327d63ca2fd122
marcos <marcosd3wow@gmail.com>
2021-09-19 10:14:07 -0300

    passou e nao passou o req 4, refatorando o index para teste

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const bodyParser = require('body-parser');
 // const readMiddleware = require('./middlewares/readMiddleware');
 const { getTalkers, getTalkersById, login, createTalker } = require('./middlewares');
 
+const HTTP_OK_STATUS = 200;
+const PORT = '3000';
+
 const app = express();
 app.use(bodyParser.json());
+
+app.use((err, _req, res, _next) => {
+  res.status(500).send(`Erro! Mensagem: ${err.message}`);
+});
 
 // app.use('/talker', readMiddleware);
 app.post('/login', login);
 app.use('/talker/:id', getTalkersById);
 app.use('/talker', getTalkers);
 app.post('/talker', createTalker);
-
-const HTTP_OK_STATUS = 200;
-const PORT = '3000';
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
