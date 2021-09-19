1b0e2a50564a62b68d026b7b89679ba301c66cda
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-09-19 17:29:16 -0300

    atualizaçao req4

--- middlewares/validates/validateTalk.js
+++ middlewares/validates/validateTalk.js
@@ -1,6 +1,6 @@
 const validateTalk = (req, res, next) => {
   const { talk } = req.body;
-  if (!talk || !talk.rate || !talk.watchedAt === null) {
+  if (!talk || !talk.rate || !talk.watchedAt === undefined) {
     return res.status(400).json({ 
       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
   }
