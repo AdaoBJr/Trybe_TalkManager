d8629a5a6970d5fe734682707112e57d9de0ca64
walterlbf <walter.lbf.f@gmail.com>
2021-09-20 19:13:06 -0400

    condição do arthTalk corrigida

--- middlewares/index.js
+++ middlewares/index.js
@@ -91,7 +91,8 @@ const authAge = (req, res, next) => {
 const authTalk = (req, res, next) => {
     const { talk } = req.body;
     
-    if (!talk || !talk.watchedAt === undefined || talk.rate === undefined) {
+    if (!talk || !talk.watchedAt || talk.rate === undefined) {
+       console.log('entrei aqui');
        return res.status(400).send(
             {
                 message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
