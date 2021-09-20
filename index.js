a7eef1cd5784ac9e558af345f1f4bcdaa8cd13e5
walterlbf <walter.lbf.f@gmail.com>
2021-09-20 15:46:01 -0400

    refatorando as middlewares

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const talkers = require('./routes/index');
-const login = require('./routes/loginRoutes');
+const rescue = require('express-rescue');
+const fs = require('fs').promises;
+const talkers = require('./talker.json');
+// const login = require('./routes/loginRoutes');
+
+const {
+  generateToken,
+  authAge,
+  authEmail,
+  authName,
+  authPassword,
+  authRate,
+  authToken,
+  authWatchedAt,
+  talkerSearch,
+  authTalk,
+} = require('./middlewares');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,9 +29,52 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.use('/talker', talkers);
-app.use('/login', login);
-
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/talker/search', authToken, talkerSearch);
+
+app.get('/talker', async (req, res) => {
+  const talkersJson = await fs.readFile(talkers);
+  const response = JSON.parse(talkersJson);
+
+  if (!response) {
+    return res.status(200).json(Array([]));
+  } 
+
+ return res.status(200).json(response);
+});
+
+app.get('/talker/:id', rescue(async (req, res) => {
+    const talkersJson = await fs.readFile(talkers);
+    const response = JSON.parse(talkersJson);
+    const { id } = req.params;
+    const chosenTalker = response.find((talker) => talker.id === Number(id));
+  
+    if (!chosenTalker) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    } 
+
+    return res.status(200).json(chosenTalker);
+  }));
+
+app.post('/login', authEmail, authPassword, generateToken);
+
+app.post('/talker',
+  authToken,
+  authName,
+  authAge,
+  authTalk,
+  authWatchedAt,
+  authRate,
+  rescue(async (req, res) => {
+    const { body } = req;
+    const talkersJson = await fs.readFile(talkers);
+    const currTalkers = JSON.parse(talkersJson);
+    const id = currTalkers.length + 1;
+    const currIds = { ...body, id };
+    const newTalkers = [...currTalkers, currIds];
+    fs.writeFile(talkers, JSON.stringify(newTalkers));
+    return res.status(201).json(currIds);
+}));
---
+++ middlewares/index.js
@@ -0,0 +1,154 @@
+const crypto = require('crypto');
+const fs = require('fs').promises;
+
+const readFileJson = async () => {
+    const talkers = await fs.readFile('./talker.json', 'utf8');
+    return JSON.parse(talkers);
+};
+
+const generateToken = (req, res) => {
+    const token = crypto.randomBytes(8).toString('hex');
+    return res.status(200).json({ token });
+};
+
+const authEmail = (req, res, next) => {
+    const { email } = req.body;
+    const emailRegex = new RegExp(/^[\w.]+@[a-z]+.\w{2,3}$/g);
+
+    if (!email || email.length === 0) { 
+       return res.status(400).json({ 
+           message: 'O campo "email" é obrigatório', 
+        });
+    }
+    if (!emailRegex.test(email)) {
+       return res.status(400).json(
+            { message: 'O "email" deve ter o formato "email@email.com"' },
+        );
+    }
+
+    next();
+};
+
+const authPassword = (req, res, next) => {
+    const { password } = req.body;
+
+    if (!password) {
+       return res.status(400).json(
+            { message: 'O campo "password" é obrigatório' },
+        );
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json(
+            { message: 'O "password" deve ter pelo menos 6 caracteres' },
+        );
+    }
+
+    next();
+};
+
+const authToken = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization) {
+        return res.status(401).send({ message: 'Token não encontrado' });
+    }
+
+    if (authorization.length < 16) {
+        return res.status(401).send({ message: 'Token inválido' });
+    }
+
+    next();
+};
+
+const authName = (req, res, next) => {
+    const { name } = req.body;
+    
+    if (!name) {
+        return res.status(400).send({ 
+        message: 'O campo "name" é obrigatório' }); 
+    }
+
+    if (name.length <= 3) {
+       return res.status(400).send(
+            { message: 'O "name" deve ter pelo menos 3 caracteres' },
+        );
+    }
+
+    next();
+};
+
+const authAge = (req, res, next) => {
+    const { age } = req.body;
+
+    if (!age) {
+       return res.status(400).send({ message: 'O campo "age" é obrigatório' });
+    }
+    if (Number(age) < 18) {
+       return res.status(400).send({
+            message: 'A pessoa palestrante deve ser maior de idade', 
+        });
+    }
+
+    next();
+};
+
+const authWatchedAt = (req, res, next) => {
+    const { watchedAt } = req.body.talk;
+  const re = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  const dateFormat = re.test(watchedAt);
+
+  if (!dateFormat) {
+    return res.status(400)
+      .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' }); 
+  }
+  next();
+};
+
+const authRate = (req, res, next) => {
+    const { rate } = req.body.talk;
+  if (!(rate > 0 && rate < 6)) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+  next();
+};
+
+const talkerSearch = async (req, res) => {
+    const { q } = req.query;
+    const talkers = await readFileJson();
+    if (!q || q === '') {
+      return res.status(200).json(talkers);    
+    }   
+    const filteredTalkers = talkers.filter((talker) => talker.name.includes(q));
+    if (filteredTalkers.length > 0) {
+      return res.status(200).json(filteredTalkers); 
+    } 
+    return res.status(200).json(Array.from([])); 
+};
+
+const authTalk = (req, res, next) => {
+    const { talk } = req.body;
+    
+    if (!talk || !talk.watchedAt.length === undefined || talk.rate.length === undefined) {
+       return res.status(400).send(
+            {
+                message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
+            },
+        );
+    }
+
+    next();
+};
+
+module.exports = {
+    generateToken,
+    authAge,
+    authEmail,
+    authName,
+    authPassword,
+    authRate,
+    authToken,
+    authWatchedAt,
+    talkerSearch,
+    authTalk,
+};
