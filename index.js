733a6895fc82101516be8ff545b630b8f1636161
LeonarDev <leonardo.majevski@gmail.com>
2021-09-21 19:13:54 -0300

    Implements requirement 01

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const HTTP_CODES = {
+  SUCCESS: 200,
+  NOT_FOUND: 404
+}
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
-});
+app.listen(PORT, () => console.log('Online'));
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  const result = await JSON.parse(talkers);
+  
+  res.status(HTTP_CODES.SUCCESS).json(result);
+})
