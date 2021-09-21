da578de3fc92da4bb5bdbee3882b82cd2264fb0a
Italo <italomatos764@gmail.com>
2021-09-21 00:14:59 -0400

    Requisito 7

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -1,14 +1,11 @@
 const express = require('express');
 
 const router = express.Router();
-
 const fs = require('fs').promises;
 
 const talkers = './talker.json';
-
 router.get('/', (_request, response) => {
   let data = [];
-
   fs.readFile(talkers, 'utf8')
   .then((info) => JSON.parse(info))
   .then((info) => {
@@ -20,14 +17,40 @@ router.get('/', (_request, response) => {
    });
 });
 
+const tokenCheck = (request, response, next) => {
+  const { authorization } = request.headers;
+  if (!authorization) {
+    return response.status(401).json(
+      { message: 'Token não encontrado' },
+    );
+  }
+  if (authorization.length !== 16) {
+    return response.status(401).json(
+      { message: 'Token inválido' },
+    );
+  }
+  next();
+};
+
+router.get('/search', tokenCheck, (req, res) => {
+  fs.readFile(talkers, 'utf8')
+  .then((info) => JSON.parse(info))
+  .then((info) => {
+    const { q } = req.query;
+    if (!q) return res.status(200).json(info);
+    const searchTalker = info.filter((talker) => talker.name.includes(q));
+    return res.status(200).json(searchTalker);
+  })
+  .catch((erro) => { res.status(400).json(erro); });
+});
+
 router.get('/:id', (request, response) => {
   const { id } = request.params;
 
   fs.readFile(talkers, 'utf8')
   .then((info) => JSON.parse(info))
   .then((info) => {
-    const filtered = info.find((item) => item.id === Number(id));
-    
+    const filtered = info.find((item) => item.id === Number(id));   
     if (!filtered) {
       return response.status(404).json({
         message: 'Pessoa palestrante não encontrada',
@@ -46,7 +69,6 @@ const nameCheck = (request, response, next) => {
       },
     );
   }
-
   if (name.length < 3) {
     return response.status(400).json(
       {
@@ -66,29 +88,11 @@ const ageCheck = (request, response, next) => {
       },
     );
   }
-
   if (age <= 17) {
     return response.status(400).json(
       {
         message: 'A pessoa palestrante deve ser maior de idade',
       },
-    );
-  }
-  next();
-};
-
-const tokenCheck = (request, response, next) => {
-  const { authorization } = request.headers;
-
-  if (!authorization) {
-    return response.status(401).json(
-      { message: 'Token não encontrado' },
-    );
-  }
-
-  if (authorization.length !== 16) {
-    return response.status(401).json(
-      { message: 'Token inválido' },
     );
   }
   next();
@@ -108,13 +112,11 @@ const watchedAtCheck = (request, response, next) => {
   const { talk } = request.body;
   const { watchedAt } = talk;
   const dataRegex = RegExp(/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/);
-
   if (!watchedAt) {
     return response.status(400).json(
       { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
     );
   }
-
   if (!dataRegex.test(watchedAt)) {
     return response.status(400).json(
       { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' },
@@ -126,11 +128,9 @@ const watchedAtCheck = (request, response, next) => {
 const rateCheck = (request, response, next) => {
   const { talk } = request.body;
   const { rate } = talk;
-  
   if (rate < 1 || rate > 5) {
     return response.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
    }
-
   if (!rate) {
     return response.status(400).json(
       { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' },
