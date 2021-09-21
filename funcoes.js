5254fb0ce6dc2fb8fc3a6cd5adb6c091e6211ec9
Bruna Campos <92brunacampos@gmail.com>
2021-09-21 19:45:42 -0300

    eslint

--- funcoes.js
+++ funcoes.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-tabs */
 const crypto = require('crypto');
 
 // token.length está retornando undefined?
@@ -6,8 +5,8 @@ exports.gerarToken = () => crypto.randomBytes(8).toString('hex'); // double
 
 exports.validarEmail = (email) => {
   const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-	const Email = regex.test(email);
-	if (!email || email.length === 0) {
+  const Email = regex.test(email);
+  if (!email || email.length === 0) {
     return { status: 400, message: 'O campo "email" é obrigatório' };
   }
   if (!Email) {
@@ -17,7 +16,7 @@ exports.validarEmail = (email) => {
 };
 
 exports.validarSenha = (password) => {
-	if (!password || password.length === 0) {
+  if (!password || password.length === 0) {
     return { status: 400, message: 'O campo "password" é obrigatório' };
   }
   if (password.length < 6) {
