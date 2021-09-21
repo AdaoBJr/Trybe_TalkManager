eb69cfcb60737604e65863160c87aeb83fe16eae
walterlbf <walter.lbf.f@gmail.com>
2021-09-20 20:13:11 -0400

    remove console log e vamos ver pq no pc passa o 5 e no github nao

--- middlewares/index.js
+++ middlewares/index.js
@@ -92,7 +92,6 @@ const authTalk = (req, res, next) => {
     const { talk } = req.body;
     
     if (!talk || !talk.watchedAt || talk.rate === undefined) {
-       console.log('entrei aqui');
        return res.status(400).send(
             {
                 message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
