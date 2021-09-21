731a2021e1af0ce615a893d8be76139656829a54
marcos <marcosd3wow@gmail.com>
2021-09-21 00:25:47 -0300

    iniciando os testes 4 5 6 / 5 invertendo a posiçao do use ntb no delete para testar

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.use(bodyParser.json());
 app.post('/login', login);
 app.use('/talker/:id', getTalkersById);
 app.post('/talker', auth, valid, createTalker);
-app.use('/talker', getTalkers);
 app.put('/talker/:id', auth, valid, modTalker);
 app.delete('/talker/:id', auth, removeTalker);
+app.use('/talker', getTalkers);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
