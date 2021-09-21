dabde200679b1ef234883d3355ab6769b7454c20
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-21 01:00:11 -0300

    Requisito 01 correçao

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { request } = require('http');
 
 const app = express();
 app.use(bodyParser.json());
