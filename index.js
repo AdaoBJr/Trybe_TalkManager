044911de405d06372ac20d6586cc0389a15137d9
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2021-09-22 08:36:36 -0400

    Finalizado requisito 01, criado endpoint GET / talker.

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
+const talkers = './talker.json';
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 400;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  fs.readFile(talkers, 'utf-8', (err, data) => {
+    if (err) {
+      res.status(HTTP_OK_STATUS).json([]);
+    }
+    const content = JSON.parse(data);
+    res.status(HTTP_OK_STATUS).json(content);
+  });
 });
 
 app.listen(PORT, () => {
