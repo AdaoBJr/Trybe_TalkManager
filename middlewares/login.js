d8edb6c6be332485bb8715bb9b4b6e85863f1143
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-22 10:08:00 -0300

    alterado regex de email

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,18 +1,18 @@
 const crypto = require('crypto');
 const HTTP_OK_STATUS = 200;
 const HTTP_400_STATUS = 400;
-const REGEX_EMAIL = /^[\w]+@([\w]+\.)+[\w]{2,4}$/gi;
+const REGEX_EMAIL = /^[^\s@]+@[^\s@]+$/;
 const EMAIL_IS_REQUIRED = { message: 'O campo "email" é obrigatório' };
 const INVALID_EMAIL = { message: 'O "email" deve ter o formato "email@email.com"' };
 const PASSWORD_IS_REQUIRED = { message: 'O campo "password" é obrigatório' };
 const INVALID_PASSWORD = { message: 'O "password" deve ter pelo menos 6 caracteres' };
 
-const login = (_require, response) => {
+const login = (request, response) => {
   const { email, password } = request.body;
   if (!email) {
     return response.status(HTTP_400_STATUS).json(EMAIL_IS_REQUIRED);
   }
-  if (!REGEX_EMAIL.test(email)) {
+  if (!email.match(REGEX_EMAIL)) {
     return response.status(HTTP_400_STATUS).json(INVALID_EMAIL);
   }
   if (!password) {
