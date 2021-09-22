ec2103be6368d2a6c74bdcefafa90bbfa0e66a0b
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-22 08:07:37 -0300

    exclui helpers

--- helpers/readFileTalker.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs').promises;
-
-const readFileTalker = async () => {
-  const allTalkers = await fs.readFile('./talker.json', 'utf8');
-  const alltalkersJSON = JSON.parse(allTalkers);
-  return alltalkersJSON;
-};
-
-module.exports = readFileTalker;
