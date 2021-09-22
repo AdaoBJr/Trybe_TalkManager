e6d1d992eab873e0cd02324af76e794da3807ef4
Maximiliano Alvarenga <maxbispo163@hotmail.com>
2021-09-22 08:42:03 -0400

    Implementado requisito 02, criado endpoint GET /talker/:id

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const HTTP_ERROR_STATUS = 400;
+const HTTP_ERROR_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -22,6 +22,18 @@ app.get('/talker', (_req, res) => {
     }
     const content = JSON.parse(data);
     res.status(HTTP_OK_STATUS).json(content);
+  });
+});
+
+app.get('/talker/:id', (req, res) => {
+  fs.readFile(talkers, 'utf-8', (err, data) => {
+    const content = JSON.parse(data);
+    const { id } = req.params;
+    const talkerId = content.find((c) => c.id === Number(id));
+    if (!talkerId) {
+      return res.status(HTTP_ERROR_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(HTTP_OK_STATUS).json(talkerId);
   });
 });
 
