ec854d9ea54d6e14fcba95aa58faf20d1cbd2577
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-22 08:34:29 -0300

    refatora gettalkerbyid

--- middlewares/getTalkerById.js
+++ middlewares/getTalkerById.js
@@ -1,11 +1,12 @@
-const { readFileTalker } = require('../helpers');
+const fs = require('fs').promises;
+
 const HTTP_OK_STATUS = 200;
 const HTTP_404_STATUS = 404;
 const NOT_REGISTERED = { message: 'Pessoa palestrante não encontrada' };
 
 const getTalkerById = async (request, response) => {
   const { id } = request.params;
-  const allTalkers = await readFileTalker();
+  const allTalkers = JSON.parse(await fs.readFile('talker.json', 'utf-8'));
   const talkerById = allTalkers.find((talker) => talker.id === parseInt(id, 10));
   if (!talkerById) {
     return response.status(HTTP_404_STATUS).json(NOT_REGISTERED);
