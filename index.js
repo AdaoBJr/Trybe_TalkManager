2f86b835f8117ba4b2743d92b54c0bd2e2453b0b
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-21 21:04:07 -0300

    Requisito 04

--- index.js
+++ index.js
@@ -13,26 +13,26 @@ const getTalkers = async () => {
 
 const PORT = '3000';
 const HTTP_OK_STATUS = 200;
-// const HTTP_201_STATUS = 201;
+const HTTP_201_STATUS = 201;
 const HTTP_400_STATUS = 400;
-// const HTTP_401_STATUS = 401;
+const HTTP_401_STATUS = 401;
 const HTTP_404_STATUS = 404;
 const VALID_EMAIL = /^[\w]+@([\w]+\.)+[\w]{2,4}$/gi;
-// const VALID_DATA = /\d{2}\/\d{2}\/\d{4}/g;
+const VALID_DATA = /\d{2}\/\d{2}\/\d{4}/g;
 const FAIL_MESSAGE = { message: 'Pessoa palestrante não encontrada' };
 const REQUIRED_EMAIL = { message: 'O campo "email" é obrigatório' };
 const INVALID_EMAIL = { message: 'O "email" deve ter o formato "email@email.com"' };
 const REQUIRED_PASSWORD = { message: 'O campo "password" é obrigatório' };
 const INVALID_PASSWORD = { message: 'O "password" deve ter pelo menos 6 caracteres' };
-// const NOT_FOUND_TOKEN = { message: 'Token não encontrado' };
-// const INVALID_TOKEN = { message: 'Token inválido' };
-// const REQUIRED_NAME = { message: 'O campo "name" é obrigatório' };
-// const INVALID_NAME = { message: 'O "name" deve ter pelo menos 3 caracteres' };
-// const REQUIRED_AGE = { message: 'O campo "age" é obrigatório' };
-// const INVALID_AGE = { message: 'A pessoa palestrante deve ser maior de idade' };
-// const INVALID_TALK = { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' };
-// const INVALID_DATE = { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
-// const INVALID_RATE = { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
+const NOT_FOUND_TOKEN = { message: 'Token não encontrado' };
+const INVALID_TOKEN = { message: 'Token inválido' };
+const REQUIRED_NAME = { message: 'O campo "name" é obrigatório' };
+const INVALID_NAME = { message: 'O "name" deve ter pelo menos 3 caracteres' };
+const REQUIRED_AGE = { message: 'O campo "age" é obrigatório' };
+const INVALID_AGE = { message: 'A pessoa palestrante deve ser maior de idade' };
+const INVALID_TALK = { message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' };
+const INVALID_DATE = { message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
+const INVALID_RATE = { message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -84,9 +84,62 @@ const isValidPassword = (request, response, next) => {
 };
 
 app.post('/login', isValidPassword, isValidEmail, (_require, response) => {
-  console.log("AQUI");
   const myToken = crypto.randomBytes(8).toString('hex');
   response.status(HTTP_OK_STATUS).json({ token: myToken });
 });
 
 // Requisito 04
+const isValidToken = (request, response, next) => {
+  const { authorization } = request.headers;
+  if (!authorization) {
+    return response.status(HTTP_401_STATUS).json(NOT_FOUND_TOKEN);
+  }
+  if (authorization.length !== 16) {
+    return response.status(HTTP_401_STATUS).json(INVALID_TOKEN);
+  }
+  next();
+};
+
+const isValidName = (request, response, next) => {
+  const { name } = request.body;
+  if (name === undefined) {
+    return response.status(HTTP_400_STATUS).json(REQUIRED_NAME);
+  }
+  if (name.length < 3) {
+    return response.status(HTTP_400_STATUS).json(INVALID_NAME);
+  }
+  next();
+};
+
+const isValidAge = (request, response, next) => {
+  const { age } = request.body;
+  if (!age) {
+    return response.status(HTTP_400_STATUS).json(REQUIRED_AGE);
+  }
+  if (age < 18) {
+    return response.status(HTTP_400_STATUS).json(INVALID_AGE);
+  }
+  next();
+};
+
+const isValidTalk = (request, response, next) => {
+  const { talk } = request.body;
+  const { watchedAt, rate } = talk;
+  if (!talk || watchedAt === undefined || rate === undefined) {
+    return response.status(HTTP_400_STATUS).json(INVALID_TALK);
+  }
+  if (!VALID_DATA.test(watchedAt)) {
+    return response.status(HTTP_400_STATUS).json(INVALID_DATE);
+  }
+  if (rate < 1 || rate > 5) {
+    return response.status(HTTP_400_STATUS).json(INVALID_RATE);
+  }
+  next();
+};
+
+app.post('/talker', isValidToken, isValidName, isValidAge, isValidTalk, async (request, response, _next) => {
+  const talker = await getTalkers();
+  const newTalker = { id: talker.length + 1, ...request.body };
+  fs.writeFile('./talker.json', JSON.stringify([...talker, newTalker]));
+  response.status(HTTP_201_STATUS).json(newTalker);
+});
