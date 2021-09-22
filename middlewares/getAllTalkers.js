e1c21fddd6de27abe63c324467273670f90895db
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-22 08:32:35 -0300

    refatora getalltalkers

--- middlewares/getAllTalkers.js
+++ middlewares/getAllTalkers.js
@@ -1,8 +1,9 @@
-const { readFileTalker } = require('../helpers');
+const fs = require('fs').promises;
+
 const HTTP_OK_STATUS = 200;
 
 const getAllTalkers = async (_request, response) => {
-  const allTalkers = await readFileTalker();
+  const allTalkers = JSON.parse(await fs.readFile('./talker.json', 'utf-8'));
   return response.status(HTTP_OK_STATUS).json(allTalkers);
 };
 
