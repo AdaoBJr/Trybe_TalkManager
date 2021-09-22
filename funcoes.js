e2aba8218732dfc7a023e96eca93e08f9307aea5
Bruna Campos <92brunacampos@gmail.com>
2021-09-22 16:31:25 -0300

    requisito 4 - post/talker

--- funcoes.js
+++ funcoes.js
@@ -1,12 +1,11 @@
 const crypto = require('crypto');
 
-// token.length está retornando undefined?
 exports.gerarToken = () => crypto.randomBytes(8).toString('hex'); // double
 
 exports.validarEmail = (email) => {
   const regex = /^[a-z0-9._]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
   const Email = regex.test(email);
-  if (!email || email.length === 0) {
+  if (!email) {
     return { status: 400, message: 'O campo "email" é obrigatório' };
   }
   if (!Email) {
@@ -16,7 +15,7 @@ exports.validarEmail = (email) => {
 };
 
 exports.validarSenha = (password) => {
-  if (!password || password.length === 0) {
+  if (!password) {
     return { status: 400, message: 'O campo "password" é obrigatório' };
   }
   if (password.length < 6) {
@@ -25,11 +24,85 @@ exports.validarSenha = (password) => {
   return { Ok: true };
 };
 
-// module.exports = { gerarToken };
+exports.validarToken = (token) => {
+  const regex = /^[\w]{16}$/i;
+  const Token = regex.test(token); // retorna true or false
+  if (!token) {
+    return { status: 401, message: 'Token não encontrado' };
+  }
+  if (!Token) {
+    return { status: 401, message: 'Token inválido' };
+  } 
+  return { Ok: true };
+};
+
+const validarNome = (name) => {
+  if (!name) {
+    return { status: 400, message: 'O campo "name" é obrigatório' };
+  }
+  if (name.length < 3) {
+    return { status: 400, message: 'O "name" deve ter pelo menos 3 caracteres' };
+  }
+  return { Ok: true };
+};
+
+const validarIdade = (age) => {
+  if (!age) {
+    return { status: 400, message: 'O campo "age" é obrigatório' };
+  }
+  if (age < 18) {
+    return { status: 400, message: 'A pessoa palestrante deve ser maior de idade' };
+  }
+  return { Ok: true };
+};
+
+const validarData = (data) => {
+  const regexWatchedAt = /^\d{2}\/\d{2}\/\d{4}$/; // formato da data
+  const date = regexWatchedAt.test(data.watchedAt); // exec?
+
+  if (!date) {
+    return { status: 400, message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' };
+   }
+  return { Ok: true };
+};
+
+const validarRate = (talk) => {
+  const { rate } = talk;
+  if (rate < 1 || rate > 5) {
+    return { status: 400, message: 'O campo "rate" deve ser um inteiro de 1 à 5' };
+ }
+ return { Ok: true };
+};
+
+const validarTalk = (talk) => {
+  if (!talk || !talk.watchedAt || !talk.rate) {
+    return {
+      status: 400,
+      message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' };
+  }
+  return { Ok: true };
+};
+
+exports.validarTalker = (name, age, talk) => {
+  const nomeValido = validarNome(name);
+  if (!nomeValido.Ok) { return nomeValido; }
+
+  const idadeValida = validarIdade(age);
+  if (!idadeValida.Ok) { return idadeValida; }
+
+  const talkValida = validarTalk(talk);
+  if (!talkValida.Ok) { return talkValida; }
+
+  const dataValida = validarData(talk);
+  if (!dataValida.Ok) { return dataValida; }
+
+  const validaRate = validarRate(talk);
+  if (!validaRate.Ok) { return validaRate; }
+
+  return { Ok: true };
+};
 
 // ------------------------------------------ //
 
 // npm run restore
 // npm run test nomeDoArquivoDeTeste.test.js
-// instalar sucrase para usar import
-// nodemon.json < { "execMap": { "js": "node -r sucrase/register"}}
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { gerarToken, validarEmail, validarSenha } = require('./funcoes');
+const {
+  gerarToken,
+  validarEmail,
+  validarSenha,
+  validarToken,
+  validarTalker,
+} = require('./funcoes');
 
 const talkerFile = './talker.json';
 
@@ -46,7 +52,6 @@ app.get('/talker/:id', (req, res) => {
 // test login.test.js
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
-  // const token = req.headers.authorization;
   const validaEmail = validarEmail(email);
   const validaSenha = validarSenha(password);
 
@@ -62,6 +67,30 @@ app.post('/login', (req, res) => {
     return res.status(validaSenha.status).json({ message: validaSenha.message });
   }
 });
+
+// test createTalker.test.js
+app.post('/talker', (req, res) => {
+  const { name, age, talk } = req.body;
+  const validaTalker = validarTalker(name, age, talk);
+  const { authorization } = req.headers;
+  const validaToken = validarToken(authorization); // auth.token?
+
+  if (!validaToken.Ok) {
+    return res.status(validaToken.status).json({ message: validaToken.message });
+  }
+  if (!validaTalker.Ok) {
+    return res.status(validaTalker.status).json({ message: validaTalker.message });
+  }
+
+  const talker = JSON.parse(fs.readFileSync(talkerFile, 'utf8'));
+  const novaPessoa = { id: talker.length + 1, name, age, talk };
+  talker.push(novaPessoa);
+  fs.writeFileSync(talkerFile, JSON.stringify(talker));
+  return res.status(201).json(novaPessoa);
+});
+
+// app.put('/talker/:id', (req, res) => {
+// });
 
 app.listen(PORT, () => {
   console.log('Online');
