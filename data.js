2a054f724c263026bdad4940b0ecb536d0eead97
Junior030 <juniorhenrique030@gmail.com>
2021-09-22 13:19:59 -0300

    Criado arquivo router req 06 and 07

--- data.js
+++ data.js
@@ -1,19 +1,18 @@
 const fs = require('fs').promises;
 
+const talkerFile = './talker.json';
+
 const talkersAll = async () => {
-  const talkerFile = './talker.json';
   const talkers = await fs.readFile(talkerFile);
   return JSON.parse(talkers);
 };
 
-const talker = async (id) => {
-  const talkerFile = './talker.json';
+const talkerGet = async (id) => {
   const talkers = JSON.parse(await fs.readFile(talkerFile));
   return talkers.find((response) => response.id === Number(id));
 };
 
 const postTalker = async (newTalker) => {
-  const talkerFile = './talker.json';
   const talkers = await talkersAll();
   const talk = {
       id: talkers.length + 1,
@@ -24,12 +23,20 @@ const postTalker = async (newTalker) => {
   return talk;
 };
 
+const putTalker = async (id, updatedTalker) => {
+  const talkers = await talkersAll();
+  const indexTalker = talkers.findIndex((r) => r.id === +(id));
+  talkers.splice(indexTalker, 1);
+  await fs.writeFile(talkerFile, JSON.stringify(talkers));
+  const updatedTalkers = await postTalker(updatedTalker);
+  return updatedTalkers;
+};
+
 const deleteTalker = async (id) => {
-  const talkerFile = './talker.json';
   const talkers = await talkersAll();
   const indexTalker = talkers.findIndex((ITEM) => ITEM.id === Number(id));
   talkers.splice(indexTalker, 1);
   await fs.writeFile(talkerFile, JSON.stringify(talkers));
 };
 
-module.exports = { talkersAll, talker, postTalker, deleteTalker };
+module.exports = { talkersAll, talkerGet, postTalker, deleteTalker, putTalker };
--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
-const { talkersAll, talker, postTalker, deleteTalker } = require('./data.js');
-const {
-  emailValid,
-  passwordValid, 
-  tokenValid, 
-  nameValid, ageValid, talkValid, watchedAtValid, rateValid } = require('./middleware.js');
-  
-// const talkerRouter = express.Router();
+const { emailValid,
+  passwordValid,
+} = require('./middleware');
+const talkerRouter = require('./router');
+
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const NOT_FOUND = 404;
-const CREATED = 201;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -22,47 +17,12 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_request, response) => {
-  const talkers = await talkersAll();
-    response.status(HTTP_OK_STATUS).json(talkers);
-});
-
-app.post(
-  '/talker',
-  tokenValid,
-  nameValid,
-  ageValid,
-  talkValid,
-  watchedAtValid,
-  rateValid,
-  async (request, response) => {
-    const value = await postTalker(request.body);
-    response.status(CREATED).json(value);
-},
-);
-
-app.get('/talker/:id', async (request, response) => {
-  const id = await talker(request.params.id);
-  if (!id) { 
-    return response.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' }); 
-  }
-  response.status(HTTP_OK_STATUS).json(id);
-});
-
 app.post('/login', emailValid, passwordValid, async (request, response) => {
   const token = randomBytes(8).toString('hex');
   response.status(HTTP_OK_STATUS).json({ token });
 });
 
-app.delete(
-  '/:id',
-  tokenValid,
-  async (request, response) => {
-    const { id } = request.params;
-    await deleteTalker(id);
-    response.status(200).json({ message: 'Pessoa palestrante deletada com sucesso' });
-  },
-);
+app.use('/talker', talkerRouter);
 
 app.listen(PORT, () => {
   console.log('Online');
