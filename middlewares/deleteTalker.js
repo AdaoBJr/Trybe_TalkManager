bd3ec0e375188218b50f03b168ceffb9860e62ff
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-09-22 07:42:03 -0300

    refatora deletetalker

--- middlewares/deleteTalker.js
+++ middlewares/deleteTalker.js
@@ -1,9 +1,19 @@
 const fs = require('fs').promises;
 const { readFileTalker } = require('../helpers');
 const HTTP_OK_STATUS = 200;
+const HTTP_401_STATUS = 401;
+const NOT_FOUND_TOKEN = { message: 'Token não encontrado' };
+const INVALID_TOKEN = { message: 'Token inválido' };
 const DELETE_OK = { message: 'Pessoa palestrante deletada com sucesso' };
 
 const deleteTalker = async (request, response) => {
+  const { authorization } = request.headers;
+  if (!authorization) {
+    return response.status(HTTP_401_STATUS).json(NOT_FOUND_TOKEN);
+  }
+  if (authorization.length !== 16) {
+    return response.status(HTTP_401_STATUS).json(INVALID_TOKEN);
+  }
   const id = Number(request.params.id);
   const talkers = await readFileTalker();
   const talker = talkers.filter((filtertalker) => filtertalker.id !== Number(id));
