35be54960518c605c288a4106b446812f061d1d0
Junior030 <juniorhenrique030@gmail.com>
2021-09-22 10:26:43 -0300

    Requisito 02 erros e 03 completo

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const { randomBytes } = require('crypto');
+const { randomBytes } = require('crypto');
 const { talkersAll, talker } = require('./data.js');
+const { emailValid, passwordValid } = require('./middleware.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,9 +24,14 @@ app.get('/talker', async (_request, response) => {
 app.get('/talker/:id', async (request, response) => {
   const id = await talker(request.params.id);
   if (!id) { 
-    return response.status(NOT_FOUND).json({ message: 'Speaker person not found' }); 
+    return response.status(NOT_FOUND).json({ message: 'Pessoa palestrante não encontrada' }); 
   }
   response.status(HTTP_OK_STATUS).json(id);
+});
+
+app.post('/login', emailValid, passwordValid, async (request, response) => {
+  const token = randomBytes(8).toString('hex');
+  response.status(HTTP_OK_STATUS).json({ token });
 });
 
 app.listen(PORT, () => {
