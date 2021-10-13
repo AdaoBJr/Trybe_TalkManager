f2d8229f825e3cf6e3402ebe0d1880560b1a77b6
João Victor Pistorio <joao.pistorio@gmail.com>
2021-10-13 11:08:56 -0300

    readAndWrite comments part2

--- services/readAndWrite.js
+++ services/readAndWrite.js
@@ -7,11 +7,15 @@ const ENCODING = 'utf8';
 // Função para ler arquivos
 async function handleFileReading(filePath, encoding = ENCODING) {
   try {
+    // Constante que recebe o resultado da leitura do arquivo
     const fileContent = await readFile(filePath, encoding);
+    // Constante com o conteúdo do arquivo formatado
     const contentFormater = JSON.parse(fileContent);
 
+    // Retorno da leitura do arquivo
     return contentFormater;
   } catch ({ message }) {
+    // Mensagem caso algo de errado aconteça
     console.error(`Erro ao ler o arquivo: ${JSON.stringify(message)}`);
   }
 }
@@ -19,10 +23,12 @@ async function handleFileReading(filePath, encoding = ENCODING) {
 // Função para escrever em arquivos
 async function handleFileWriting(filePath, newContent, encoding = ENCODING) {
   try {
+    // Converte o conteúdo paasado para JSON
     const stringifyContent = JSON.stringify(newContent);
-    
+    // Escreve n arquivo o que foi passado
     await writeFile(filePath, stringifyContent, encoding);
   } catch ({ message }) {
+    // Mensagem caso algo de errado aconteça
     console.error(`Erro ao escrever o arquivo: ${message}`);
   }
 }
